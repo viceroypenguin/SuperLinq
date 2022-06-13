8e2136c72f3a77020216a36d775c2ef30416e1a6
Stuart Turner <stuart@turner-isageek.com>
2022-06-12 20:31:39 -0500

    Use GH Actions as build system

--- pack.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -e
-cd "$(dirname "$0")"
-VERSION_SUFFIX=
-if [ ! -z "$1" ]; then VERSION_SUFFIX="--version-suffix $1"; fi
-./build.sh
-if [ ! -d dist ]; then mkdir dist; fi
-dotnet pack --no-build -c Release $VERSION_SUFFIX
