a588ca4929a585297c8e1f68af77e2a6f5a789da
Stuart Turner <stuart@turner-isageek.com>
2022-07-04 16:20:14 -0500

    Add ValueTask/CancellationToken to some apis

--- Source/SuperLinq.Async/Choose.cs
+++ Source/SuperLinq.Async/Choose.cs
@@ -33,7 +33,10 @@ public static partial class AsyncSuperEnumerable
 		this IAsyncEnumerable<T> source,
 		Func<T, (bool, TResult)> chooser)
 	{
-		return Choose(source, x => new ValueTask<(bool, TResult)>(chooser(x)));
+		source.ThrowIfNull();
+		chooser.ThrowIfNull();
+
+		return Choose(source, (x, ct) => new ValueTask<(bool, TResult)>(chooser(x)));
 	}
 
 	/// <summary>
@@ -70,13 +73,50 @@ public static partial class AsyncSuperEnumerable
 		source.ThrowIfNull();
 		chooser.ThrowIfNull();
 
+		return Choose(source, (x, ct) => chooser(x));
+	}
+
+	/// <summary>
+	/// Applies a function to each element of the source sequence and
+	/// returns a new sequence of result elements for source elements
+	/// where the function returns a couple (2-tuple) having a <see langword="true"/>
+	/// as its first element and result as the second.
+	/// </summary>
+	/// <typeparam name="T">
+	/// The type of the elements in <paramref name="source"/>.</typeparam>
+	/// <typeparam name="TResult">
+	/// The type of the elements in the returned sequence.</typeparam>
+	/// <param name="source"> The source sequence.</param>
+	/// <param name="chooser">The function that is applied to each source
+	/// element.</param>
+	/// <returns>A sequence <typeparamref name="TResult"/> elements.</returns>
+	/// <remarks>
+	/// This method uses deferred execution semantics and streams its
+	/// results.
+	/// </remarks>
+	/// <example>
+	/// <code><![CDATA[
+	/// var str = "O,l,2,3,4,S,6,7,B,9";
+	/// var xs = str.Split(',').Choose(s => (int.TryParse(s, out var n), n));
+	/// ]]></code>
+	/// The <c>xs</c> variable will be a sequence of the integers 2, 3, 4,
+	/// 6, 7 and 9.
+	/// </example>
+
+	public static IAsyncEnumerable<TResult> Choose<T, TResult>(
+		this IAsyncEnumerable<T> source,
+		Func<T, CancellationToken, ValueTask<(bool, TResult)>> chooser)
+	{
+		source.ThrowIfNull();
+		chooser.ThrowIfNull();
+
 		return _(source, chooser);
 
-		static async IAsyncEnumerable<TResult> _(IAsyncEnumerable<T> source, Func<T, ValueTask<(bool, TResult)>> chooser, [EnumeratorCancellation] CancellationToken cancellationToken = default)
+		static async IAsyncEnumerable<TResult> _(IAsyncEnumerable<T> source, Func<T, CancellationToken, ValueTask<(bool, TResult)>> chooser, [EnumeratorCancellation] CancellationToken cancellationToken = default)
 		{
 			await foreach (var item in source.WithCancellation(cancellationToken).ConfigureAwait(false))
 			{
-				var (some, value) = await chooser(item).ConfigureAwait(false);
+				var (some, value) = await chooser(item, cancellationToken).ConfigureAwait(false);
 				if (some)
 					yield return value;
 			}
--- Source/SuperLinq/Generate.cs
+++ Source/SuperLinq/Generate.cs
@@ -11,12 +11,13 @@ public static partial class SuperEnumerable
 	/// Generator function which takes the previous series element and uses it to generate the next element.
 	/// </param>
 	/// <returns>A sequence containing the generated values.</returns>
+	/// <exception cref="ArgumentNullException"><paramref name="generator"/> is <see langword="null"/>.</exception>
 	/// <remarks>
 	/// This function defers element generation until needed and streams the results.
 	/// </remarks>
 	/// <example>
 	/// <code><![CDATA[
-	/// var result = MoreEnumerable.Generate(2, n => n * n).Take(5);
+	/// var result = SuperEnumerable.Generate(2, n => n * n).Take(5);
 	/// ]]></code>
 	/// The <c>result</c> variable, when iterated over, will yield 2, 4, 16, 256, and 65536, in turn.
 	/// </example>
