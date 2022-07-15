5eacdaf56f58b3a53064c3e8069946442eb653b8
Stuart Turner <stuart@turner-isageek.com>
2022-07-15 12:37:48 -0500

    Add .GetShortestPathCost() [Dijkstra]

---
+++ Source/SuperLinq/GetShortestPath.cs
@@ -0,0 +1,275 @@
+﻿// temporary until all methods are implemented
+#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
+
+using SuperLinq.Collections;
+
+namespace SuperLinq;
+
+public partial class SuperEnumerable
+{
+	#region Dijkstra
+
+	#region Simple Cost
+
+	/// <summary>
+	/// Calculate the cost of the shortest path from
+	/// state <paramref name="start"/> to state <paramref name="end"/>,
+	/// using Dijkstra's algorithm.
+	/// </summary>
+	/// <typeparam name="TState">The type of each state in the map</typeparam>
+	/// <typeparam name="TCost">The type of the cost to traverse between states</typeparam>
+	/// <param name="start">The starting state</param>
+	/// <param name="getNeighbors">
+	/// A function that returns the neighbors for a given state
+	/// and the total cost to get to that state based on the 
+	/// traversal cost at the current state.
+	/// </param>
+	/// <param name="end">The target state</param>
+	/// <returns>
+	/// The traversal cost of the shortest path from <paramref name="start"/>
+	/// to <paramref name="end"/>.
+	/// </returns>
+	/// <exception cref="ArgumentNullException"><paramref name="getNeighbors"/> is <see langword="null"/>.</exception>
+	/// <remarks>
+	/// <para>
+	///  This method uses Dijkstra's algorithm to explore a map
+	///  and find the shortest path from <paramref name="start"/>
+	///  to <paramref name="end"/>. An <see cref="UpdatablePriorityQueue{TElement, TPriority}"/>
+	///  is used to manage the list of <typeparamref name="TState"/>s
+	///  to process, to reduce the computation cost of this operator.
+	/// </para>
+	/// <para>
+	///  Loops and cycles are automatically detected and handled
+	///  correctly by this operator; only the cheapest path to
+	///  a given <typeparamref name="TState"/> is used, and other
+	///  paths (including loops) are discarded.
+	/// </para>
+	/// <para>
+	///  Dijkstra's algorithm assumes that all costs are positive,
+	///  that is to say, that it is not possible to go a negative
+	///  distance from one state to the next. Violating this assumption
+	///  will have undefined behavior.
+	/// </para>
+	/// <para>
+	///	 This method uses <see cref="EqualityComparer{T}.Default"/>
+	///	 to compare <typeparamref name="TState"/>s and 
+	///	 <see cref="Comparer{T}.Default"/> to compare traversal
+	///	 <typeparamref name="TCost"/>s.
+	/// </para>
+	/// <para>
+	///  This operator executes immediately.
+	/// </para>
+	/// </remarks>
+	public static TCost? GetShortestPathCost<TState, TCost>(
+		TState start,
+		Func<TState, TCost?, IEnumerable<(TState nextState, TCost cost)>> getNeighbors,
+		TState end)
+		where TState : notnull
+		where TCost : notnull
+	{
+		return GetShortestPathCost(
+			start, getNeighbors, end,
+			stateComparer: null,
+			costComparer: null);
+	}
+
+	/// <summary>
+	/// Calculate the cost of the shortest path from
+	/// state <paramref name="start"/> to state <paramref name="end"/>,
+	/// using Dijkstra's algorithm.
+	/// </summary>
+	/// <typeparam name="TState">The type of each state in the map</typeparam>
+	/// <typeparam name="TCost">The type of the cost to traverse between states</typeparam>
+	/// <param name="start">The starting state</param>
+	/// <param name="getNeighbors">
+	/// A function that returns the neighbors for a given state
+	/// and the total cost to get to that state based on the 
+	/// traversal cost at the current state.
+	/// </param>
+	/// <param name="end">The target state</param>
+	/// <param name="stateComparer">A custom equality comparer for <typeparamref name="TState"/></param>
+	/// <param name="costComparer">A custom comparer for <typeparamref name="TCost"/></param>
+	/// <returns>
+	/// The traversal cost of the shortest path from <paramref name="start"/>
+	/// to <paramref name="end"/>.
+	/// </returns>
+	/// <exception cref="ArgumentNullException"><paramref name="getNeighbors"/> is <see langword="null"/>.</exception>
+	/// <remarks>
+	/// <para>
+	///  This method uses Dijkstra's algorithm to explore a map
+	///  and find the shortest path from <paramref name="start"/>
+	///  to <paramref name="end"/>. An <see cref="UpdatablePriorityQueue{TElement, TPriority}"/>
+	///  is used to manage the list of <typeparamref name="TState"/>s
+	///  to process, to reduce the computation cost of this operator.
+	/// </para>
+	/// <para>
+	///  Loops and cycles are automatically detected and handled
+	///  correctly by this operator; only the cheapest path to
+	///  a given <typeparamref name="TState"/> is used, and other
+	///  paths (including loops) are discarded.
+	/// </para>
+	/// <para>
+	///  Dijkstra's algorithm assumes that all costs are positive,
+	///  that is to say, that it is not possible to go a negative
+	///  distance from one state to the next. Violating this assumption
+	///  will have undefined behavior.
+	/// </para>
+	/// <para>
+	///  This operator executes immediately.
+	/// </para>
+	/// </remarks>
+	public static TCost? GetShortestPathCost<TState, TCost>(
+		TState start,
+		Func<TState, TCost?, IEnumerable<(TState nextState, TCost cost)>> getNeighbors,
+		TState end,
+		IEqualityComparer<TState>? stateComparer,
+		IComparer<TCost>? costComparer)
+		where TState : notnull
+		where TCost : notnull
+	{
+		getNeighbors.ThrowIfNull();
+
+		stateComparer ??= EqualityComparer<TState>.Default;
+		costComparer ??= Comparer<TCost>.Default;
+
+		var totalCost = new Dictionary<TState, TCost?>(stateComparer);
+		var queue = new UpdatablePriorityQueue<TState, TCost>(16, costComparer, stateComparer);
+
+		TState current = start;
+		TCost? cost = default;
+		do
+		{
+			if (totalCost.TryGetValue(current, out _))
+				continue;
+
+			totalCost[current] = cost;
+			if (stateComparer.Equals(current, end))
+				break;
+
+			var newStates = getNeighbors(current, cost);
+			queue.EnqueueRangeMinimum(newStates);
+		} while (queue.TryDequeue(out current!, out cost));
+
+		return cost;
+	}
+
+	#endregion
+
+	#region Single Shortest Path
+
+	public static IEnumerable<(TState nextState, TCost cost)>
+		GetShortestPath<TState, TCost>(
+			TState start,
+			Func<TState, IEnumerable<(TState nextState, TCost cost)>> getNeighbors,
+			TState end)
+		where TState : notnull
+		where TCost : notnull
+	{
+		throw new NotImplementedException();
+	}
+
+	public static IEnumerable<(TState nextState, TCost cost)>
+		GetShortestPath<TState, TCost>(
+			TState start,
+			Func<TState, IEnumerable<(TState nextState, TCost cost)>> getNeighbors,
+			TState end,
+			IEqualityComparer<TState> stateComparer,
+			IComparer<TCost> costComparer)
+		where TState : notnull
+		where TCost : notnull
+	{
+		throw new NotImplementedException();
+	}
+
+	#endregion
+
+	#region Full Map Cost
+
+	public static IReadOnlyDictionary<TState, (TState previousState, TCost cost)>
+		GetShortestPaths<TState, TCost>(
+			TState start,
+			Func<TState, IEnumerable<(TState nextState, TCost cost)>> getNeighbors,
+			TState end)
+		where TState : notnull
+		where TCost : notnull
+	{
+		throw new NotImplementedException();
+	}
+
+	public static IReadOnlyDictionary<TState, (TState previousState, TCost cost)>
+		GetShortestPaths<TState, TCost>(
+			TState start,
+			Func<TState, IEnumerable<(TState nextState, TCost cost)>> getNeighbors,
+			TState end,
+			IEqualityComparer<TState>? stateComparer,
+			IComparer<TCost>? costComparer)
+		where TState : notnull
+		where TCost : notnull
+	{
+		throw new NotImplementedException();
+	}
+
+	#endregion
+
+	#endregion
+
+	#region A*
+
+	#region Simple Cost
+
+	public static TCost GetShortestPathCost<TState, TCost>(
+		TState start,
+		Func<TState, IEnumerable<(TState nextState, TCost cost, TCost bestGuess)>> getNeighbors,
+		TState end)
+		where TState : notnull
+		where TCost : notnull
+	{
+		throw new NotImplementedException();
+	}
+
+	public static TCost GetShortestPathCost<TState, TCost>(
+		TState start,
+		Func<TState, IEnumerable<(TState nextState, TCost cost, TCost bestGuess)>> getNeighbors,
+		TState end,
+		IEqualityComparer<TState>? stateComparer,
+		IComparer<TCost>? costComparer)
+		where TState : notnull
+		where TCost : notnull
+	{
+		throw new NotImplementedException();
+	}
+
+	#endregion
+
+	#region Single Shortest Path
+
+	public static IEnumerable<(TState nextState, TCost cost)>
+		GetShortestPath<TState, TCost>(
+			TState start,
+			Func<TState, IEnumerable<(TState nextState, TCost cost, TCost bestGuess)>> getNeighbors,
+			TState end)
+		where TState : notnull
+		where TCost : notnull
+	{
+		throw new NotImplementedException();
+	}
+
+	public static IEnumerable<(TState nextState, TCost cost)>
+		GetShortestPath<TState, TCost>(
+			TState start,
+			Func<TState, IEnumerable<(TState nextState, TCost cost, TCost bestGuess)>> getNeighbors,
+			TState end,
+			IEqualityComparer<TState> stateComparer,
+			IComparer<TCost> costComparer)
+		where TState : notnull
+		where TCost : notnull
+	{ 
+		throw new NotImplementedException();
+	}
+
+	#endregion
+
+	#endregion
+}
+
+#pragma warning restore CS1591 // Missing XML comment for publicly visible type or member
---
+++ Tests/SuperLinq.Test/GetShortestPathTest.cs
@@ -0,0 +1,58 @@
+﻿namespace Test;
+
+public class GetShortestPathTest
+{
+	public static IEnumerable<object?[]> GetStringIntCostData { get; } =
+		new[]
+		{
+			new object?[] { null, null, 15, },
+			new object?[] { StringComparer.InvariantCultureIgnoreCase, null, 10, },
+			new object?[] { null, Comparer<int>.Create((x, y) => -x.CompareTo(y)), 150, },
+			new object?[] { StringComparer.InvariantCultureIgnoreCase, Comparer<int>.Create((x, y) => -x.CompareTo(y)), 1000, },
+		};
+
+	private static ILookup<string, (string to, int cost)> BuildStringIntMap()
+	{
+		var costs =
+			new[]
+			{
+				(from: "start", to: "a", cost: 1),
+				(from: "a", to: "b", cost: 2),
+				(from: "b", to: "c", cost: 3),
+				(from: "c", to: "d", cost: 4),
+				(from: "d", to: "end", cost: 5),
+				(from: "start", to: "A", cost: 10),
+				(from: "A", to: "B", cost: 20),
+				(from: "B", to: "C", cost: 30),
+				(from: "C", to: "D", cost: 40),
+				(from: "D", to: "end", cost: 50),
+				(from: "start", to: "END", cost: 10),
+				(from: "start", to: "END", cost: 1000),
+			};
+		var map = costs
+			.Concat(costs.Select(x => (from: x.to, to: x.from, x.cost)))
+			.Where(x =>
+				x.to != "start"
+				&& x.from != "end")
+			.ToLookup(x => x.from, x => (x.to, x.cost));
+		return map;
+	}
+
+	[Theory]
+	[MemberData(nameof(GetStringIntCostData))]
+	public void GetStringIntCost(
+		IEqualityComparer<string>? stateComparer,
+		IComparer<int>? costComparer,
+		int expectedCost)
+	{
+		var map = BuildStringIntMap();
+		var actualCost = SuperEnumerable.GetShortestPathCost(
+			"start",
+			(x, c) => map[x].Select(y => (y.to, c + y.cost)),
+			"end",
+			stateComparer,
+			costComparer);
+
+		Assert.Equal(expectedCost, actualCost);
+	}
+}
