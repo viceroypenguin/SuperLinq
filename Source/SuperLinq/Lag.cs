1b8509b03832e55f3141fc83d8af564ed04e1c50
Stuart Turner <stuart@turner-isageek.com>
2022-08-10 15:54:08 -0500

    Improve .Lead/.Lag documentation

--- Source/SuperLinq/Lag.cs
+++ Source/SuperLinq/Lag.cs
@@ -5,15 +5,16 @@ public static partial class SuperEnumerable
 	/// <summary>
 	/// Produces a projection of a sequence by evaluating pairs of elements separated by a negative offset.
 	/// </summary>
-	/// <remarks>
-	/// This operator evaluates in a deferred and streaming manner.<br/>
-	/// For elements prior to the lag offset, <see langword="default"/>(<typeparamref name="TSource"/>?) is used as the lagged value.<br/>
-	/// </remarks>
 	/// <typeparam name="TSource">The type of the elements of the source sequence</typeparam>
 	/// <param name="source">The sequence over which to evaluate lag</param>
 	/// <param name="offset">The offset (expressed as a positive number) by which to lag each value of the sequence</param>
 	/// <returns>A sequence of tuples with the current and lagged elements</returns>
-
+	/// <exception cref="ArgumentNullException"><paramref name="source"/> is <see langword="null"/>.</exception>
+	/// <exception cref="ArgumentOutOfRangeException"><paramref name="offset"/> is below 1.</exception>
+	/// <remarks>
+	/// This operator evaluates in a deferred and streaming manner.<br/>
+	/// For elements prior to the lag offset, <see langword="default"/>(<typeparamref name="TSource"/>?) is used as the lagged value.<br/>
+	/// </remarks>
 	public static IEnumerable<(TSource current, TSource? lag)> Lag<TSource>(this IEnumerable<TSource> source, int offset)
 	{
 		Guard.IsNotNull(source);
@@ -25,17 +26,19 @@ public static partial class SuperEnumerable
 	/// <summary>
 	/// Produces a projection of a sequence by evaluating pairs of elements separated by a negative offset.
 	/// </summary>
-	/// <remarks>
-	/// This operator evaluates in a deferred and streaming manner.<br/>
-	/// For elements prior to the lag offset, <see langword="default"/>(<typeparamref name="TSource"/>?) is used as the lagged value.<br/>
-	/// </remarks>
 	/// <typeparam name="TSource">The type of the elements of the source sequence</typeparam>
 	/// <typeparam name="TResult">The type of the elements of the result sequence</typeparam>
 	/// <param name="source">The sequence over which to evaluate lag</param>
 	/// <param name="offset">The offset (expressed as a positive number) by which to lag each value of the sequence</param>
 	/// <param name="resultSelector">A projection function which accepts the current and lagged items (in that order) and returns a result</param>
 	/// <returns>A sequence produced by projecting each element of the sequence with its lagged pairing</returns>
-
+	/// <exception cref="ArgumentNullException"><paramref name="source"/> is <see langword="null"/>.</exception>
+	/// <exception cref="ArgumentNullException"><paramref name="resultSelector"/> is <see langword="null"/>.</exception>
+	/// <exception cref="ArgumentOutOfRangeException"><paramref name="offset"/> is below 1.</exception>
+	/// <remarks>
+	/// This operator evaluates in a deferred and streaming manner.<br/>
+	/// For elements prior to the lag offset, <see langword="default"/>(<typeparamref name="TSource"/>?) is used as the lagged value.<br/>
+	/// </remarks>
 	public static IEnumerable<TResult> Lag<TSource, TResult>(this IEnumerable<TSource> source, int offset, Func<TSource, TSource?, TResult> resultSelector)
 	{
 		Guard.IsNotNull(source);
@@ -48,9 +51,6 @@ public static partial class SuperEnumerable
 	/// <summary>
 	/// Produces a projection of a sequence by evaluating pairs of elements separated by a negative offset.
 	/// </summary>
-	/// <remarks>
-	/// This operator evaluates in a deferred and streaming manner.<br/>
-	/// </remarks>
 	/// <typeparam name="TSource">The type of the elements of the source sequence</typeparam>
 	/// <typeparam name="TResult">The type of the elements of the result sequence</typeparam>
 	/// <param name="source">The sequence over which to evaluate lag</param>
@@ -58,7 +58,12 @@ public static partial class SuperEnumerable
 	/// <param name="defaultLagValue">A default value supplied for the lagged value prior to the lag offset</param>
 	/// <param name="resultSelector">A projection function which accepts the current and lagged items (in that order) and returns a result</param>
 	/// <returns>A sequence produced by projecting each element of the sequence with its lagged pairing</returns>
-
+	/// <exception cref="ArgumentNullException"><paramref name="source"/> is <see langword="null"/>.</exception>
+	/// <exception cref="ArgumentNullException"><paramref name="resultSelector"/> is <see langword="null"/>.</exception>
+	/// <exception cref="ArgumentOutOfRangeException"><paramref name="offset"/> is below 1.</exception>
+	/// <remarks>
+	/// This operator evaluates in a deferred and streaming manner.<br/>
+	/// </remarks>
 	public static IEnumerable<TResult> Lag<TSource, TResult>(this IEnumerable<TSource> source, int offset, TSource defaultLagValue, Func<TSource, TSource, TResult> resultSelector)
 	{
 		Guard.IsNotNull(source);
--- Source/SuperLinq/Lead.cs
+++ Source/SuperLinq/Lead.cs
@@ -5,16 +5,17 @@ public static partial class SuperEnumerable
 	/// <summary>
 	/// Produces a projection of a sequence by evaluating pairs of elements separated by a positive offset.
 	/// </summary>
+	/// <typeparam name="TSource">The type of the elements in the source sequence</typeparam>
+	/// <param name="source">The sequence over which to evaluate Lead</param>
+	/// <param name="offset">The offset (expressed as a positive number) by which to lead each element of the sequence</param>
+	/// <returns>A sequence of tuples with the current and lead elements</returns>
+	/// <exception cref="ArgumentNullException"><paramref name="source"/> is <see langword="null"/>.</exception>
+	/// <exception cref="ArgumentOutOfRangeException"><paramref name="offset"/> is below 1.</exception>
 	/// <remarks>
 	/// This operator evaluates in a deferred and streaming manner.<br/>
 	/// For elements of the sequence that are less than <paramref name="offset"/> items from the end,
 	/// <see langword="default"/>(<typeparamref name="TSource"/>?) is used as the lead value.<br/>
 	/// </remarks>
-	/// <typeparam name="TSource">The type of the elements in the source sequence</typeparam>
-	/// <param name="source">The sequence over which to evaluate Lead</param>
-	/// <param name="offset">The offset (expressed as a positive number) by which to lead each element of the sequence</param>
-	/// <returns>A sequence of tuples with the current and lead elements</returns>
-
 	public static IEnumerable<(TSource current, TSource? lead)> Lead<TSource>(this IEnumerable<TSource> source, int offset)
 	{
 		Guard.IsNotNull(source);
@@ -26,18 +27,20 @@ public static partial class SuperEnumerable
 	/// <summary>
 	/// Produces a projection of a sequence by evaluating pairs of elements separated by a positive offset.
 	/// </summary>
-	/// <remarks>
-	/// This operator evaluates in a deferred and streaming manner.<br/>
-	/// For elements of the sequence that are less than <paramref name="offset"/> items from the end,
-	/// <see langword="default"/>(<typeparamref name="TSource"/>?) is used as the lead value.<br/>
-	/// </remarks>
 	/// <typeparam name="TSource">The type of the elements in the source sequence</typeparam>
 	/// <typeparam name="TResult">The type of the elements in the result sequence</typeparam>
 	/// <param name="source">The sequence over which to evaluate Lead</param>
 	/// <param name="offset">The offset (expressed as a positive number) by which to lead each element of the sequence</param>
 	/// <param name="resultSelector">A projection function which accepts the current and subsequent (lead) element (in that order) and produces a result</param>
 	/// <returns>A sequence produced by projecting each element of the sequence with its lead pairing</returns>
-
+	/// <exception cref="ArgumentNullException"><paramref name="source"/> is <see langword="null"/>.</exception>
+	/// <exception cref="ArgumentNullException"><paramref name="resultSelector"/> is <see langword="null"/>.</exception>
+	/// <exception cref="ArgumentOutOfRangeException"><paramref name="offset"/> is below 1.</exception>
+	/// <remarks>
+	/// This operator evaluates in a deferred and streaming manner.<br/>
+	/// For elements of the sequence that are less than <paramref name="offset"/> items from the end,
+	/// <see langword="default"/>(<typeparamref name="TSource"/>?) is used as the lead value.<br/>
+	/// </remarks>
 	public static IEnumerable<TResult> Lead<TSource, TResult>(this IEnumerable<TSource> source, int offset, Func<TSource, TSource?, TResult> resultSelector)
 	{
 		Guard.IsNotNull(source);
@@ -60,7 +63,12 @@ public static partial class SuperEnumerable
 	/// <param name="defaultLeadValue">A default value supplied for the leading element when none is available</param>
 	/// <param name="resultSelector">A projection function which accepts the current and subsequent (lead) element (in that order) and produces a result</param>
 	/// <returns>A sequence produced by projecting each element of the sequence with its lead pairing</returns>
-
+	/// <exception cref="ArgumentNullException"><paramref name="source"/> is <see langword="null"/>.</exception>
+	/// <exception cref="ArgumentNullException"><paramref name="resultSelector"/> is <see langword="null"/>.</exception>
+	/// <exception cref="ArgumentOutOfRangeException"><paramref name="offset"/> is below 1.</exception>
+	/// <remarks>
+	/// This operator evaluates in a deferred and streaming manner.<br/>
+	/// </remarks>
 	public static IEnumerable<TResult> Lead<TSource, TResult>(this IEnumerable<TSource> source, int offset, TSource defaultLeadValue, Func<TSource, TSource, TResult> resultSelector)
 	{
 		Guard.IsNotNull(source);
