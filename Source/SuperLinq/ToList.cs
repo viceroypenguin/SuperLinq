89ebdd4a0e62d44c52423b03bd453491d8507885
Konrad Góra <konrad.k.gora@gmail.com>
2022-10-22 21:47:19 +0200

    ToList() with length

---
+++ Source/SuperLinq/ToList.cs
@@ -0,0 +1,20 @@
+﻿namespace SuperLinq;
+
+public static partial class SuperEnumerable
+{
+	/// <summary>
+	/// Creates a <see cref="List{T}" /> from <see cref="IEnumerable{T}"/>.
+	/// </summary>
+	/// <typeparam name="T">The type of list elements.</typeparam>
+	/// <param name="source">The <see cref="IEnumerable{T}"/> to create <see cref="List{T}" /> from.</param >
+	/// <param name="length">Expected length of source.</param>
+	/// <returns>
+	/// A <see cref="List{T}"/> that contains elements from the input sequence
+	/// </returns>
+	public static List<T> ToList<T>(this IEnumerable<T> source, int length)
+	{
+		var resultList = new List<T>(length);
+		source.ForEach(resultList.Add);
+		return resultList;
+	}
+}
---
+++ Tests/SuperLinq.Test/ToListTests.cs
@@ -0,0 +1,30 @@
+﻿namespace Test;
+
+public class ToListTests
+{
+	[Fact]
+	public void ToListWithSmallerAmountOfElementsThanLength()
+	{
+		const int Length = 2;
+		var enumerable = Enumerable.Range(0, 1);
+
+		var result = enumerable.ToList(Length);
+
+		Assert.Equal(enumerable, result);
+		Assert.Equal(Length, result.Capacity);
+	}
+
+	[Theory]
+	[InlineData(3)]
+	[InlineData(5)]
+	public void ToListWithHigherAmountOfElementsThanLength(int dataLength)
+	{
+		const int Length = 2;
+		var enumerable = Enumerable.Range(0, dataLength);
+
+		var result = enumerable.ToList(Length);
+
+		Assert.Equal(enumerable, result);
+		Assert.True(result.Capacity > Length);
+	}
+}
