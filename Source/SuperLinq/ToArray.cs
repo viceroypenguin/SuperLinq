2de7684418c373044b3797180d85d08e94bb4068
Konrad Góra <konrad.k.gora@gmail.com>
2022-10-22 22:16:43 +0200

    ToArray() with length

---
+++ Source/SuperLinq/ToArray.cs
@@ -0,0 +1,46 @@
+﻿namespace SuperLinq;
+
+public static partial class SuperEnumerable
+{
+	/// <summary>
+	/// Creates an array from <see cref="IEnumerable{T}"/>.
+	/// </summary>
+	/// <typeparam name="T">The type of list elements.</typeparam>
+	/// <param name="source">The <see cref="IEnumerable{T}"/> to create array" /> from.</param >
+	/// <param name="length">Expected length of source.</param>
+	/// <returns>
+	/// An array that contains elements from the input sequence
+	/// </returns>
+	public static T[] ToArray<T>(this IEnumerable<T> source, int length)
+	{
+		var resultArray = new T[length];
+		var i = 0;
+		foreach (var item in source)
+		{
+			if (i >= resultArray.Length)
+			{
+				resultArray = CreateTwiceBiggerArray(resultArray);
+			}
+			resultArray[i++] = item;
+		}
+		return i < resultArray.Length
+			? resultArray.TrimArray(i) : resultArray;
+	}
+
+	private static T[] CreateTwiceBiggerArray<T>(T[] array)
+	{
+		var resultArray = new T[array.Length * 2];
+		for (var i = 0; i < array.Length; i++)
+		{
+			resultArray[i] = array[i];
+		}
+		return resultArray;
+	}
+
+	private static T[] TrimArray<T>(this T[] array, int length)
+	{
+		var resultArray = new T[length];
+		Array.Copy(array, resultArray, length);
+		return resultArray;
+	}
+}
---
+++ Tests/SuperLinq.Test/ToArrayTests.cs
@@ -0,0 +1,28 @@
+﻿namespace Test;
+
+public class ToArrayTests
+{
+	[Fact]
+	public void ToArrayWithSmallerAmountOfElementsThanLength()
+	{
+		const int Length = 2;
+		var enumerable = Enumerable.Range(0, 1);
+
+		var result = enumerable.ToArray(Length);
+
+		Assert.Equal(enumerable, result);
+	}
+
+	[Theory]
+	[InlineData(3)]
+	[InlineData(5)]
+	public void ToArrayWithHigherAmountOfElementsThanLength(int dataLength)
+	{
+		const int Length = 2;
+		var enumerable = Enumerable.Range(0, dataLength);
+
+		var result = enumerable.ToArray(Length);
+
+		Assert.Equal(enumerable, result);
+	}
+}
