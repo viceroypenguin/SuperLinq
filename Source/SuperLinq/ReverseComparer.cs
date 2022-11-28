0f5a007451ce7ceb113f1c1fd2462629da21d93c
Stuart Turner <stuart@turner-isageek.com>
2022-11-28 09:57:16 -0600

    `ReverseComparer`: Remove dead code (#102)

--- Source/SuperLinq/ReverseComparer.cs
+++ Source/SuperLinq/ReverseComparer.cs
@@ -4,10 +4,8 @@ internal sealed class ReverseComparer<T> : IComparer<T>
 {
 	private readonly IComparer<T> _underlying;
 
-	public ReverseComparer(IComparer<T>? underlying)
-	{
-		_underlying = underlying ?? Comparer<T>.Default;
-	}
+	public ReverseComparer(IComparer<T> underlying) =>
+		_underlying = underlying;
 
 	public int Compare(T? x, T? y) =>
 		-_underlying.Compare(x, y);
