5e3b5a0d09ce2c75d9b3fddbcd492c9e4f4c16fd
Stuart Turner <stuart@turner-isageek.com>
2022-12-19 18:06:43 -0600

    Remove dead code

--- Tests/SuperLinq.Test/Comparable.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Test;
-
-static class Comparable<T> where T : IComparable<T>
-{
-	public static readonly IComparer<T> DescendingOrderComparer =
-		Comparer<T>.Create((x, y) => -Math.Sign(x.CompareTo(y)));
-}
