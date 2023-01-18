d0911993af331d9df65807d2156b95dfd3484edc
Stuart Turner <stuart@turner-isageek.com>
2023-01-18 08:23:06 -0600

    Update `FallbackIfEmpty` tests

--- Tests/SuperLinq.Async.Test/FallbackIfEmptyTest.cs
+++ Tests/SuperLinq.Async.Test/FallbackIfEmptyTest.cs
@@ -5,16 +5,18 @@ public class FallbackIfEmptyTest
 	[Fact]
 	public async Task FallbackIfEmptyWithEmptySequence()
 	{
-		var source = AsyncEnumerable.Empty<int>().Select(x => x);
-		await source.FallbackIfEmpty(12).AssertSequenceEqual(12);
-		await source.FallbackIfEmpty(12, 23).AssertSequenceEqual(12, 23);
+		await using (var source = AsyncEnumerable.Empty<int>().AsTestingSequence())
+			await source.FallbackIfEmpty(12).AssertSequenceEqual(12);
+		await using (var source = AsyncEnumerable.Empty<int>().AsTestingSequence())
+			await source.FallbackIfEmpty(12, 23).AssertSequenceEqual(12, 23);
 	}
 
 	[Fact]
 	public async Task FallbackIfEmptyWithNotEmptySequence()
 	{
-		var source = Seq(1);
-		Assert.Equal(source, await source.ToAsyncEnumerable().FallbackIfEmpty(12).ToListAsync());
-		Assert.Equal(source, await source.ToAsyncEnumerable().FallbackIfEmpty(12, 23).ToListAsync());
+		await using (var source = AsyncSeq(1).AsTestingSequence())
+			await source.FallbackIfEmpty(12).AssertSequenceEqual(1);
+		await using (var source = AsyncSeq(1).AsTestingSequence())
+			await source.FallbackIfEmpty(12, 23).AssertSequenceEqual(1);
 	}
 }
--- Tests/SuperLinq.Test/FallbackIfEmptyTest.cs
+++ Tests/SuperLinq.Test/FallbackIfEmptyTest.cs
@@ -5,16 +5,18 @@ public class FallbackIfEmptyTest
 	[Fact]
 	public void FallbackIfEmptyWithEmptySequence()
 	{
-		var source = Enumerable.Empty<int>().Select(x => x);
-		source.FallbackIfEmpty(12).AssertSequenceEqual(12);
-		source.FallbackIfEmpty(12, 23).AssertSequenceEqual(12, 23);
+		using (var source = Enumerable.Empty<int>().AsTestingSequence())
+			source.FallbackIfEmpty(12).AssertSequenceEqual(12);
+		using (var source = Enumerable.Empty<int>().AsTestingSequence())
+			source.FallbackIfEmpty(12, 23).AssertSequenceEqual(12, 23);
 	}
 
 	[Fact]
 	public void FallbackIfEmptyWithNotEmptySequence()
 	{
-		var source = Seq(1);
-		Assert.Equal(source, source.FallbackIfEmpty(12));
-		Assert.Equal(source, source.FallbackIfEmpty(12, 23));
+		using (var source = Seq(1).AsTestingSequence())
+			source.FallbackIfEmpty(12).AssertSequenceEqual(1);
+		using (var source = Seq(1).AsTestingSequence())
+			source.FallbackIfEmpty(12, 23).AssertSequenceEqual(1);
 	}
 }
