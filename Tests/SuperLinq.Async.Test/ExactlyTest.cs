65b0b2ac82017f42992a71c170e20df740c8e21a
Stuart Turner <stuart@turner-isageek.com>
2023-01-18 07:39:39 -0600

    Update `Exactly` tests

--- Tests/SuperLinq.Async.Test/ExactlyTest.cs
+++ Tests/SuperLinq.Async.Test/ExactlyTest.cs
@@ -12,30 +12,35 @@ public class ExactlyTest
 	[Fact]
 	public async Task ExactlyWithEmptySequenceHasExactlyZeroElements()
 	{
-		Assert.True(await AsyncEnumerable.Empty<int>().Exactly(0));
+		await using var xs = AsyncEnumerable.Empty<int>().AsTestingSequence();
+		Assert.True(await xs.Exactly(0));
 	}
 
 	[Fact]
 	public async Task ExactlyWithEmptySequenceHasExactlyOneElement()
 	{
-		Assert.False(await AsyncEnumerable.Empty<int>().Exactly(1));
+		await using var xs = AsyncEnumerable.Empty<int>().AsTestingSequence();
+		Assert.False(await xs.Exactly(1));
 	}
 
 	[Fact]
 	public async Task ExactlyWithSingleElementHasExactlyOneElements()
 	{
-		Assert.True(await AsyncSeq(1).Exactly(1));
+		await using var xs = AsyncSeq(1).AsTestingSequence();
+		Assert.True(await xs.Exactly(1));
 	}
 
 	[Fact]
 	public async Task ExactlyWithManyElementHasExactlyOneElement()
 	{
-		Assert.False(await AsyncSeq(1, 2, 3).Exactly(1));
+		await using var xs = AsyncSeq(1, 2, 3).AsTestingSequence();
+		Assert.False(await xs.Exactly(1));
 	}
 
 	[Fact]
 	public async Task ExactlyDoesNotIterateUnnecessaryElements()
 	{
-		Assert.False(await AsyncSeqExceptionAt(4).Exactly(2));
+		await using var xs = AsyncSeqExceptionAt(4).AsTestingSequence();
+		Assert.False(await xs.Exactly(2));
 	}
 }
