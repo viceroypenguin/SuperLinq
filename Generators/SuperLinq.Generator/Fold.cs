64dc746bf2318ff0004f2fd6104009f6291cf330
Stuart Turner <stuart@turner-isageek.com>
2023-01-26 07:25:40 -0600

    Migrate `Fold` operator

--- Generators/SuperLinq.Generator/Fold.cs
+++ Generators/SuperLinq.Generator/Fold.cs
@@ -1,6 +1,7 @@
-﻿using System.Globalization;
-using System.Text;
+﻿using System.Text;
+using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.Text;
+using Scriban;
 
 namespace SuperLinq;
 
@@ -8,67 +9,16 @@ internal static class Fold
 {
 	public static SourceText Generate()
 	{
-		var sb = new StringBuilder();
-		sb.Append(@"
-namespace SuperLinq;
+		var template = Template.Parse(ThisAssembly.Resources.Fold.Text);
+		var output = template.Render(ArgumentNames.Instance);
 
-public static partial class SuperEnumerable
-{");
+		// Apply formatting since indenting isn't that nice in Scriban when rendering nested 
+		// structures via functions.
+		output = Microsoft.CodeAnalysis.CSharp.SyntaxFactory.ParseCompilationUnit(output)
+			.NormalizeWhitespace()
+			.GetText()
+			.ToString();
 
-		var overloads =
-			from i in Enumerable.Range(1, 16)
-			let istr = i.ToString(CultureInfo.InvariantCulture)
-			select new
-			{
-				Ts = string.Join(", ", Enumerable.Repeat("T", i)),
-				Count = i,
-				CountElements = istr + " " + (i == 1 ? "element" : "elements"),
-				CountArg = istr,
-				FolderArgs = "folder" + istr + ": folder",
-			};
-
-		foreach (var e in overloads)
-		{
-			sb.Append($@"
-    /// <summary>
-    /// Returns the result of applying a function to a sequence of {e.CountElements}.
-    /// </summary>
-    /// <remarks>
-    /// This operator uses immediate execution and effectively buffers
-    /// as many items of the source sequence as necessary.
-    /// </remarks>
-    /// <typeparam name=""T"">Type of element in the source sequence</typeparam>
-    /// <typeparam name=""TResult"">Type of the result</typeparam>
-    /// <param name=""source"">The sequence of items to fold.</param>
-    /// <param name=""folder"">Function to apply to the elements in the sequence.</param>
-    /// <returns>The folded value returned by <paramref name=""folder""/>.</returns>
-    /// <exception cref=""ArgumentNullException""><paramref name=""source""/> is null</exception>
-    /// <exception cref=""ArgumentNullException""><paramref name=""folder""/> is null</exception>
-    /// <exception cref=""InvalidOperationException""><paramref name=""source""/> does not contain exactly {e.CountElements}.</exception>
-    public static TResult Fold<T, TResult>(this IEnumerable<T> source, Func<{e.Ts}, TResult> folder)
-    {{
-		Guard.IsNotNull(source);
-		Guard.IsNotNull(folder);
-
-		var elements = source.Take({e.CountArg} + 1).ToList();
-		if (elements.Count != {e.CountArg})
-			ThrowHelper.ThrowInvalidOperationException(
-				$""Sequence contained an incorrect number of elements. (Expected: {e.CountArg}, Actual: {{elements.Count}})"");
-
-		return folder(");
-
-			for (var i = 0; i < e.Count; i++)
-				sb.Append($@"
-			elements[{i}]{(i == e.Count - 1 ? "" : ",")}");
-
-			sb.Append(@"
-		);
-	}");
-		}
-
-		sb.Append(@"
-}");
-
-		return SourceText.From(sb.ToString(), Encoding.UTF8);
+		return SourceText.From(output, Encoding.UTF8);
 	}
 }
