4954fa7fe99bb1fee74868624cfcb553fe5dfac2
Stuart Turner <stuart@turner-isageek.com>
2023-01-30 08:09:29 -0600

    Update `CopyTo` tests to validate return value

--- Tests/SuperLinq.Async.Test/CopyToTest.cs
+++ Tests/SuperLinq.Async.Test/CopyToTest.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.ObjectModel;
+﻿using System.Collections.ObjectModel;
 
 namespace Test.Async;
 
@@ -42,11 +41,13 @@ public class CopyToTest
 	{
 		var array = new int[4];
 
-		await AsyncSeq(1).CopyTo(array);
+		var cnt = await AsyncSeq(1).CopyTo(array);
 		array.AssertSequenceEqual(1, 0, 0, 0);
+		Assert.Equal(1, cnt);
 
-		await AsyncSeq(2).CopyTo(array, 1);
+		cnt = await AsyncSeq(2).CopyTo(array, 1);
 		array.AssertSequenceEqual(1, 2, 0, 0);
+		Assert.Equal(1, cnt);
 	}
 
 	[Fact]
@@ -54,14 +55,17 @@ public class CopyToTest
 	{
 		var list = new List<int>();
 
-		await AsyncSeq(1).CopyTo(list);
+		var cnt = await AsyncSeq(1).CopyTo(list);
 		list.AssertSequenceEqual(1);
+		Assert.Equal(1, cnt);
 
-		await AsyncSeq(2).CopyTo(list, 1);
+		cnt = await AsyncSeq(2).CopyTo(list, 1);
 		list.AssertSequenceEqual(1, 2);
+		Assert.Equal(1, cnt);
 
-		await AsyncSeq(3, 4).CopyTo(list, 1);
+		cnt = await AsyncSeq(3, 4).CopyTo(list, 1);
 		list.AssertSequenceEqual(1, 3, 4);
+		Assert.Equal(2, cnt);
 	}
 
 	[Fact]
@@ -69,13 +73,16 @@ public class CopyToTest
 	{
 		var list = new Collection<int>();
 
-		await AsyncSeq(1).CopyTo(list);
+		var cnt = await AsyncSeq(1).CopyTo(list);
 		list.AssertSequenceEqual(1);
+		Assert.Equal(1, cnt);
 
-		await AsyncSeq(2).CopyTo(list, 1);
+		cnt = await AsyncSeq(2).CopyTo(list, 1);
 		list.AssertSequenceEqual(1, 2);
+		Assert.Equal(1, cnt);
 
-		await AsyncSeq(3, 4).CopyTo(list, 1);
+		cnt = await AsyncSeq(3, 4).CopyTo(list, 1);
 		list.AssertSequenceEqual(1, 3, 4);
+		Assert.Equal(2, cnt);
 	}
 }
