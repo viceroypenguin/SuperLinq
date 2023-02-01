35ebe439e352206603e013062864fdb273ba6a1a
Stuart Turner <stuart@turner-isageek.com>
2023-02-01 10:58:53 -0600

    Update `TakeUntil` tests

--- Tests/SuperLinq.Test/TakeUntilTest.cs
+++ Tests/SuperLinq.Test/TakeUntilTest.cs
@@ -3,37 +3,47 @@
 public class TakeUntilTest
 {
 	[Fact]
-	public void TakeUntilPredicateNeverFalse()
-	{
-		var sequence = Enumerable.Range(0, 5).TakeUntil(x => x != 100);
-		sequence.AssertSequenceEqual(0);
-	}
-
-	[Fact]
-	public void TakeUntilPredicateNeverTrue()
-	{
-		var sequence = Enumerable.Range(0, 5).TakeUntil(x => x == 100);
-		sequence.AssertSequenceEqual(0, 1, 2, 3, 4);
-	}
-
-	[Fact]
-	public void TakeUntilPredicateBecomesTrueHalfWay()
-	{
-		var sequence = Enumerable.Range(0, 5).TakeUntil(x => x == 2);
-		sequence.AssertSequenceEqual(0, 1, 2);
-	}
-
-	[Fact]
 	public void TakeUntilEvaluatesSourceLazily()
 	{
 		new BreakingSequence<string>().TakeUntil(x => x.Length == 0);
 	}
 
 	[Fact]
+	public void TakeUntilPredicateNeverFalse()
+	{
+		using var sequence = Enumerable.Range(0, 5).AsTestingSequence();
+		sequence
+			.TakeUntil(x => x != 100)
+			.AssertSequenceEqual(0);
+	}
+
+	[Fact]
+	public void TakeUntilPredicateNeverTrue()
+	{
+		using var sequence = Enumerable.Range(0, 5).AsTestingSequence();
+		sequence
+			.TakeUntil(x => x == 100)
+			.AssertSequenceEqual(Enumerable.Range(0, 5));
+	}
+
+	[Fact]
+	public void TakeUntilPredicateBecomesTrueHalfWay()
+	{
+		using var sequence = Enumerable.Range(0, 5).AsTestingSequence();
+		sequence
+			.TakeUntil(x => x == 2)
+			.AssertSequenceEqual(0, 1, 2);
+	}
+
+	[Fact]
 	public void TakeUntilEvaluatesPredicateLazily()
 	{
 		// Predicate would explode at x == 0, but we never need to evaluate it due to the Take call.
-		var sequence = Enumerable.Range(-2, 5).TakeUntil(x => 1 / x == 1).Take(3);
-		sequence.AssertSequenceEqual(-2, -1, 0);
+		using var sequence = Enumerable.Range(-2, 5).AsTestingSequence();
+
+		sequence
+			.TakeUntil(x => 1 / x == 1)
+			.Take(3)
+			.AssertSequenceEqual(-2, -1, 0);
 	}
 }
