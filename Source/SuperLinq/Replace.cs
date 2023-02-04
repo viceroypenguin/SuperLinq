5495d27932640ef6c5f57c488d475200c53962cc
Stuart Turner <stuart@turner-isageek.com>
2023-02-04 08:41:35 -0600

    Add `int` form of `Replace` operator (#187)

--- Source/SuperLinq/Replace.cs
+++ Source/SuperLinq/Replace.cs
@@ -3,17 +3,50 @@
 public static partial class SuperEnumerable
 {
 	/// <summary>
-	/// Replaces a single value in a sequence at a specified index
-	/// with the given replacement value.
+	/// Replaces a single value in a sequence at a specified index with the given replacement value.
 	/// </summary>
 	/// <typeparam name="TSource">Type of item in the sequence</typeparam>
 	/// <param name="source">The source sequence.</param>
 	/// <param name="index">The index of the value to replace.</param>
 	/// <param name="value">The replacement value to use at <paramref name="index"/>.</param>
 	/// <returns>
-	/// A sequence with the original values from <paramref name="source"/>,
-	/// except for position <paramref name="index"/> which has the value
-	/// <paramref name="value"/>.
+	/// A sequence with the original values from <paramref name="source"/>, except for position <paramref name="index"/>
+	/// which has the value <paramref name="value"/>.
+	/// </returns>
+	/// <exception cref="ArgumentNullException"><paramref name="source"/> is <see langword="null"/>.</exception>
+	/// <remarks>
+	/// This operator evaluates in a deferred and streaming manner.
+	/// </remarks>
+	public static IEnumerable<TSource> Replace<TSource>(
+		this IEnumerable<TSource> source,
+		int index,
+		TSource value)
+	{
+		Guard.IsNotNull(source);
+
+		return _(source, index, value);
+
+		static IEnumerable<TSource> _(
+			IEnumerable<TSource> source,
+			int index,
+			TSource value)
+		{
+			var i = 0;
+			foreach (var e in source)
+				yield return i++ == index ? value : e;
+		}
+	}
+
+	/// <summary>
+	/// Replaces a single value in a sequence at a specified index with the given replacement value.
+	/// </summary>
+	/// <typeparam name="TSource">Type of item in the sequence</typeparam>
+	/// <param name="source">The source sequence.</param>
+	/// <param name="index">The index of the value to replace.</param>
+	/// <param name="value">The replacement value to use at <paramref name="index"/>.</param>
+	/// <returns>
+	/// A sequence with the original values from <paramref name="source"/>, except for position <paramref name="index"/>
+	/// which has the value <paramref name="value"/>.
 	/// </returns>
 	/// <exception cref="ArgumentNullException"><paramref name="source"/> is <see langword="null"/>.</exception>
 	/// <remarks>
@@ -30,6 +63,12 @@ public static partial class SuperEnumerable
 
 		static IEnumerable<TSource> _(IEnumerable<TSource> source, TSource value, Index index)
 		{
+			if (index.IsFromEnd
+				&& source.TryGetCollectionCount(out var count))
+			{
+				index = index.GetOffset(count);
+			}
+
 			if (!index.IsFromEnd)
 			{
 				var cnt = index.Value;
--- Tests/SuperLinq.Async.Test/ReplaceTest.cs
+++ Tests/SuperLinq.Async.Test/ReplaceTest.cs
@@ -6,15 +6,18 @@ public class ReplaceTest
 	public void ReplaceIsLazy()
 	{
 		new AsyncBreakingSequence<int>().Replace(0, 10);
+		new AsyncBreakingSequence<int>().Replace(new Index(0), 10);
 		new AsyncBreakingSequence<int>().Replace(^0, 10);
 	}
 
 	[Fact]
 	public async Task ReplaceEmptySequence()
 	{
-		await using var seq = Enumerable.Empty<int>().AsTestingSequence(maxEnumerations: 4);
+		await using var seq = Enumerable.Empty<int>().AsTestingSequence(maxEnumerations: 6);
 		await seq.Replace(0, 10).AssertSequenceEqual();
 		await seq.Replace(10, 10).AssertSequenceEqual();
+		await seq.Replace(new Index(0), 10).AssertSequenceEqual();
+		await seq.Replace(new Index(10), 10).AssertSequenceEqual();
 		await seq.Replace(^0, 10).AssertSequenceEqual();
 		await seq.Replace(^10, 10).AssertSequenceEqual();
 	}
@@ -23,11 +26,23 @@ public class ReplaceTest
 		Enumerable.Range(0, 10).Select(i => new object[] { i, });
 
 	[Theory, MemberData(nameof(Indices))]
-	public async Task ReplaceStartIndex(int index)
+	public async Task ReplaceIntIndex(int index)
 	{
 		await using var seq = Enumerable.Range(1, 10).AsTestingSequence();
 
 		var result = seq.Replace(index, 30);
+		await result.AssertSequenceEqual(
+			Enumerable.Range(1, index)
+				.Append(30)
+				.Concat(Enumerable.Range(index + 2, 9 - index)));
+	}
+
+	[Theory, MemberData(nameof(Indices))]
+	public async Task ReplaceStartIndex(int index)
+	{
+		await using var seq = Enumerable.Range(1, 10).AsTestingSequence();
+
+		var result = seq.Replace(new Index(index), 30);
 		await result.AssertSequenceEqual(
 			Enumerable.Range(1, index)
 				.Append(30)
@@ -47,11 +62,20 @@ public class ReplaceTest
 	}
 
 	[Fact]
-	public async Task ReplaceStartIndexPastSequenceLength()
+	public async Task ReplaceIntIndexPastSequenceLength()
 	{
 		await using var seq = Enumerable.Range(1, 10).AsTestingSequence();
 
 		var result = seq.Replace(10, 30);
+		await result.AssertSequenceEqual(Enumerable.Range(1, 10));
+	}
+
+	[Fact]
+	public async Task ReplaceStartIndexPastSequenceLength()
+	{
+		await using var seq = Enumerable.Range(1, 10).AsTestingSequence();
+
+		var result = seq.Replace(new Index(10), 30);
 		await result.AssertSequenceEqual(Enumerable.Range(1, 10));
 	}
 
