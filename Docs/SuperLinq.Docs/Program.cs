21287f2c91ed0b42036d840699d3b2f694f4364c
Stuart Turner <stuart@turner-isageek.com>
2023-02-17 11:10:39 -0600

    Set VirtualRoot to match deployment

--- Docs/SuperLinq.Docs/Program.cs
+++ Docs/SuperLinq.Docs/Program.cs
@@ -1,6 +1,7 @@
 ﻿await Bootstrapper
 	.Factory
 	.CreateDocs(args)
+	.AddSetting("LinkRoot", "/SuperLinq/")
 	.AddSourceFiles(
 		"../../../Source/SuperLinq/{!bin,!obj,}/**/*.cs",
 		"../../../Source/SuperLinq.Async/{!bin,!obj,}/**/*.cs")
