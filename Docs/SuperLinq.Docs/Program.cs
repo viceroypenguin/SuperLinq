3da4d461b9b8703e3895f7a654144a4841189899
Stuart Turner <stuart@turner-isageek.com>
2023-02-17 10:26:19 -0600

    Add documentation generator project

---
+++ Docs/SuperLinq.Docs/Program.cs
@@ -0,0 +1,7 @@
+﻿await Bootstrapper
+	.Factory
+	.CreateDocs(args)
+	.AddSourceFiles(
+		"../../../Source/SuperLinq/{!bin,!obj,}/**/*.cs",
+		"../../../Source/SuperLinq.Async/{!bin,!obj,}/**/*.cs")
+	.RunAsync();
---
+++ Docs/SuperLinq.Docs/input/index.md
@@ -0,0 +1,6 @@
+﻿Title: Home
+---
+# Link to API
+
+[SuperLinq methods](xref:api-SuperLinq.SuperEnumerable)<br/>
+[SuperLinq.Async methods](xref:api-SuperLinq.Async.AsyncSuperEnumerable)
