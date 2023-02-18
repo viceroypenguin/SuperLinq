78312f57da585df7088f5982bbd8a2d1c6cd1d39
Stuart Turner <stuart@turner-isageek.com>
2023-02-18 14:01:05 -0600

    Add `Defer` operator (#223)

---
+++ Tests/SuperLinq.Test/DeferTest.cs
@@ -0,0 +1,35 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT License.
+
+namespace Test;
+
+public class DeferTest
+{
+	[Fact]
+	public void DeferIsLazy()
+	{
+		_ = SuperEnumerable.Defer(BreakingFunc.Of<IEnumerable<int>>());
+	}
+
+	[Fact]
+	public void DeferBehavior()
+	{
+		var starts = 0;
+		var length = 5;
+
+		var seq = SuperEnumerable.Defer(() =>
+		{
+			starts++;
+			return Enumerable.Range(1, length);
+		});
+
+		Assert.Equal(0, starts);
+
+		seq.AssertSequenceEqual(Enumerable.Range(1, length));
+		Assert.Equal(1, starts);
+
+		length = 10;
+		seq.AssertSequenceEqual(Enumerable.Range(1, length));
+		Assert.Equal(2, starts);
+	}
+}
