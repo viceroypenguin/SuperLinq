c3979db7dcb30f5262dda5b968d287086058a170
Stuart Turner <stuart@turner-isageek.com>
2023-02-18 08:23:10 -0600

    try assembly loading workaround?

--- Docs/SuperLinq.Docs/Program.cs
+++ Docs/SuperLinq.Docs/Program.cs
@@ -1,3 +1,14 @@
-﻿using Microsoft.DocAsCode;
+﻿using Microsoft.Build.Locator;
+using System.Runtime.Loader;
+using Microsoft.DocAsCode;
+
+var instance = MSBuildLocator.RegisterDefaults();
+AssemblyLoadContext.Default.Resolving += (assemblyLoadContext, assemblyName) =>
+{
+	var path = Path.Combine(instance.MSBuildPath, assemblyName.Name + ".dll");
+	return File.Exists(path)
+		? assemblyLoadContext.LoadFromAssemblyPath(path)
+		: null;
+};
 
 await Docset.Build("docfx.json");
