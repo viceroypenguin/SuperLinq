5d664488c0c2bbf94625840cf8811a61c76e9851
Stuart Turner <stuart@turner-isageek.com>
2023-02-18 11:56:55 -0600

    Rename `ScanEx` back to `Scan`

---
+++ Source/SuperLinq/Scan.cs
@@ -0,0 +1,133 @@
+﻿namespace SuperLinq;
+
+public static partial class SuperEnumerable
+{
+	/// <summary>
+	/// Performs a scan (inclusive prefix sum) on a sequence of elements. This operator is similar to <see
+	/// cref="Enumerable.Aggregate{TSource}"/> except that <see cref="Scan{TSource}"/> returns the sequence of
+	/// intermediate results as well as the final one.
+	/// </summary>
+	/// <typeparam name="TSource">Type of elements in source sequence</typeparam>
+	/// <param name="source">Source sequence</param>
+	/// <param name="transformation">Transformation operation</param>
+	/// <returns>The scanned sequence</returns>
+	/// <exception cref="ArgumentNullException"><paramref name="source"/> or <paramref name="transformation"/> is <see
+	/// langword="null"/>.</exception>
+	/// <remarks>
+	/// This operator uses deferred execution and streams its result.
+	/// </remarks>
+	public static IEnumerable<TSource> Scan<TSource>(
+		this IEnumerable<TSource> source,
+		Func<TSource, TSource, TSource> transformation)
+	{
+		Guard.IsNotNull(source);
+		Guard.IsNotNull(transformation);
+
+		return Core(source, transformation);
+
+		static IEnumerable<TSource> Core(IEnumerable<TSource> source, Func<TSource, TSource, TSource> transformation)
+		{
+			using var e = source.GetEnumerator();
+
+			if (!e.MoveNext())
+				yield break;
+
+			var state = e.Current;
+			yield return state;
+
+			while (e.MoveNext())
+			{
+				state = transformation(state, e.Current);
+				yield return state;
+			}
+		}
+	}
+
+	/// <summary>
+	/// Performs a scan (inclusive prefix sum) on a sequence of elements. This operator is similar to <see
+	/// cref="Enumerable.Aggregate{TSource}"/> except that <see cref="Scan{TSource}"/> returns the sequence of
+	/// intermediate results as well as the final one.
+	/// </summary>
+	/// <typeparam name="TSource">Type of elements in source sequence</typeparam>
+	/// <param name="source">Source sequence</param>
+	/// <param name="transformation">Transformation operation</param>
+	/// <returns>The scanned sequence</returns>
+	/// <exception cref="ArgumentNullException"><paramref name="source"/> or <paramref name="transformation"/> is <see
+	/// langword="null"/>.</exception>
+	/// <remarks>
+	/// This operator uses deferred execution and streams its result.
+	/// </remarks>
+	[Obsolete("Method renamed back to `Scan`.")]
+	public static IEnumerable<TSource> ScanEx<TSource>(
+		this IEnumerable<TSource> source,
+		Func<TSource, TSource, TSource> transformation)
+	{
+		return Scan(source, transformation);
+	}
+
+	/// <summary>
+	/// Performs a scan (inclusive prefix sum) on a sequence of elements. This operator is similar to <see
+	/// cref="Enumerable.Aggregate{TSource, TState}"/> except that <see cref="Scan{TSource,TState}"/> returns the
+	/// sequence of intermediate results as well as the final one.
+	/// </summary>
+	/// <typeparam name="TSource">Type of elements in source sequence</typeparam>
+	/// <typeparam name="TState">Type of state</typeparam>
+	/// <param name="source">Source sequence</param>
+	/// <param name="seed">Initial state to seed</param>
+	/// <param name="transformation">Transformation operation</param>
+	/// <returns>The scanned sequence</returns>
+	/// <exception cref="ArgumentNullException"><paramref name="source"/> or <paramref name="transformation"/> is <see
+	/// langword="null"/>.</exception>
+	/// <remarks>
+	/// This operator uses deferred execution and streams its result.
+	/// </remarks>
+	public static IEnumerable<TState> Scan<TSource, TState>(
+		this IEnumerable<TSource> source,
+		TState seed,
+		Func<TState, TSource, TState> transformation)
+	{
+		Guard.IsNotNull(source);
+		Guard.IsNotNull(transformation);
+
+		return Core(source, seed, transformation);
+
+		static IEnumerable<TState> Core(
+			IEnumerable<TSource> source,
+			TState state,
+			Func<TState, TSource, TState> transformation)
+		{
+			yield return state;
+
+			foreach (var e in source)
+			{
+				state = transformation(state, e);
+				yield return state;
+			}
+		}
+	}
+
+	/// <summary>
+	/// Performs a scan (inclusive prefix sum) on a sequence of elements. This operator is similar to <see
+	/// cref="Enumerable.Aggregate{TSource, TState}"/> except that <see cref="Scan{TSource,TState}"/> returns the
+	/// sequence of intermediate results as well as the final one.
+	/// </summary>
+	/// <typeparam name="TSource">Type of elements in source sequence</typeparam>
+	/// <typeparam name="TState">Type of state</typeparam>
+	/// <param name="source">Source sequence</param>
+	/// <param name="seed">Initial state to seed</param>
+	/// <param name="transformation">Transformation operation</param>
+	/// <returns>The scanned sequence</returns>
+	/// <exception cref="ArgumentNullException"><paramref name="source"/> or <paramref name="transformation"/> is <see
+	/// langword="null"/>.</exception>
+	/// <remarks>
+	/// This operator uses deferred execution and streams its result.
+	/// </remarks>
+	[Obsolete("Method renamed back to `Scan`.")]
+	public static IEnumerable<TState> ScanEx<TSource, TState>(
+		this IEnumerable<TSource> source,
+		TState seed,
+		Func<TState, TSource, TState> transformation)
+	{
+		return Scan(source, seed, transformation);
+	}
+}
--- Tests/SuperLinq.Test/ScanTest.cs
+++ Tests/SuperLinq.Test/ScanTest.cs
@@ -1,73 +1,73 @@
 ﻿namespace Test;
 
-public class ScanExTest
+public class ScanTest
 {
 	[Fact]
-	public void ScanExEmpty()
+	public void ScanEmpty()
 	{
 		using var seq = TestingSequence.Of<int>();
 
-		var result = seq.ScanEx((a, b) => a + b);
+		var result = seq.Scan((a, b) => a + b);
 		result.AssertSequenceEqual();
 	}
 
 	[Fact]
-	public void ScanExSum()
+	public void ScanSum()
 	{
 		using var seq = Enumerable.Range(1, 10).AsTestingSequence();
 
-		var result = seq.ScanEx((a, b) => a + b);
+		var result = seq.Scan((a, b) => a + b);
 		result.AssertSequenceEqual(1, 3, 6, 10, 15, 21, 28, 36, 45, 55);
 	}
 
 	[Fact]
-	public void ScanExIsLazy()
+	public void ScanIsLazy()
 	{
-		_ = new BreakingSequence<object>().ScanEx(BreakingFunc.Of<object, object, object>());
+		_ = new BreakingSequence<object>().Scan(BreakingFunc.Of<object, object, object>());
 	}
 
 	[Fact]
-	public void ScanExDoesNotIterateExtra()
+	public void ScanDoesNotIterateExtra()
 	{
 		using var seq = SeqExceptionAt(4).AsTestingSequence(maxEnumerations: 2);
 
-		var result = seq.ScanEx((a, b) => a + b);
+		var result = seq.Scan((a, b) => a + b);
 
 		_ = Assert.Throws<TestException>(result.Consume);
 		result.Take(3).AssertSequenceEqual(1, 3, 6);
 	}
 
 	[Fact]
-	public void SeededScanExEmpty()
+	public void SeededScanEmpty()
 	{
 		using var seq = TestingSequence.Of<int>();
 
-		var result = seq.ScanEx(-1, (a, b) => a + b);
+		var result = seq.Scan(-1, (a, b) => a + b);
 		Assert.Equal(-1, result.Single());
 	}
 
 	[Fact]
-	public void SeededScanExSum()
+	public void SeededScanSum()
 	{
 		using var seq = Enumerable.Range(1, 10).AsTestingSequence();
 
-		var result = seq.ScanEx(0, (a, b) => a + b);
+		var result = seq.Scan(0, (a, b) => a + b);
 		result.AssertSequenceEqual(0, 1, 3, 6, 10, 15, 21, 28, 36, 45, 55);
 	}
 
 	[Fact]
-	public void SeededScanExIsLazy()
+	public void SeededScanIsLazy()
 	{
-		_ = new BreakingSequence<object>().ScanEx(seed: null,
+		_ = new BreakingSequence<object>().Scan(seed: null,
 			BreakingFunc.Of<object?, object, object>());
 	}
 
 	[Fact]
-	public void SeededScanExDoesNotIterateExtra()
+	public void SeededScanDoesNotIterateExtra()
 	{
 		using var seq = SeqExceptionAt(4).AsTestingSequence(maxEnumerations: 2);
 
-		var result = seq.ScanEx(0, (a, b) => a + b);
+		var result = seq.Scan(0, (a, b) => a + b);
 
 		_ = Assert.Throws<TestException>(result.Consume);
 		result.Take(4).AssertSequenceEqual(0, 1, 3, 6);
