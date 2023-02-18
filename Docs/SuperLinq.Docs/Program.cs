f7ee8322f210fccf2221a626fc5382fd8c2c59d5
Stuart Turner <stuart@turner-isageek.com>
2023-02-18 08:25:33 -0600

    nope...

--- Docs/SuperLinq.Docs/Program.cs
+++ Docs/SuperLinq.Docs/Program.cs
@@ -1,14 +1,3 @@
-﻿using Microsoft.Build.Locator;
-using System.Runtime.Loader;
-using Microsoft.DocAsCode;
-
-var instance = MSBuildLocator.RegisterDefaults();
-AssemblyLoadContext.Default.Resolving += (assemblyLoadContext, assemblyName) =>
-{
-	var path = Path.Combine(instance.MSBuildPath, assemblyName.Name + ".dll");
-	return File.Exists(path)
-		? assemblyLoadContext.LoadFromAssemblyPath(path)
-		: null;
-};
+﻿using Microsoft.DocAsCode;
 
 await Docset.Build("docfx.json");
