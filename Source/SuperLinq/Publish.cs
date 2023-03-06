8b71d0ac55092f52be1d77338a6836b95ee04ce6
Stuart Turner <stuart@turner-isageek.com>
2023-03-06 16:25:05 -0600

    Add `Publish` operator

---
+++ Source/SuperLinq/Publish.cs
@@ -0,0 +1,235 @@
+﻿using System.Collections;
+using System.Runtime.ExceptionServices;
+
+namespace SuperLinq;
+
+public static partial class SuperEnumerable
+{
+	/// <summary>
+	/// Creates a buffer with a view over the source sequence, causing each enumerator to obtain access to the remainder
+	/// of the sequence from the current index in the buffer.
+	/// </summary>
+	/// <typeparam name="TSource">Source sequence element type.</typeparam>
+	/// <param name="source">Source sequence.</param>
+	/// <returns>
+	/// Buffer enabling each enumerator to retrieve elements from the shared source sequence, starting from the index at
+	/// the point of obtaining the enumerator.
+	/// </returns>
+	/// <exception cref="ArgumentNullException"><paramref name="source"/> is <see langword="null"/>.</exception>
+	/// <example>
+	/// <code><![CDATA[
+	///		var rng = Enumerable.Range(0, 10).Publish();
+	///		var e1 = rng.GetEnumerator();    // e1 has a view on the source starting from element 0
+	///		Assert.IsTrue(e1.MoveNext());
+	///		Assert.AreEqual(0, e1.Current);
+	///		Assert.IsTrue(e1.MoveNext());
+	///		Assert.AreEqual(1, e1.Current);
+	///		var e2 = rng.GetEnumerator();
+	///		Assert.IsTrue(e2.MoveNext());    // e2 has a view on the source starting from element 2
+	///		Assert.AreEqual(2, e2.Current);
+	///		Assert.IsTrue(e1.MoveNext());    // e1 continues to enumerate over its view
+	///		Assert.AreEqual(2, e1.Current);
+	/// ]]></code>
+	/// </example>
+	public static IBuffer<TSource> Publish<TSource>(this IEnumerable<TSource> source)
+	{
+		Guard.IsNotNull(source);
+
+		return new PublishBuffer<TSource>(source);
+	}
+
+	/// <summary>
+	/// Publishes the source sequence within a selector function where each enumerator can obtain a view over a tail of
+	/// the source sequence.
+	/// </summary>
+	/// <typeparam name="TSource">Source sequence element type.</typeparam>
+	/// <typeparam name="TResult">Result sequence element type.</typeparam>
+	/// <param name="source">Source sequence.</param>
+	/// <param name="selector">Selector function with published access to the source sequence for each
+	/// enumerator.</param>
+	/// <returns>Sequence resulting from applying the selector function to the published view over the source
+	/// sequence.</returns>
+	/// <exception cref="ArgumentNullException"><paramref name="source"/> or <paramref name="selector"/> is <see
+	/// langword="null"/>.</exception>
+	public static IEnumerable<TResult> Publish<TSource, TResult>(this IEnumerable<TSource> source, Func<IEnumerable<TSource>, IEnumerable<TResult>> selector)
+	{
+		Guard.IsNotNull(source);
+		Guard.IsNotNull(selector);
+
+		return Core(source, selector);
+
+		static IEnumerable<TResult> Core(IEnumerable<TSource> source, Func<IEnumerable<TSource>, IEnumerable<TResult>> selector)
+		{
+			using var buffer = source.Publish();
+			foreach (var i in selector(buffer))
+				yield return i;
+		}
+	}
+
+	private sealed class PublishBuffer<T> : IBuffer<T>
+	{
+		private readonly object _lock = new();
+
+		private IEnumerable<T>? _source;
+
+		private IEnumerator<T>? _enumerator;
+		private List<Queue<T>>? _buffers;
+		private bool _initialized;
+		private int _version;
+
+		private ExceptionDispatchInfo? _exception;
+		private bool? _exceptionOnGetEnumerator;
+
+		private bool _disposed;
+
+		public PublishBuffer(IEnumerable<T> source)
+		{
+			_source = source;
+		}
+
+		public int Count => _buffers?.Count > 0 ? _buffers.Max(x => x.Count) : 0;
+
+		public void Reset()
+		{
+			lock (_lock)
+			{
+				if (_disposed)
+					ThrowHelper.ThrowObjectDisposedException(nameof(IBuffer<T>));
+
+				_initialized = false;
+				_version++;
+
+				_buffers = null;
+
+				_enumerator?.Dispose();
+				_enumerator = null;
+				_exception = null;
+				_exceptionOnGetEnumerator = null;
+			}
+		}
+
+		public IEnumerator<T> GetEnumerator()
+		{
+			var (buffer, version) = InitializeEnumerator();
+			return GetEnumeratorImpl(buffer, version);
+		}
+
+		private (Queue<T> buffer, int version) InitializeEnumerator()
+		{
+			lock (_lock)
+			{
+				if (_disposed)
+					ThrowHelper.ThrowObjectDisposedException(nameof(IBuffer<T>));
+
+				Assert.NotNull(_source);
+
+				if (_exceptionOnGetEnumerator == true)
+				{
+					Assert.NotNull(_exception);
+					_exception.Throw();
+				}
+
+				if (!_initialized)
+				{
+					try
+					{
+						_enumerator = _source.GetEnumerator();
+						_buffers = new();
+						_initialized = true;
+					}
+					catch (Exception ex)
+					{
+						_exception = ExceptionDispatchInfo.Capture(ex);
+						_exceptionOnGetEnumerator = true;
+						throw;
+					}
+				}
+
+				Assert.NotNull(_buffers);
+
+				var queue = new Queue<T>();
+				_buffers.Add(queue);
+				return (queue, _version);
+			}
+		}
+		private IEnumerator<T> GetEnumeratorImpl(Queue<T> buffer, int version)
+		{
+			try
+			{
+				while (true)
+				{
+					T? element;
+					lock (_lock)
+					{
+						if (_disposed)
+							ThrowHelper.ThrowObjectDisposedException(nameof(IBuffer<T>));
+						if (!_initialized
+							|| version != _version)
+						{
+							ThrowHelper.ThrowInvalidOperationException("Buffer reset during iteration.");
+						}
+
+						if (buffer.Count == 0)
+						{
+							_exception?.Throw();
+
+							if (_enumerator == null)
+								break;
+
+							var moved = false;
+							try
+							{
+								moved = _enumerator.MoveNext();
+							}
+							catch (Exception ex)
+							{
+								_exception = ExceptionDispatchInfo.Capture(ex);
+								_exceptionOnGetEnumerator = false;
+								_enumerator.Dispose();
+								_enumerator = null;
+								throw;
+							}
+
+							if (!moved)
+							{
+								_enumerator.Dispose();
+								_enumerator = null;
+								break;
+							}
+
+							Assert.NotNull(_buffers);
+
+							var current = _enumerator.Current;
+							foreach (var q in _buffers)
+								q.Enqueue(current);
+						}
+
+						element = buffer.Dequeue();
+					}
+
+					yield return element;
+				}
+			}
+			finally
+			{
+				_ = _buffers?.Remove(buffer);
+			}
+		}
+
+		IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+
+		public void Dispose()
+		{
+			lock (_lock)
+			{
+				_disposed = true;
+
+				_buffers = null;
+
+				_enumerator?.Dispose();
+				_enumerator = null;
+				_source = null;
+			}
+		}
+	}
+}
---
+++ Tests/SuperLinq.Test/PublishTest.cs
@@ -0,0 +1,306 @@
+﻿using System.Collections;
+using CommunityToolkit.Diagnostics;
+
+namespace Test;
+
+public class PublishTest
+{
+	[Fact]
+	public void PublishIsLazy()
+	{
+		_ = new BreakingSequence<int>().Publish();
+	}
+
+	[Fact]
+	public void PublishWithSingleConsumer()
+	{
+		using var seq = Enumerable.Range(1, 10).AsTestingSequence();
+
+		using var result = seq.Publish();
+		result.AssertSequenceEqual(Enumerable.Range(1, 10));
+	}
+
+	[Fact]
+	public void PublishWithMultipleConsumers()
+	{
+		using var seq = Enumerable.Range(1, 10).AsTestingSequence();
+
+		using var result = seq.Publish();
+
+		using var r1 = result.Read();
+		Assert.Equal(1, r1.Read());
+		Assert.Equal(2, r1.Read());
+		Assert.Equal(0, result.Count);
+
+		using var r2 = result.Read();
+		Assert.Equal(3, r1.Read());
+		Assert.Equal(3, r2.Read());
+		Assert.Equal(0, result.Count);
+
+		Assert.Equal(4, r1.Read());
+		Assert.Equal(5, r1.Read());
+		Assert.Equal(6, r1.Read());
+		Assert.Equal(7, r1.Read());
+		Assert.Equal(4, result.Count);
+
+		Assert.Equal(4, r2.Read());
+		Assert.Equal(5, r2.Read());
+		Assert.Equal(2, result.Count);
+
+		Assert.Equal(8, r1.Read());
+		Assert.Equal(9, r1.Read());
+		Assert.Equal(4, result.Count);
+
+		using var r3 = result.Read();
+		Assert.Equal(10, r3.Read());
+		r3.ReadEnd();
+		Assert.Equal(5, result.Count);
+
+		Assert.Equal(6, r2.Read());
+		Assert.Equal(7, r2.Read());
+		Assert.Equal(3, result.Count);
+
+		Assert.Equal(10, r1.Read());
+		r1.ReadEnd();
+
+		Assert.Equal(8, r2.Read());
+		Assert.Equal(9, r2.Read());
+		Assert.Equal(10, r2.Read());
+		r2.ReadEnd();
+		Assert.Equal(0, result.Count);
+	}
+
+	[Fact]
+	public void PublishWithInnerConsumer()
+	{
+		using var seq = Enumerable.Range(1, 6).AsTestingSequence();
+
+		using var result = seq.Publish();
+
+		using var r1 = result.Read();
+		Assert.Equal(1, r1.Read());
+		Assert.Equal(2, r1.Read());
+
+		using (var r2 = result.Read())
+		{
+			Assert.Equal(3, r2.Read());
+			Assert.Equal(4, r2.Read());
+			Assert.Equal(2, result.Count);
+		}
+
+		Assert.Equal(3, r1.Read());
+		Assert.Equal(4, r1.Read());
+		Assert.Equal(5, r1.Read());
+		Assert.Equal(6, r1.Read());
+
+		r1.ReadEnd();
+		Assert.Equal(0, result.Count);
+	}
+
+	[Fact]
+	public void PublishWithSequentialPartialConsumers()
+	{
+		using var seq = Enumerable.Range(1, 10).AsTestingSequence();
+
+		using var result = seq.Publish();
+
+		using (var r1 = result.Read())
+		{
+			Assert.Equal(1, r1.Read());
+			Assert.Equal(2, r1.Read());
+			Assert.Equal(3, r1.Read());
+			Assert.Equal(4, r1.Read());
+			Assert.Equal(5, r1.Read());
+			Assert.Equal(0, result.Count);
+		}
+
+		using (var r2 = result.Read())
+		{
+			Assert.Equal(6, r2.Read());
+			Assert.Equal(7, r2.Read());
+			Assert.Equal(8, r2.Read());
+			Assert.Equal(9, r2.Read());
+			Assert.Equal(10, r2.Read());
+			r2.ReadEnd();
+			Assert.Equal(0, result.Count);
+		}
+
+		using var r3 = result.Read();
+		r3.ReadEnd();
+		Assert.Equal(0, result.Count);
+	}
+
+	[Fact]
+	public void PublishDisposesAfterSourceIsIteratedEntirely()
+	{
+		using var seq = Enumerable.Range(0, 10).AsTestingSequence();
+
+		using var buffer = seq.Publish();
+		buffer.Consume();
+
+		Assert.True(seq.IsDisposed);
+	}
+
+	[Fact]
+	public void PublishDisposesWithPartialEnumeration()
+	{
+		using var seq = Enumerable.Range(0, 10).AsTestingSequence();
+
+		using var buffer = seq.Publish();
+
+		using (buffer)
+			buffer.Take(5).Consume();
+
+		Assert.True(seq.IsDisposed);
+	}
+
+	[Fact]
+	public void PublishRestartsAfterReset()
+	{
+		var starts = 0;
+
+		IEnumerable<int> TestSequence()
+		{
+			starts++;
+			yield return 1;
+			yield return 2;
+		}
+
+		using var seq = TestSequence().AsTestingSequence(maxEnumerations: 2);
+		using var buffer = seq.Publish();
+
+		buffer.Take(1).Consume();
+		Assert.Equal(1, starts);
+
+		buffer.Reset();
+		buffer.Take(1).Consume();
+		Assert.Equal(2, starts);
+	}
+
+	[Fact]
+	public void PublishThrowsWhenCacheDisposedDuringIteration()
+	{
+		using var seq = Enumerable.Range(0, 10).AsTestingSequence();
+		using var buffer = seq.Publish();
+
+		using var reader = buffer.Read();
+
+		Assert.Equal(0, reader.Read());
+		buffer.Dispose();
+
+		_ = Assert.Throws<ObjectDisposedException>(
+			() => reader.Read());
+	}
+
+	[Fact]
+	public void PublishThrowsWhenResetDuringIteration()
+	{
+		using var seq = Enumerable.Range(0, 10).AsTestingSequence();
+		using var buffer = seq.Publish();
+
+		using var reader = buffer.Read();
+
+		Assert.Equal(0, reader.Read());
+		buffer.Reset();
+
+		var ex = Assert.Throws<InvalidOperationException>(
+			() => reader.Read());
+		Assert.Equal("Buffer reset during iteration.", ex.Message);
+	}
+
+	[Fact]
+	public void PublishThrowsWhenGettingIteratorAfterDispose()
+	{
+		using var seq = Enumerable.Range(0, 10).AsTestingSequence();
+		using var buffer = seq.Publish();
+		buffer.Consume();
+		buffer.Dispose();
+
+		_ = Assert.Throws<ObjectDisposedException>(
+			buffer.Consume);
+	}
+
+	[Fact]
+	public void PublishThrowsWhenResettingAfterDispose()
+	{
+		using var seq = Enumerable.Range(0, 10).AsTestingSequence();
+		using var buffer = seq.Publish();
+		buffer.Consume();
+		buffer.Dispose();
+
+		_ = Assert.Throws<ObjectDisposedException>(
+			buffer.Reset);
+	}
+
+	[Fact]
+	public void PublishRethrowsErrorDuringIterationToAllIteratorsUntilReset()
+	{
+		using var xs = SeqExceptionAt(2).AsTestingSequence(maxEnumerations: 2);
+
+		using var buffer = xs.Publish();
+
+		using (var r1 = buffer.Read())
+		using (var r2 = buffer.Read())
+		{
+			Assert.Equal(1, r1.Read());
+			Assert.Equal(1, r2.Read());
+
+			_ = Assert.Throws<TestException>(() => r1.Read());
+			_ = Assert.Throws<TestException>(() => r2.Read());
+			Guard.IsTrue(xs.IsDisposed);
+		}
+
+		using (var r3 = buffer.Read())
+			_ = Assert.Throws<TestException>(() => r3.Read());
+
+		buffer.Reset();
+
+		using (var r4 = buffer.Read())
+			Assert.Equal(1, r4.Read());
+	}
+
+	[Fact]
+	public void PublishRethrowsErrorDuringFirstIterationStartToAllIterationsUntilReset()
+	{
+		using var seq = new FailingEnumerable().AsTestingSequence(maxEnumerations: 2);
+
+		using var buffer = seq.Publish();
+
+		for (var i = 0; i < 2; i++)
+			_ = Assert.Throws<TestException>(() => buffer.First());
+
+		buffer.Reset();
+		buffer.AssertSequenceEqual(1);
+	}
+
+	private class FailingEnumerable : IEnumerable<int>
+	{
+		private bool _started;
+		public IEnumerator<int> GetEnumerator()
+		{
+			if (!_started)
+			{
+				_started = true;
+				throw new TestException();
+			}
+			return Enumerable.Range(1, 1).GetEnumerator();
+		}
+
+		IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+	}
+
+	[Fact]
+	public void PublishLambdaIsLazy()
+	{
+		_ = new BreakingSequence<int>().Publish(BreakingFunc.Of<IEnumerable<int>, IEnumerable<string>>());
+	}
+
+	[Fact]
+	public void PublishLambdaSimple()
+	{
+		using var seq = Enumerable.Range(1, 10).AsTestingSequence();
+
+		var result = seq.Publish(xs => xs.Zip(xs, (l, r) => l + r).Take(4));
+		result.AssertSequenceEqual(2, 4, 6, 8);
+	}
+}
