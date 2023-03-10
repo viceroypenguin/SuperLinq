167e84399292134fc988ec2313e5d4d3b51348c7
Stuart Turner <stuart@turner-isageek.com>
2023-03-10 10:08:27 -0600

    Remove `System.Interactive.Async` w/ placeholders

---
+++ Source/SuperLinq.Async/Do.cs
@@ -0,0 +1,77 @@
+﻿namespace SuperLinq.Async;
+
+public static partial class AsyncSuperEnumerable
+{
+	/// <summary>
+	/// Lazily invokes an action for each value in the sequence.
+	/// </summary>
+	/// <typeparam name="TSource">Source sequence element type.</typeparam>
+	/// <param name="source">Source sequence.</param>
+	/// <param name="onNext">Action to invoke for each element.</param>
+	/// <returns>Sequence exhibiting the specified side-effects upon enumeration.</returns>
+	/// <exception cref="ArgumentNullException"><paramref name="source"/> or <paramref name="onNext"/> is <see
+	/// langword="null"/>.</exception>
+	/// <remarks>
+	/// This method uses deferred execution and streams its results.
+	/// </remarks>
+	public static IAsyncEnumerable<TSource> Do<TSource>(this IAsyncEnumerable<TSource> source, Action<TSource> onNext)
+	{
+		throw new NotImplementedException();
+	}
+
+	/// <summary>
+	/// Lazily invokes an action for each value in the sequence, and executes an action for successful termination.
+	/// </summary>
+	/// <typeparam name="TSource">Source sequence element type.</typeparam>
+	/// <param name="source">Source sequence.</param>
+	/// <param name="onNext">Action to invoke for each element.</param>
+	/// <param name="onCompleted">Action to invoke on successful termination of the sequence.</param>
+	/// <returns>Sequence exhibiting the specified side-effects upon enumeration.</returns>
+	/// <exception cref="ArgumentNullException"><paramref name="source"/>, <paramref name="onNext"/>, or <paramref
+	/// name="onCompleted"/> is <see langword="null"/>.</exception>
+	/// <remarks>
+	/// This method uses deferred execution and streams its results.
+	/// </remarks>
+	public static IAsyncEnumerable<TSource> Do<TSource>(this IAsyncEnumerable<TSource> source, Action<TSource> onNext, Action onCompleted)
+	{
+		throw new NotImplementedException();
+	}
+
+	/// <summary>
+	/// Lazily invokes an action for each value in the sequence, and executes an action upon exceptional termination.
+	/// </summary>
+	/// <typeparam name="TSource">Source sequence element type.</typeparam>
+	/// <param name="source">Source sequence.</param>
+	/// <param name="onNext">Action to invoke for each element.</param>
+	/// <param name="onError">Action to invoke on exceptional termination of the sequence.</param>
+	/// <returns>Sequence exhibiting the specified side-effects upon enumeration.</returns>
+	/// <exception cref="ArgumentNullException"><paramref name="source"/>, <paramref name="onNext"/>, or <paramref
+	/// name="onError"/> is <see langword="null"/>.</exception>
+	/// <remarks>
+	/// This method uses deferred execution and streams its results.
+	/// </remarks>
+	public static IAsyncEnumerable<TSource> Do<TSource>(this IAsyncEnumerable<TSource> source, Action<TSource> onNext, Action<Exception> onError)
+	{
+		throw new NotImplementedException();
+	}
+
+	/// <summary>
+	/// Lazily invokes an action for each value in the sequence, and executes an action upon successful or exceptional
+	/// termination.
+	/// </summary>
+	/// <typeparam name="TSource">Source sequence element type.</typeparam>
+	/// <param name="source">Source sequence.</param>
+	/// <param name="onNext">Action to invoke for each element.</param>
+	/// <param name="onError">Action to invoke on exceptional termination of the sequence.</param>
+	/// <param name="onCompleted">Action to invoke on successful termination of the sequence.</param>
+	/// <returns>Sequence exhibiting the specified side-effects upon enumeration.</returns>
+	/// <exception cref="ArgumentNullException"><paramref name="source"/>, <paramref name="onNext"/> <paramref
+	/// name="onError"/>, or <paramref name="onCompleted"/> is <see langword="null"/>.</exception>
+	/// <remarks>
+	/// This method uses deferred execution and streams its results.
+	/// </remarks>
+	public static IAsyncEnumerable<TSource> Do<TSource>(this IAsyncEnumerable<TSource> source, Action<TSource> onNext, Action<Exception> onError, Action onCompleted)
+	{
+		throw new NotImplementedException();
+	}
+}
---
+++ Source/SuperLinq.Async/Repeat.cs
@@ -0,0 +1,43 @@
+﻿namespace SuperLinq.Async;
+
+public partial class AsyncSuperEnumerable
+{
+	/// <summary>
+	/// Generates a sequence by repeating the given value infinitely.
+	/// </summary>
+	/// <typeparam name="TResult">Result sequence element type.</typeparam>
+	/// <param name="value">Value to repeat in the resulting sequence.</param>
+	/// <returns>Sequence repeating the given value infinitely.</returns>
+	public static IAsyncEnumerable<TResult> Repeat<TResult>(TResult value)
+	{
+		throw new NotImplementedException();
+	}
+
+	/// <summary>
+	/// Repeats and concatenates the source sequence infinitely.
+	/// </summary>
+	/// <typeparam name="TSource">Source sequence element type.</typeparam>
+	/// <param name="source">Source sequence.</param>
+	/// <returns>Sequence obtained by concatenating the source sequence to itself infinitely.</returns>
+	/// <exception cref="ArgumentNullException"><paramref name="source"/> is <see langword="null"/>.</exception>
+	public static IAsyncEnumerable<TSource> Repeat<TSource>(this IAsyncEnumerable<TSource> source)
+	{
+		throw new NotImplementedException();
+	}
+
+	/// <summary>
+	/// Repeats and concatenates the source sequence the given number of times.
+	/// </summary>
+	/// <typeparam name="TSource">Source sequence element type.</typeparam>
+	/// <param name="source">Source sequence.</param>
+	/// <param name="count">Number of times to repeat the source sequence.</param>
+	/// <returns>Sequence obtained by concatenating the source sequence to itself the specified number of
+	/// times.</returns>
+	/// <exception cref="ArgumentNullException"><paramref name="source"/> is <see langword="null"/>.</exception>
+	/// <exception cref="ArgumentOutOfRangeException"><paramref name="count"/> is less than or equal to
+	/// <c>0</c>.</exception>
+	public static IAsyncEnumerable<TSource> Repeat<TSource>(this IAsyncEnumerable<TSource> source, int count)
+	{
+		throw new NotImplementedException();
+	}
+}
