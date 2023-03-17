2145bf7d601b3c5cc721fe9c9795d1540cdc0933
Stuart Turner <stuart@turner-isageek.com>
2023-03-17 17:13:41 -0500

    Add `If` operator

---
+++ Source/SuperLinq.Async/If.cs
@@ -0,0 +1,104 @@
+﻿namespace SuperLinq.Async;
+
+public static partial class AsyncSuperEnumerable
+{
+	/// <summary>
+	/// Returns an enumerable sequence if the evaluation result of the given condition is <see langword="true"/>,
+	/// otherwise returns an empty sequence.
+	/// </summary>
+	/// <typeparam name="TResult">Result sequence element type.</typeparam>
+	/// <param name="condition">Condition to evaluate.</param>
+	/// <param name="thenSource">Sequence to return in case the condition evaluates true.</param>
+	/// <returns>The given input sequence if the condition evaluates true; otherwise, an empty sequence.</returns>
+	/// <exception cref="ArgumentNullException"><paramref name="condition"/> or <paramref name="thenSource"/> is <see
+	/// langword="null"/>.</exception>
+	/// <remarks>
+	/// <para>
+	/// <paramref name="condition"/> is not evaluated until enumeration. If the value is <see langword="true"/>, then
+	/// <paramref name="thenSource"/> is enumerated. Otherwise, the sequence will be empty.
+	/// </para>
+	/// </remarks>
+	public static IAsyncEnumerable<TResult> If<TResult>(Func<bool> condition, IAsyncEnumerable<TResult> thenSource)
+	{
+		return If(condition.ToAsync(), thenSource, AsyncEnumerable.Empty<TResult>());
+	}
+
+	/// <summary>
+	/// Returns an enumerable sequence if the evaluation result of the given condition is <see langword="true"/>,
+	/// otherwise returns an empty sequence.
+	/// </summary>
+	/// <typeparam name="TResult">Result sequence element type.</typeparam>
+	/// <param name="condition">Condition to evaluate.</param>
+	/// <param name="thenSource">Sequence to return in case the condition evaluates true.</param>
+	/// <returns>The given input sequence if the condition evaluates true; otherwise, an empty sequence.</returns>
+	/// <exception cref="ArgumentNullException"><paramref name="condition"/> or <paramref name="thenSource"/> is <see
+	/// langword="null"/>.</exception>
+	/// <remarks>
+	/// <para>
+	/// <paramref name="condition"/> is not evaluated until enumeration. If the value is <see langword="true"/>, then
+	/// <paramref name="thenSource"/> is enumerated. Otherwise, the sequence will be empty.
+	/// </para>
+	/// </remarks>
+	public static IAsyncEnumerable<TResult> If<TResult>(Func<ValueTask<bool>> condition, IAsyncEnumerable<TResult> thenSource)
+	{
+		return If(condition, thenSource, AsyncEnumerable.Empty<TResult>());
+	}
+
+	/// <summary>
+	/// Returns an enumerable sequence based on the evaluation result of the given condition.
+	/// </summary>
+	/// <typeparam name="TResult">Result sequence element type.</typeparam>
+	/// <param name="condition">Condition to evaluate.</param>
+	/// <param name="thenSource">Sequence to return in case the condition evaluates <see langword="true"/>.</param>
+	/// <param name="elseSource">Sequence to return in case the condition evaluates <see langword="false"/>.</param>
+	/// <returns>Either of the two input sequences based on the result of evaluating the condition.</returns>
+	/// <exception cref="ArgumentNullException"><paramref name="condition"/>, <paramref name="thenSource"/>, or
+	/// <paramref name="elseSource"/> is <see langword="null"/>.</exception>
+	/// <remarks>
+	/// <para>
+	/// <paramref name="condition"/> is not evaluated until enumeration. If the value is <see langword="true"/>, then
+	/// <paramref name="thenSource"/> is enumerated. Otherwise, <paramref name="elseSource"/> will be enumerated.
+	/// </para>
+	/// </remarks>
+	public static IAsyncEnumerable<TResult> If<TResult>(
+		Func<bool> condition,
+		IAsyncEnumerable<TResult> thenSource,
+		IAsyncEnumerable<TResult> elseSource)
+	{
+		return If(condition.ToAsync(), thenSource, elseSource);
+	}
+
+	/// <summary>
+	/// Returns an enumerable sequence based on the evaluation result of the given condition.
+	/// </summary>
+	/// <typeparam name="TResult">Result sequence element type.</typeparam>
+	/// <param name="condition">Condition to evaluate.</param>
+	/// <param name="thenSource">Sequence to return in case the condition evaluates <see langword="true"/>.</param>
+	/// <param name="elseSource">Sequence to return in case the condition evaluates <see langword="false"/>.</param>
+	/// <returns>Either of the two input sequences based on the result of evaluating the condition.</returns>
+	/// <exception cref="ArgumentNullException"><paramref name="condition"/>, <paramref name="thenSource"/>, or
+	/// <paramref name="elseSource"/> is <see langword="null"/>.</exception>
+	/// <remarks>
+	/// <para>
+	/// <paramref name="condition"/> is not evaluated until enumeration. If the value is <see langword="true"/>, then
+	/// <paramref name="thenSource"/> is enumerated. Otherwise, <paramref name="elseSource"/> will be enumerated.
+	/// </para>
+	/// </remarks>
+	public static IAsyncEnumerable<TResult> If<TResult>(
+		Func<ValueTask<bool>> condition,
+		IAsyncEnumerable<TResult> thenSource,
+		IAsyncEnumerable<TResult> elseSource)
+	{
+		Guard.IsNotNull(condition);
+		Guard.IsNotNull(thenSource);
+		Guard.IsNotNull(elseSource);
+
+		return Case(
+			condition,
+			new Dictionary<bool, IAsyncEnumerable<TResult>>
+			{
+				[true] = thenSource,
+				[false] = elseSource,
+			});
+	}
+}
