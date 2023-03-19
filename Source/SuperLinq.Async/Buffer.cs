a1735a32312c8f530085fb707146bd889938d862
Stuart Turner <stuart@turner-isageek.com>
2023-03-19 16:45:57 -0500

    Add `Buffer` operator (#291)

--- Source/SuperLinq.Async/Buffer.cs
+++ Source/SuperLinq.Async/Buffer.cs
@@ -28,4 +28,57 @@ public static partial class AsyncSuperEnumerable
 	{
 		return Batch(source, count);
 	}
+
+	/// <summary>
+	/// Generates a sequence of buffers over the source sequence, with specified length and possible overlap.
+	/// </summary>
+	/// <typeparam name="TSource">Source sequence element type.</typeparam>
+	/// <param name="source">Source sequence.</param>
+	/// <param name="count">Number of elements for allocated buffers.</param>
+	/// <param name="skip">Number of elements to skip between the start of consecutive buffers.</param>
+	/// <returns>Sequence of buffers containing source sequence elements.</returns>
+	/// <exception cref="ArgumentNullException"><paramref name="source"/> is <see langword="null"/>.</exception>
+	/// <exception cref="ArgumentOutOfRangeException"><paramref name="count"/> or <paramref name="skip"/> is less than
+	/// or equal to <c>0</c>.</exception>
+	/// <remarks>
+	/// <para>
+	/// A chunk can contain fewer elements than <paramref name="count"/>, specifically the final buffers of <paramref
+	/// name="source"/>.
+	/// </para>
+	/// <para>
+	/// Returned subsequences are buffered, but the overall operation is streamed.<br/>
+	/// </para>
+	/// </remarks>
+	public static IAsyncEnumerable<IList<TSource>> Buffer<TSource>(this IAsyncEnumerable<TSource> source, int count, int skip)
+	{
+		Guard.IsNotNull(source);
+		Guard.IsGreaterThan(count, 0);
+		Guard.IsGreaterThan(skip, 0);
+
+		return Core(source, count, skip);
+
+		static async IAsyncEnumerable<IList<TSource>> Core(
+			IAsyncEnumerable<TSource> source,
+			int count, int skip,
+			[EnumeratorCancellation] CancellationToken cancellationToken = default)
+		{
+			var lists = new Queue<List<TSource>>();
+
+			var i = 0;
+			await foreach (var el in source.WithCancellation(cancellationToken).ConfigureAwait(false))
+			{
+				if (i++ % skip == 0)
+					lists.Enqueue(new());
+
+				foreach (var l in lists)
+					l.Add(el);
+
+				if (lists.Count > 0 && lists.Peek().Count == count)
+					yield return lists.Dequeue();
+			}
+
+			while (lists.Count > 0)
+				yield return lists.Dequeue();
+		}
+	}
 }
