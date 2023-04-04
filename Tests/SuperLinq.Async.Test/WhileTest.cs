0cb9b62a1379c0ae6a785c8f2e7e0d5abc3285fd
Stuart Turner <stuart@turner-isageek.com>
2023-04-04 13:31:10 -0500

    Add `While` operator

---
+++ Tests/SuperLinq.Async.Test/WhileTest.cs
@@ -0,0 +1,35 @@
+﻿namespace Test.Async;
+
+public class WhileTest
+{
+	[Fact]
+	public void WhileIsLazy()
+	{
+		_ = AsyncSuperEnumerable.While(
+				BreakingFunc.Of<bool>(),
+				new AsyncBreakingSequence<int>());
+		_ = AsyncSuperEnumerable.While(
+				BreakingFunc.Of<ValueTask<bool>>(),
+				new AsyncBreakingSequence<int>());
+	}
+
+	[Fact]
+	public async Task WhileBehavior()
+	{
+		var starts = 0;
+		var seq = AsyncSuperEnumerable.While(
+			() =>
+				starts++ switch
+				{
+					0 or 1 => true,
+					2 => false,
+					_ => throw new TestException(),
+				},
+			AsyncEnumerable.Range(1, 10));
+
+		Assert.Equal(0, starts);
+		await seq.AssertSequenceEqual(
+			Enumerable.Range(1, 10).Concat(Enumerable.Range(1, 10)));
+		Assert.Equal(3, starts);
+	}
+}
