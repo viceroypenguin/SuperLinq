e954d9b92af0197c1aa19ab37aff778cadf254d7
Stuart Turner <stuart@turner-isageek.com>
2023-04-05 10:52:57 -0500

    Add `DoWhile` operator (#328)

---
+++ Tests/SuperLinq.Async.Test/DoWhileTest.cs
@@ -0,0 +1,34 @@
+﻿namespace Test.Async;
+
+public class DoWhileTest
+{
+	[Fact]
+	public void DoWhileIsLazy()
+	{
+		_ = new AsyncBreakingSequence<int>().DoWhile(BreakingFunc.Of<bool>());
+	}
+
+	[Fact]
+	public async Task DoWhileBehavior()
+	{
+		using var ts = Enumerable.Range(1, 10).AsTestingSequence();
+
+		var starts = 0;
+		var seq = ts
+			.DoWhile(
+				() =>
+					starts++ switch
+					{
+						0 or 1 => true,
+						2 => false,
+						_ => throw new TestException(),
+					});
+
+		Assert.Equal(0, starts);
+		await seq.AssertSequenceEqual(
+			Enumerable.Range(1, 10)
+				.Concat(Enumerable.Range(1, 10))
+				.Concat(Enumerable.Range(1, 10)));
+		Assert.Equal(3, starts);
+	}
+}
