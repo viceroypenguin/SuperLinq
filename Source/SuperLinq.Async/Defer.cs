58f44638d7cf3e46ce9dfa38ad8cf54807dacc58
Stuart Turner <stuart@turner-isageek.com>
2023-04-14 10:07:24 -0500

    Add `Defer` operator

---
+++ Source/SuperLinq.Async/Defer.cs
@@ -0,0 +1,27 @@
+﻿namespace SuperLinq.Async;
+
+public static partial class AsyncSuperEnumerable
+{
+	/// <summary>
+	/// Creates an enumerable sequence based on an enumerable factory function.
+	/// </summary>
+	/// <typeparam name="TResult">Result sequence element type.</typeparam>
+	/// <param name="enumerableFactory">Enumerable factory function.</param>
+	/// <returns>Sequence that will invoke the enumerable factory upon iteration.</returns>
+	/// <exception cref="ArgumentNullException"><paramref name="enumerableFactory"/> is <see
+	/// langword="null"/>.</exception>
+	public static IAsyncEnumerable<TResult> Defer<TResult>(Func<IAsyncEnumerable<TResult>> enumerableFactory)
+	{
+		Guard.IsNotNull(enumerableFactory);
+
+		return Core(enumerableFactory);
+
+		static async IAsyncEnumerable<TResult> Core(
+			Func<IAsyncEnumerable<TResult>> enumerableFactory,
+			[EnumeratorCancellation] CancellationToken cancellationToken = default)
+		{
+			await foreach (var el in enumerableFactory().WithCancellation(cancellationToken).ConfigureAwait(false))
+				yield return el;
+		}
+	}
+}
