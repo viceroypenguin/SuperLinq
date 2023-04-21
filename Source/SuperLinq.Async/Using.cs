4b157c3d75437885557f4b0e9603cdb70a7a1fcb
Stuart Turner <stuart@turner-isageek.com>
2023-04-21 11:29:43 -0500

    Add `Using` operator

---
+++ Source/SuperLinq.Async/Using.cs
@@ -0,0 +1,51 @@
+﻿namespace SuperLinq.Async;
+
+public static partial class AsyncSuperEnumerable
+{
+	/// <summary>
+	/// Generates a sequence that's dependent on a resource object whose lifetime is determined by the sequence usage
+	/// duration.
+	/// </summary>
+	/// <typeparam name="TSource">Source element type.</typeparam>
+	/// <typeparam name="TResource">Resource type.</typeparam>
+	/// <param name="resourceFactory">Resource factory function.</param>
+	/// <param name="enumerableFactory">Enumerable factory function, having access to the obtained resource.</param>
+	/// <returns>Sequence whose use controls the lifetime of the associated obtained resource.</returns>
+	/// <exception cref="ArgumentNullException"><paramref name="resourceFactory"/> or <paramref
+	/// name="enumerableFactory"/> is <see langword="null"/>.</exception>
+	/// <remarks>
+	/// <para>
+	/// <paramref name="resourceFactory"/> and <paramref name="enumerableFactory"/> are evaluated lazily, once
+	/// enumeration has begun. The value returned by <paramref name="resourceFactory"/> will be disposed after the
+	/// enumeration has completed. 
+	/// </para>
+	/// <para>
+	/// The values returned by <paramref name="enumerableFactory"/> and <paramref name="enumerableFactory"/> are not
+	/// cached; multiple iterations of the <see cref="IAsyncEnumerable{T}"/> returned by this method will call these methods
+	/// separately for each iteration.
+	/// </para>
+	/// </remarks>
+	public static IAsyncEnumerable<TSource> Using<TSource, TResource>(
+		Func<TResource> resourceFactory,
+		Func<TResource, IAsyncEnumerable<TSource>> enumerableFactory)
+		where TResource : IAsyncDisposable
+	{
+		Guard.IsNotNull(resourceFactory);
+		Guard.IsNotNull(enumerableFactory);
+
+		return Core(resourceFactory, enumerableFactory);
+
+		static async IAsyncEnumerable<TSource> Core(
+			Func<TResource> resourceFactory,
+			Func<TResource, IAsyncEnumerable<TSource>> enumerableFactory,
+			[EnumeratorCancellation] CancellationToken cancellationToken = default)
+		{
+			var resource = resourceFactory();
+			await using (resource.ConfigureAwait(false))
+			{
+				await foreach (var item in enumerableFactory(resource).WithCancellation(cancellationToken).ConfigureAwait(false))
+					yield return item;
+			}
+		}
+	}
+}
---
+++ Tests/SuperLinq.Async.Test/UsingTest.cs
@@ -0,0 +1,91 @@
+﻿namespace Test.Async;
+
+public class UsingTest
+{
+	[Fact]
+	public void UsingIsLazy()
+	{
+		_ = AsyncSuperEnumerable.Using(
+			BreakingFunc.Of<IAsyncDisposable>(),
+			BreakingFunc.Of<IAsyncDisposable, IAsyncEnumerable<int>>());
+	}
+
+	[Fact]
+	public async Task UsingDisposesCorrectly()
+	{
+		var starts = 0;
+		IEnumerable<int> Test(TestDisposable disposable)
+		{
+			starts++;
+			Assert.False(disposable.IsDisposed);
+			yield return 1;
+			Assert.False(disposable.IsDisposed);
+			yield return 2;
+		}
+
+		TestDisposable? dis = null;
+		var seq = AsyncSuperEnumerable.Using(
+			() => dis = new TestDisposable(),
+			d => Test(d).ToAsyncEnumerable());
+		Assert.Null(dis);
+		Assert.Equal(0, starts);
+
+		await seq.Consume();
+		Assert.NotNull(dis);
+		Assert.True(dis.IsDisposed);
+		Assert.Equal(1, starts);
+	}
+
+	[Fact]
+	public async Task UsingDisposesOnIterationError()
+	{
+		var starts = 0;
+		IEnumerable<int> Test(TestDisposable disposable)
+		{
+			starts++;
+			Assert.False(disposable.IsDisposed);
+			yield return 1;
+			Assert.False(disposable.IsDisposed);
+			throw new TestException();
+		}
+
+		TestDisposable? dis = null;
+		var seq = AsyncSuperEnumerable.Using(
+			() => dis = new TestDisposable(),
+			d => Test(d).ToAsyncEnumerable());
+		Assert.Null(dis);
+		Assert.Equal(0, starts);
+
+		_ = await Assert.ThrowsAsync<TestException>(async () =>
+			await seq.Consume());
+		Assert.NotNull(dis);
+		Assert.True(dis.IsDisposed);
+		Assert.Equal(1, starts);
+	}
+
+
+	[Fact]
+	public async Task UsingDisposesOnFunctionError()
+	{
+		TestDisposable? dis = null;
+		var seq = AsyncSuperEnumerable.Using<int, TestDisposable>(
+			() => dis = new TestDisposable(),
+			_ => throw new TestException());
+		Assert.Null(dis);
+
+		_ = await Assert.ThrowsAsync<TestException>(async () =>
+			await seq.Consume());
+		Assert.NotNull(dis);
+		Assert.True(dis.IsDisposed);
+	}
+
+	private class TestDisposable : IAsyncDisposable
+	{
+		public bool IsDisposed { get; set; }
+		public ValueTask DisposeAsync()
+		{
+			IsDisposed = true;
+			return default(ValueTask);
+		}
+	}
+}
