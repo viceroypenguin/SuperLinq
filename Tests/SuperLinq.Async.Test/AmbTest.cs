1bb90446c0203be461a6248b74e8150a4c26c814
Stuart Turner <stuart@turner-isageek.com>
2023-04-26 16:21:54 -0500

    Better fix `Amb` tests

--- Tests/SuperLinq.Async.Test/AmbTest.cs
+++ Tests/SuperLinq.Async.Test/AmbTest.cs
@@ -12,12 +12,18 @@ public class AmbTest
 	}
 
 	[Theory]
-	[InlineData(1)]
-	[InlineData(2)]
-	[InlineData(3)]
-	public async Task AmbSyncReturnsFirst(int sequenceNumber)
+	[InlineData(1, false)]
+	[InlineData(2, false)]
+	[InlineData(3, false)]
+	[InlineData(1, true)]
+	[InlineData(2, true)]
+	[InlineData(3, true)]
+	public async Task AmbEmptyReturnsFirst(int sequenceNumber, bool asyncEmpty)
 	{
-		var sync = AsyncEnumerable.Range(1, 5);
+		var empty = AsyncEnumerable.Empty<int>();
+		await using var tsEmpty = empty.AsTestingSequence();
+		if (asyncEmpty) empty = tsEmpty;
+
 		var async = AsyncEnumerable.Range(6, 5)
 			.SelectAwaitWithCancellation(async (i, ct) =>
 			{
@@ -26,24 +32,24 @@ public class AmbTest
 			});
 
 		// will switch to using `TestingSequence` once I turn on sync/async versions of it
-		var seq1 = sequenceNumber == 1 ? sync : async;
-		var seq2 = sequenceNumber == 2 ? sync : async;
-		var seq3 = sequenceNumber == 3 ? sync : async;
+		var seq1 = sequenceNumber == 1 ? empty : async;
+		var seq2 = sequenceNumber == 2 ? empty : async;
+		var seq3 = sequenceNumber == 3 ? empty : async;
 
 		var ts = new[] { seq1, seq2, seq3, };
 
 		var result = ts.Amb();
 
-		await result.AssertSequenceEqual(Enumerable.Range(1, 5));
+		await result.AssertSequenceEqual();
 	}
 
 	[Theory]
 	[InlineData(1)]
 	[InlineData(2)]
 	[InlineData(3)]
-	public async Task AmbSyncEmptyReturnsFirst(int sequenceNumber)
+	public async Task AmbSyncReturnsFirst(int sequenceNumber)
 	{
-		var sync = AsyncEnumerable.Empty<int>();
+		var sync = AsyncEnumerable.Range(1, 5);
 		var async = AsyncEnumerable.Range(6, 5)
 			.SelectAwaitWithCancellation(async (i, ct) =>
 			{
@@ -90,50 +96,5 @@ public class AmbTest
 		var result = ts.Amb();
 
 		await result.AssertSequenceEqual(Enumerable.Range(1, 5));
-	}
-
-	[Theory]
-	[InlineData(1)]
-	[InlineData(2)]
-	[InlineData(3)]
-	public async Task AmbAsyncEmptyComesFirst(int sequenceNumber)
-	{
-		var shorter = EmptyAsyncIterator<int>.Instance;
-		var longer = AsyncEnumerable.Range(6, 5)
-			.SelectAwaitWithCancellation(async (i, ct) =>
-			{
-				await Task.Delay(250, ct);
-				return i;
-			});
-		await using var seq1 = (sequenceNumber == 1 ? shorter : longer).AsTestingSequence();
-		await using var seq2 = (sequenceNumber == 2 ? shorter : longer).AsTestingSequence();
-		await using var seq3 = (sequenceNumber == 3 ? shorter : longer).AsTestingSequence();
-
-		var ts = new[] { seq1, seq2, seq3, };
-
-		var result = ts.Amb();
-
-		await result.AssertSequenceEqual(Enumerable.Range(1, 5));
-	}
-
-	private sealed class EmptyAsyncIterator<TValue> : IAsyncEnumerable<TValue>, IAsyncEnumerator<TValue>
-	{
-		public static readonly EmptyAsyncIterator<TValue> Instance = new();
-
-		public TValue Current => default!;
-		public async ValueTask<bool> MoveNextAsync()
-		{
-			await Task.Yield();
-			return false;
-		}
-
-		public IAsyncEnumerator<TValue> GetAsyncEnumerator(CancellationToken cancellationToken)
-		{
-			cancellationToken.ThrowIfCancellationRequested(); // NB: [LDM-2018-11-28] Equivalent to async iterator behavior.
-
-			return this;
-		}
-
-		public ValueTask DisposeAsync() => default;
 	}
 }
