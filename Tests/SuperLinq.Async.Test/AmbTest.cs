8dbcb4dd0ef7d56c3b89d27ff5c14a295b0a5caa
Stuart Turner <stuart@turner-isageek.com>
2023-04-26 15:58:54 -0500

    Address `Amb` testing issues

--- Tests/SuperLinq.Async.Test/AmbTest.cs
+++ Tests/SuperLinq.Async.Test/AmbTest.cs
@@ -1,4 +1,6 @@
-﻿namespace Test.Async;
+﻿using System.Xml.Linq;
+
+namespace Test.Async;
 
 public class AmbTest
 {
@@ -22,9 +24,37 @@ public class AmbTest
 				await Task.Delay(250, ct);
 				return i;
 			});
-		await using var seq1 = (sequenceNumber == 1 ? sync : async).AsTestingSequence();
-		await using var seq2 = (sequenceNumber == 2 ? sync : async).AsTestingSequence();
-		await using var seq3 = (sequenceNumber == 3 ? sync : async).AsTestingSequence();
+
+		// will switch to using `TestingSequence` once I turn on sync/async versions of it
+		var seq1 = sequenceNumber == 1 ? sync : async;
+		var seq2 = sequenceNumber == 2 ? sync : async;
+		var seq3 = sequenceNumber == 3 ? sync : async;
+
+		var ts = new[] { seq1, seq2, seq3, };
+
+		var result = ts.Amb();
+
+		await result.AssertSequenceEqual(Enumerable.Range(1, 5));
+	}
+
+	[Theory]
+	[InlineData(1)]
+	[InlineData(2)]
+	[InlineData(3)]
+	public async Task AmbSyncEmptyReturnsFirst(int sequenceNumber)
+	{
+		var sync = AsyncEnumerable.Empty<int>();
+		var async = AsyncEnumerable.Range(6, 5)
+			.SelectAwaitWithCancellation(async (i, ct) =>
+			{
+				await Task.Delay(250, ct);
+				return i;
+			});
+
+		// will switch to using `TestingSequence` once I turn on sync/async versions of it
+		var seq1 = sequenceNumber == 1 ? sync : async;
+		var seq2 = sequenceNumber == 2 ? sync : async;
+		var seq3 = sequenceNumber == 3 ? sync : async;
 
 		var ts = new[] { seq1, seq2, seq3, };
 
@@ -60,5 +90,50 @@ public class AmbTest
 		var result = ts.Amb();
 
 		await result.AssertSequenceEqual(Enumerable.Range(1, 5));
+	}
+
+	[Theory]
+	[InlineData(1)]
+	[InlineData(2)]
+	[InlineData(3)]
+	public async Task AmbAsyncEmptyComesFirst(int sequenceNumber)
+	{
+		var shorter = EmptyAsyncIterator<int>.Instance;
+		var longer = AsyncEnumerable.Range(6, 5)
+			.SelectAwaitWithCancellation(async (i, ct) =>
+			{
+				await Task.Delay(250, ct);
+				return i;
+			});
+		await using var seq1 = (sequenceNumber == 1 ? shorter : longer).AsTestingSequence();
+		await using var seq2 = (sequenceNumber == 2 ? shorter : longer).AsTestingSequence();
+		await using var seq3 = (sequenceNumber == 3 ? shorter : longer).AsTestingSequence();
+
+		var ts = new[] { seq1, seq2, seq3, };
+
+		var result = ts.Amb();
+
+		await result.AssertSequenceEqual(Enumerable.Range(1, 5));
+	}
+
+	private sealed class EmptyAsyncIterator<TValue> : IAsyncEnumerable<TValue>, IAsyncEnumerator<TValue>
+	{
+		public static readonly EmptyAsyncIterator<TValue> Instance = new();
+
+		public TValue Current => default!;
+		public async ValueTask<bool> MoveNextAsync()
+		{
+			await Task.Yield();
+			return false;
+		}
+
+		public IAsyncEnumerator<TValue> GetAsyncEnumerator(CancellationToken cancellationToken)
+		{
+			cancellationToken.ThrowIfCancellationRequested(); // NB: [LDM-2018-11-28] Equivalent to async iterator behavior.
+
+			return this;
+		}
+
+		public ValueTask DisposeAsync() => default;
 	}
 }
