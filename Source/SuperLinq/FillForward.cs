7b9e1a5ffe2f6ee76e7c4bbe0ea5707f1fc9e44a
Stuart Turner <stuart@turner-isageek.com>
2023-05-04 21:38:01 -0500

    Correct nullability signature for `FillForward`

--- Source/SuperLinq/FillForward.cs
+++ Source/SuperLinq/FillForward.cs
@@ -18,10 +18,9 @@ public static partial class SuperEnumerable
 	/// results. If references or values are null at the start of the
 	/// sequence then they remain null.
 	/// </remarks>
-
-	public static IEnumerable<T> FillForward<T>(this IEnumerable<T> source)
+	public static IEnumerable<T?> FillForward<T>(this IEnumerable<T?> source)
 	{
-		return source.FillForward(e => e == null);
+		return source.FillForward(static e => e == null);
 	}
 
 	/// <summary>
@@ -42,7 +41,6 @@ public static partial class SuperEnumerable
 	/// results. If elements are missing at the start of the sequence then
 	/// they remain missing.
 	/// </remarks>
-
 	public static IEnumerable<T> FillForward<T>(this IEnumerable<T> source, Func<T, bool> predicate)
 	{
 		Guard.IsNotNull(source);
@@ -74,7 +72,6 @@ public static partial class SuperEnumerable
 	/// results. If elements are missing at the start of the sequence then
 	/// they remain missing.
 	/// </remarks>
-
 	public static IEnumerable<T> FillForward<T>(this IEnumerable<T> source, Func<T, bool> predicate, Func<T, T, T> fillSelector)
 	{
 		Guard.IsNotNull(source);
