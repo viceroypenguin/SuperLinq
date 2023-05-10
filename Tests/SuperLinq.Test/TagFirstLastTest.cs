dc1497baee822eed440c151c90afab7870252595
Stuart Turner <stuart@turner-isageek.com>
2023-05-10 11:45:15 -0500

    `TagFirstLast`: `IList<>` implementation

--- Tests/SuperLinq.Test/TagFirstLastTest.cs
+++ Tests/SuperLinq.Test/TagFirstLastTest.cs
@@ -9,44 +9,35 @@ public class TagFirstLastTest
 		_ = new BreakingSequence<object>().TagFirstLast(BreakingFunc.Of<object, bool, bool, object>());
 	}
 
-	[Fact]
-	public void TagFirstLastEmptySequence()
-	{
-		using var sequence = TestingSequence.Of<int>();
+	public static IEnumerable<object[]> GetSourceSequences() =>
+		Enumerable.Range(0, 4)
+			.SelectMany(n =>
+				Enumerable.Range(0, n)
+					.GetListSequences()
+					.Select(x => new object[] { x, n }));
 
-		var result = sequence.TagFirstLast();
-		result.AssertSequenceEqual();
+	[Theory]
+	[MemberData(nameof(GetSourceSequences))]
+	public void TagFirstLastVaryingLengths(IDisposableEnumerable<int> seq, int n)
+	{
+		using (seq)
+		{
+			var result = seq.TagFirstLast();
+			result.AssertSequenceEqual(
+				Enumerable.Range(0, n)
+					.Select(x => (x, x == 0, x == n - 1)));
+		}
 	}
 
 	[Fact]
-	public void TagFirstLastWithSourceSequenceOfOne()
+	public void TagFirstLastListBehavior()
 	{
-		using var sequence = TestingSequence.Of(123);
+		using var seq = Enumerable.Range(0, 10_000).AsBreakingList();
 
-		var result = sequence.TagFirstLast((item, isFirst, isLast) => new { Item = item, IsFirst = isFirst, IsLast = isLast });
-		result.AssertSequenceEqual(new { Item = 123, IsFirst = true, IsLast = true });
-	}
-
-	[Fact]
-	public void TagFirstLastWithSourceSequenceOfTwo()
-	{
-		using var sequence = TestingSequence.Of(123, 456);
-
-		var result = sequence.TagFirstLast((item, isFirst, isLast) => new { Item = item, IsFirst = isFirst, IsLast = isLast });
-		result.AssertSequenceEqual(
-			new { Item = 123, IsFirst = true, IsLast = false },
-			new { Item = 456, IsFirst = false, IsLast = true });
-	}
-
-	[Fact]
-	public void TagFirstLastWithSourceSequenceOfThree()
-	{
-		using var sequence = TestingSequence.Of(123, 456, 789);
-
-		var result = sequence.TagFirstLast((item, isFirst, isLast) => new { Item = item, IsFirst = isFirst, IsLast = isLast });
-		result.AssertSequenceEqual(
-			new { Item = 123, IsFirst = true, IsLast = false },
-			new { Item = 456, IsFirst = false, IsLast = false },
-			new { Item = 789, IsFirst = false, IsLast = true });
+		var result = seq.TagFirstLast();
+		Assert.Equal(10_000, result.Count());
+		Assert.Equal((0, true, false), result.ElementAt(0));
+		Assert.Equal((30, false, false), result.ElementAt(30));
+		Assert.Equal((9_999, false, true), result.ElementAt(^1));
 	}
 }
