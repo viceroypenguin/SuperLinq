626973102bf88cd401a734d829c853a6a2436c34
Stuart Turner <stuart@turner-isageek.com>
2023-05-21 09:41:12 -0500

    Build error

--- Tests/SuperLinq.Test/BatchTest.cs
+++ Tests/SuperLinq.Test/BatchTest.cs
@@ -138,7 +138,7 @@ public class BatchTest
 		using var seq = Enumerable.Range(0, 10_002).AsBreakingList();
 
 		var result = seq.Batch(20);
-		Assert.Equal(10_002 / 20 + 1, result.Count());
+		Assert.Equal((10_002 / 20) + 1, result.Count());
 		Assert.Equal(Enumerable.Range(1_000, 20), result.ElementAt(50));
 		Assert.Equal(Enumerable.Range(9_980, 20), result.ElementAt(^2));
 		Assert.Equal(Enumerable.Range(10_000, 2), result.ElementAt(^1));
