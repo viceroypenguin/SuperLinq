5aabe4fdd19efdb67391f6314ecffe56d206bc4b
Stuart Turner <stuart@turner-isageek.com>
2023-05-25 06:36:38 -0500

    `AssertCount`: `IList<>` implementation (#459)

--- Source/SuperLinq/AssertCount.cs
+++ Source/SuperLinq/AssertCount.cs
@@ -24,28 +24,105 @@ public static partial class SuperEnumerable
 		Guard.IsNotNull(source);
 		Guard.IsGreaterThanOrEqualTo(count, 0);
 
+		if (source is IList<TSource> list)
+			return new AssertCountListIterator<TSource>(list, count);
+
+		if (source.TryGetCollectionCount() is int)
+			return new AssertCountCollectionIterator<TSource>(source, count);
+
 		return Core(source, count);
 
 		static IEnumerable<TSource> Core(IEnumerable<TSource> source, int count)
 		{
-			if (source.TryGetCollectionCount() is int c)
+			var c = 0;
+			foreach (var item in source)
 			{
-				Guard.IsEqualTo(c, count, $"{nameof(source)}.Count()");
+				if (++c > count)
+					break;
+				yield return item;
+			}
+			Guard.IsEqualTo(c, count, $"{nameof(source)}.Count()");
+		}
+	}
 
-				foreach (var item in source)
-					yield return item;
-			}
-			else
+	private class AssertCountCollectionIterator<T> : CollectionIterator<T>
+	{
+		private readonly IEnumerable<T> _source;
+		private readonly int _count;
+
+		public AssertCountCollectionIterator(IEnumerable<T> source, int count)
+		{
+			_source = source;
+			_count = count;
+		}
+
+		public override int Count
+		{
+			get
 			{
-				c = 0;
-				foreach (var item in source)
-				{
-					if (++c > count)
-						break;
-					yield return item;
-				}
-				Guard.IsEqualTo(c, count, $"{nameof(source)}.Count()");
+				Guard.IsEqualTo(_source.GetCollectionCount(), _count, "source.Count()");
+				return _count;
 			}
+		}
+
+		protected override IEnumerable<T> GetEnumerable()
+		{
+			Guard.IsEqualTo(_source.GetCollectionCount(), _count, "source.Count()");
+
+			foreach (var item in _source)
+				yield return item;
+		}
+
+		public override void CopyTo(T[] array, int arrayIndex)
+		{
+			Guard.IsNotNull(array);
+			Guard.IsBetweenOrEqualTo(arrayIndex, 0, Count - 1);
+
+			_ = _source.CopyTo(array, arrayIndex);
+		}
+	}
+
+	private class AssertCountListIterator<T> : ListIterator<T>
+	{
+		private readonly IList<T> _source;
+		private readonly int _count;
+
+		public AssertCountListIterator(IList<T> source, int count)
+		{
+			_source = source;
+			_count = count;
+		}
+
+		public override int Count
+		{
+			get
+			{
+				Guard.IsEqualTo(_source.Count, _count, "source.Count()");
+				return _count;
+			}
+		}
+
+		protected override IEnumerable<T> GetEnumerable()
+		{
+			var cnt = (uint)Count;
+			for (var i = 0; i < cnt; i++)
+			{
+				yield return _source[i];
+			}
+		}
+
+		public override void CopyTo(T[] array, int arrayIndex)
+		{
+			Guard.IsNotNull(array);
+			Guard.IsBetweenOrEqualTo(arrayIndex, 0, Count - 1);
+
+			_source.CopyTo(array, arrayIndex);
+		}
+
+		protected override T ElementAt(int index)
+		{
+			Guard.IsBetweenOrEqualTo(index, 0, Count - 1);
+			return _source[index];
 		}
 	}
 }
--- Tests/SuperLinq.Test/AssertCountTest.cs
+++ Tests/SuperLinq.Test/AssertCountTest.cs
@@ -15,20 +15,9 @@ public class AssertCountTest
 			() => new BreakingSequence<int>().AssertCount(-1));
 	}
 
-	[Fact]
-	public void AssertCountSequenceWithMatchingLength()
-	{
-		var data = new[] { "foo", "bar", "baz" };
-		data.AssertCount(3).Consume();
-		using (var xs = data.AsTestingSequence())
-			xs.AssertCount(3).Consume();
-		using (var xs = data.AsTestingCollection())
-			xs.AssertCount(3).Consume();
-	}
-
 	public static IEnumerable<object[]> GetSequences() =>
 		Enumerable.Range(1, 10)
-			.GetBreakingCollectionSequences()
+			.GetAllSequences()
 			.Select(x => new object[] { x });
 
 	[Theory]
@@ -37,8 +26,22 @@ public class AssertCountTest
 	{
 		using (seq)
 		{
+			var result = seq.AssertCount(11);
 			_ = Assert.Throws<ArgumentException>("source.Count()",
-				() => seq.AssertCount(11).Consume());
+				() => result.Consume());
+		}
+	}
+
+	[Theory]
+	[MemberData(nameof(GetSequences))]
+	public void AssertCountEqualSequence(IDisposableEnumerable<int> seq)
+	{
+		using (seq)
+		{
+			var result = seq.AssertCount(10);
+			result.AssertSequenceEqual(
+				Enumerable.Range(1, 10),
+				testCollectionEnumerable: true);
 		}
 	}
 
@@ -48,9 +51,36 @@ public class AssertCountTest
 	{
 		using (seq)
 		{
+			var result = seq.AssertCount(9);
 			_ = Assert.Throws<ArgumentException>("source.Count()",
-				() => seq.AssertCount(9).Consume());
+				() => result.Consume());
 		}
+	}
+
+	[Fact]
+	public void AssertCountCollectionBehavior()
+	{
+		using var seq = Enumerable.Range(0, 10_000).AsBreakingCollection();
+
+		var result = seq.AssertCount(10_000);
+		Assert.Equal(10_000, result.Count());
+		result.AssertSequenceEqual(Enumerable.Range(0, 10_000));
+	}
+
+	[Fact]
+	public void AssertCountListBehavior()
+	{
+		using var seq = Enumerable.Range(0, 10_000).AsBreakingList();
+
+		var result = seq.AssertCount(10_000);
+		Assert.Equal(10_000, result.Count());
+		Assert.Equal(200, result.ElementAt(200));
+		Assert.Equal(1_200, result.ElementAt(1_200));
+		Assert.Equal(8_800, result.ElementAt(^1_200));
+
+		_ = Assert.Throws<ArgumentOutOfRangeException>(
+			"index",
+			() => result.ElementAt(40_001));
 	}
 
 	[Fact]
@@ -58,6 +88,8 @@ public class AssertCountTest
 	{
 		var stack = new Stack<int>(Enumerable.Range(1, 3));
 		var result = stack.AssertCount(4);
+		_ = Assert.Throws<ArgumentException>("source.Count()",
+			() => result.Consume());
 		stack.Push(4);
 		result.Consume();
 	}
