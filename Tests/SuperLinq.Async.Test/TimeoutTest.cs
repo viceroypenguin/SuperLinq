812b04028eee207ecef2907948172a4e1bdc60fd
Julien Aspirot <julien.aspirot@usherbrooke.ca>
2023-05-27 21:58:05 -0400

    added test coverage for cases where the task is not via ThrowIfCancellationRequested()

--- Tests/SuperLinq.Async.Test/TimeoutTest.cs
+++ Tests/SuperLinq.Async.Test/TimeoutTest.cs
@@ -57,4 +57,60 @@ public class TimeoutTest
 		_ = await Assert.ThrowsAsync<TimeoutException>(
 			async () => await result.Consume());
 	}
+
+	[Fact]
+	public async Task TimeoutExceptionWithOperationCanceledExceptionInnerException()
+	{
+		var sequence = new SequenceWithoutThrowIfCancellationRequested();
+
+		var result = sequence.Timeout(TimeSpan.FromMilliseconds(0));
+
+		var timeoutException = await Assert.ThrowsAsync<TimeoutException>(
+			async () => await result.Consume());
+
+		Assert.Null(timeoutException.InnerException);
+	}
+
+	[Fact]
+	public async Task TimeoutExceptionWithoutOperationCanceledExceptionInnerException()
+	{
+		var sequence = new SequenceWithThrowIfCancellationRequested();
+
+		var result = sequence.Timeout(TimeSpan.FromMilliseconds(0));
+
+		var timeoutException = await Assert.ThrowsAsync<TimeoutException>(
+			async () => await result.Consume());
+
+		_ = Assert.IsAssignableFrom<OperationCanceledException>(timeoutException.InnerException);
+	}
+
+	private class SequenceWithoutThrowIfCancellationRequested : IAsyncEnumerable<int>
+	{
+		public IAsyncEnumerator<int> GetAsyncEnumerator(CancellationToken cancellationToken = new())
+		{
+			// cancellationToken.ThrowIfCancellationRequested() is purposefully not done here
+
+			return AsyncEnumerable.Range(1, 5)
+				.SelectAwait(async x =>
+				{
+					await Task.Delay(TimeSpan.FromMilliseconds(1), CancellationToken.None);
+					return x;
+				}).GetAsyncEnumerator(CancellationToken.None);
+		}
+	}
+
+	private class SequenceWithThrowIfCancellationRequested : IAsyncEnumerable<int>
+	{
+		public IAsyncEnumerator<int> GetAsyncEnumerator(CancellationToken cancellationToken = new())
+		{
+			cancellationToken.ThrowIfCancellationRequested();
+
+			return AsyncEnumerable.Range(1, 5)
+				.SelectAwait(async x =>
+				{
+					await Task.Delay(TimeSpan.FromMilliseconds(1), cancellationToken);
+					return x;
+				}).GetAsyncEnumerator(cancellationToken);
+		}
+	}
 }
