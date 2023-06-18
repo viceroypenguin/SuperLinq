0938b16488752818dfd2fb076fb4fe9fd28f9a47
Stuart Turner <stuart@turner-isageek.com>
2023-06-17 19:57:18 -0500

    Address Test Comments

--- Tests/SuperLinq.Test/FallbackIfEmptyTest.cs
+++ Tests/SuperLinq.Test/FallbackIfEmptyTest.cs
@@ -5,60 +5,60 @@ public class FallbackIfEmptyTest
 	[Fact]
 	public void FallbackIfEmptyWithEmptySequence()
 	{
-		using var source = Enumerable.Empty<int>().AsTestingSequence(maxEnumerations: 2);
+		using var source = Seq<int>().AsTestingSequence();
 		source.FallbackIfEmpty(12).AssertSequenceEqual(12);
 	}
 
 	[Fact]
 	public void FallbackIfEmptyWithCollectionSequence()
 	{
-		using var source = Enumerable.Empty<int>().AsTestingCollection();
+		using var source = Seq<int>().AsTestingCollection();
 		source.FallbackIfEmpty(12).AssertSequenceEqual(12);
 	}
 
 	[Fact]
 	public void FallbackIfEmptyWithNotEmptySequence()
 	{
-		using var source = Seq(1).AsTestingSequence(maxEnumerations: 2);
+		using var source = Seq(1).AsTestingSequence();
 		source.FallbackIfEmpty(new BreakingSequence<int>()).AssertSequenceEqual(1);
 	}
 
 	[Fact]
 	public void FallbackIfEmptyWithNotEmptyCollectionSequence()
 	{
-		using var source = Seq(1).AsTestingCollection(maxEnumerations: 2);
+		using var source = Seq(1).AsTestingCollection();
 		source.FallbackIfEmpty(new BreakingSequence<int>()).AssertSequenceEqual(1);
 	}
 
 	[Fact]
 	public void AssertFallbackIfEmptyCollectionBehaviorOnEmptyCollection()
 	{
-		using var source = Enumerable.Empty<int>().AsTestingCollection(maxEnumerations: 2);
+		using var source = Seq<int>().AsBreakingCollection();
+		using var fallback = Enumerable.Range(0, 10_000).AsBreakingCollection();
 
-		var result = source.FallbackIfEmpty(12);
-		result.AssertCollectionErrorChecking(1);
+		var result = source.FallbackIfEmpty(fallback);
+		result.AssertCollectionErrorChecking(10_000);
 	}
 
 	[Fact]
 	public void AssertFallbackIfEmptyCollectionBehaviorOnNonEmptyCollection()
 	{
-		using var source = Seq(1).AsTestingCollection(maxEnumerations: 2);
-
+		using var source = Enumerable.Range(0, 10_000).AsBreakingCollection();
 		using var fallback = new BreakingCollection<int>();
+
 		var result = source.FallbackIfEmpty(fallback);
-		result.AssertCollectionErrorChecking(1);
+		result.AssertCollectionErrorChecking(10_000);
 	}
 
 	[Fact]
 	public void FallbackIfEmptyUsesCollectionCountAtIterationTime()
 	{
-		var stack = new Stack<int>(Enumerable.Range(1, 3));
+		var stack = new Stack<int>();
+
 		var result = stack.FallbackIfEmpty(4);
+		result.AssertSequenceEqual(4);
 
-		result.Consume();
-
-		stack.Push(4);
-
-		result.Consume();
+		stack.Push(1);
+		result.AssertSequenceEqual(1);
 	}
 }
