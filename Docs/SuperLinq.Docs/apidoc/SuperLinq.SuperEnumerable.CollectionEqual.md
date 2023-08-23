c0cb16e08ee3d87038a3638ec3ec71c469c434db
Stuart Turner <stuart@turner-isageek.com>
2023-08-23 08:32:10 -0500

    Update documentation for `CollectionEqual`

---
+++ Docs/SuperLinq.Docs/apidoc/SuperLinq.SuperEnumerable.CollectionEqual.md
@@ -0,0 +1,14 @@
+﻿---
+uid: SuperLinq.SuperEnumerable.CollectionEqual``1(System.Collections.Generic.IEnumerable{``0},System.Collections.Generic.IEnumerable{``0})
+example: [*content]
+---
+The following code example demonstrates how to compare two collections using the `CollectionEqual` operator.
+[!code-csharp[](SuperLinq/CollectionEqual/CollectionEqual1.linq#L6-)]
+
+---
+uid: SuperLinq.SuperEnumerable.CollectionEqual``1(System.Collections.Generic.IEnumerable{``0},System.Collections.Generic.IEnumerable{``0},System.Collections.Generic.IEqualityComparer{``0})
+example: [*content]
+---
+The following code example demonstrates how to compare two collections using the `CollectionEqual` operator.
+[!code-csharp[](SuperLinq/CollectionEqual/CollectionEqual2.linq#L6-)]
+
--- Source/SuperLinq/CollectionEqual.cs
+++ Source/SuperLinq/CollectionEqual.cs
@@ -3,30 +3,30 @@
 public static partial class SuperEnumerable
 {
 	/// <summary>
-	/// Determines whether two collections are equal by comparing the elements by using
-	/// the default equality comparer for their type.
+	///	    Determines whether two collections are equal by comparing the elements by using the default equality
+	///     comparer for their type.
 	/// </summary>
 	/// <typeparam name="TSource">
-	/// The type of the elements of the input sequences.</typeparam>
+	///	    The type of the elements of the input sequences.</typeparam>
 	/// <param name="first">
-	/// An <see cref="IEnumerable{T}"/> to compare to <paramref name="second"/>.
+	///	    An <see cref="IEnumerable{T}"/> to compare to <paramref name="second"/>.
 	/// </param>
 	/// <param name="second">
-	/// An <see cref="IEnumerable{T}"/> to compare to the <paramref name="first"/> sequence.
+	///	    An <see cref="IEnumerable{T}"/> to compare to the <paramref name="first"/> sequence.
 	/// </param>
 	/// <returns>
-	/// <see langword="true"/> if the two source sequences are of equal length and their corresponding
-	/// elements are equal according to the default equality comparer for their type;
-	/// otherwise, <see langword="false"/>.
+	///	    <see langword="true"/> if the two source sequences are of equal length and their corresponding elements are
+	///     equal according to the default equality comparer for their type; otherwise, <see langword="false"/>.
 	/// </returns>
-	/// <exception cref="ArgumentNullException"><paramref name="first"/> is null.</exception>
-	/// <exception cref="ArgumentNullException"><paramref name="second"/> is null.</exception>
+	/// <exception cref="ArgumentNullException">
+	///	    <paramref name="first"/> or <paramref name="second"/> is <see langword="null"/>.
+	///	</exception>
 	/// <remarks>
 	/// <para>
-	/// This method uses the default equality comparer for <typeparamref name="TSource"/>, <see cref="EqualityComparer{T}.Default"/>, 
-	/// determine whether two sequences have the same collection of elements.
-	/// A collection may have more than one of the same element, so this method
-	/// will compare the value and count of each element between both sequences.
+	///	    This method uses the default equality comparer for <typeparamref name="TSource"/>, <see
+	///     cref="EqualityComparer{T}.Default"/>, determine whether two sequences have the same collection of elements.
+	///     A collection may have more than one of the same element, so this method will compare the value and count of
+	///     each element between both sequences.
 	/// </para>
 	/// <para>
 	/// This method executes immediately.
@@ -40,38 +40,37 @@ public static partial class SuperEnumerable
 	}
 
 	/// <summary>
-	/// Determines whether two collections are equal by comparing the elements by using
-	/// a specified <see cref="IEqualityComparer{T}"/>.
+	///	    Determines whether two collections are equal by comparing the elements by using a specified <see
+	///     cref="IEqualityComparer{T}"/>.
 	/// </summary>
 	/// <typeparam name="TSource">
-	/// The type of the elements of the input sequences.</typeparam>
+	///	    The type of the elements of the input sequences.</typeparam>
 	/// <param name="first">
-	/// An <see cref="IEnumerable{T}"/> to compare to <paramref name="second"/>.
+	///	    An <see cref="IEnumerable{T}"/> to compare to <paramref name="second"/>.
 	/// </param>
 	/// <param name="second">
-	/// An <see cref="IEnumerable{T}"/> to compare to the <paramref name="first"/> sequence.
+	///	    An <see cref="IEnumerable{T}"/> to compare to the <paramref name="first"/> sequence.
 	/// </param>
 	/// <param name="comparer">
-	/// An <see cref="IEqualityComparer{T}"/> to use to compare elements.
+	///	    An <see cref="IEqualityComparer{T}"/> to use to compare elements.
 	/// </param>
 	/// <returns>
-	/// <see langword="true"/> if the two source sequences are of equal length and their corresponding
-	/// elements are equal according to the default equality comparer for their type;
-	/// otherwise, <see langword="false"/>.
+	///	    <see langword="true"/> if the two source sequences are of equal length and their corresponding elements are
+	///     equal according to the default equality comparer for their type; otherwise, <see langword="false"/>.
 	/// </returns>
-	/// <exception cref="ArgumentNullException"><paramref name="first"/> is null.</exception>
-	/// <exception cref="ArgumentNullException"><paramref name="second"/> is null.</exception>
+	/// <exception cref="ArgumentNullException">
+	///	    <paramref name="first"/> or <paramref name="second"/> is <see langword="null"/>.
+	///	</exception>
 	/// <remarks>
 	/// <para>
-	/// This method uses the provided equality comparer for <typeparamref name="TSource"/> to
-	/// determine whether two sequences have the same collection of elements.
-	/// A collection may have more than one of the same element, so this method
-	/// will compare the value and count of each element between both sequences.
-	/// If <paramref name="comparer"/> is <see langword="null"/>, the default equality comparer,
-	/// <see cref="EqualityComparer{T}.Default"/>, is used.
+	///	    This method uses the provided equality comparer for <typeparamref name="TSource"/> to determine whether two
+	///     sequences have the same collection of elements. A collection may have more than one of the same element, so
+	///     this method will compare the value and count of each element between both sequences. If <paramref
+	///     name="comparer"/> is <see langword="null"/>, the default equality comparer, <see
+	///     cref="EqualityComparer{T}.Default"/>, is used.
 	/// </para>
 	/// <para>
-	/// This method executes immediately.
+	///	    This method executes immediately.
 	/// </para>
 	/// </remarks>
 	public static bool CollectionEqual<TSource>(
