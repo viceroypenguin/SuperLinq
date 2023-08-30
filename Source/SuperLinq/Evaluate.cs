dc80dc0eef4b5dd958b0e48da73892a6a4ea451b
Stuart Turner <stuart@turner-isageek.com>
2023-08-30 08:25:59 -0500

    Update documentation for `Evaluate`

--- Source/SuperLinq/Evaluate.cs
+++ Source/SuperLinq/Evaluate.cs
@@ -3,18 +3,25 @@
 public partial class SuperEnumerable
 {
 	/// <summary>
-	/// Returns a sequence containing the values resulting from invoking (in order) each function in the source sequence of functions.
+	///	    Returns a sequence containing the values resulting from invoking (in order) each function in the source
+	///     sequence of functions.
 	/// </summary>
+	/// <typeparam name="T">
+	///	    The type of the object returned by the functions.
+	/// </typeparam>
+	/// <param name="functions">
+	///	    The functions to evaluate.
+	/// </param>
+	/// <returns>
+	///	    A sequence with results from invoking each function in the <paramref name="functions"/> sequence.
+	/// </returns>
+	/// <exception cref="ArgumentNullException">
+	///	    <paramref name="functions"/> is <see langword="null"/>.
+	/// </exception>
 	/// <remarks>
-	/// This operator uses deferred execution and streams the results.
-	/// If the resulting sequence is enumerated multiple times, the functions will be
-	/// evaluated multiple times too.
+	///	    This operator uses deferred execution and streams the results. If the resulting sequence is enumerated
+	///     multiple times, the functions will be evaluated multiple times too.
 	/// </remarks>
-	/// <typeparam name="T">The type of the object returned by the functions.</typeparam>
-	/// <param name="functions">The functions to evaluate.</param>
-	/// <returns>A sequence with results from invoking <paramref name="functions"/>.</returns>
-	/// <exception cref="ArgumentNullException">When <paramref name="functions"/> is <see langword="null"/>.</exception>
-
 	public static IEnumerable<T> Evaluate<T>(this IEnumerable<Func<T>> functions)
 	{
 		Guard.IsNotNull(functions);
