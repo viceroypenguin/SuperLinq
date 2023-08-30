eda7e8692c5e1b3233a14fcb333b8706ffad6d04
Stuart Turner <stuart@turner-isageek.com>
2023-08-30 07:06:32 -0500

    Update documentation for `ElementAt`

--- Source/SuperLinq/ElementAt.cs
+++ Source/SuperLinq/ElementAt.cs
@@ -7,22 +7,46 @@ namespace SuperLinq;
 
 public static partial class SuperEnumerable
 {
-	/// <summary>Returns the element at a specified index in a sequence.</summary>
-	/// <typeparam name="TSource">The type of the elements of <paramref name="source" />.</typeparam>
-	/// <param name="source">An <see cref="IEnumerable{T}" /> to return an element from.</param>
-	/// <param name="index">The index of the element to retrieve, which is either from the start or the end.</param>
-	/// <exception cref="ArgumentNullException"><paramref name="source" /> is <see langword="null" />.</exception>
-	/// <exception cref="ArgumentOutOfRangeException"><paramref name="index" /> is outside the bounds of the <paramref name="source" /> sequence.</exception>
-	/// <returns>The element at the specified position in the <paramref name="source" /> sequence.</returns>
+	/// <summary>
+	///	    Returns the element at a specified index in a sequence.
+	/// </summary>
+	/// <typeparam name="TSource">
+	///	    The type of the elements of <paramref name="source" />.
+	/// </typeparam>
+	/// <param name="source">
+	///	    An <see cref="IEnumerable{T}" /> to return an element from.
+	/// </param>
+	/// <param name="index">
+	///	    The index of the element to retrieve, which is either from the start or the end.
+	/// </param>
+	/// <exception cref="ArgumentNullException">
+	///	    <paramref name="source" /> is <see langword="null" />.
+	/// </exception>
+	/// <exception cref="ArgumentOutOfRangeException">
+	///	    <paramref name="index" /> is outside the bounds of the <paramref name="source" /> sequence.
+	/// </exception>
+	/// <returns>
+	///	    The element at the specified position in the <paramref name="source" /> sequence.
+	/// </returns>
 	/// <remarks>
-	/// <para>If the type of <paramref name="source" /> implements <see cref="IList{T}" />, that implementation is used to obtain the element at the specified index. Otherwise, this method obtains the specified element.</para>
-	/// <para>This method throws an exception if <paramref name="index" /> is out of range. To instead return a default value when the specified index is out of range, use the <see cref="SuperEnumerable.ElementAtOrDefault" /> method.</para>
+	/// <para>
+	///	    If the type of <paramref name="source" /> implements <see cref="IList{T}" />, that implementation is used to
+	///     obtain the element at the specified index. Otherwise, this method obtains the specified element.
+	/// </para>
+	/// <para>
+	///	    This method throws an exception if <paramref name="index" /> is out of range. To instead return a default
+	///     value when the specified index is out of range, use the <see cref="ElementAtOrDefault" /> method.
+	/// </para>
+	/// <para>
+	///		This operator is implemented in the bcl as of net6. Source and binary compatibility should be retained
+	///		across net versions, but this method should be inaccessible in net6+.
+	/// </para>
 	/// </remarks>
-#if !NET6_0_OR_GREATER
-	public static TSource ElementAt<TSource>(this IEnumerable<TSource> source, Index index)
-#else
+#if NET6_0_OR_GREATER
 	[Obsolete("This method has been implemented by the framework.")]
 	public static TSource ElementAt<TSource>(IEnumerable<TSource> source, Index index)
+#else
+	public static TSource ElementAt<TSource>(this IEnumerable<TSource> source, Index index)
 #endif
 	{
 		Guard.IsNotNull(source);
@@ -45,21 +69,40 @@ public static partial class SuperEnumerable
 		return element;
 	}
 
-	/// <summary>Returns the element at a specified index in a sequence or a default value if the index is out of range.</summary>
-	/// <typeparam name="TSource">The type of the elements of <paramref name="source" />.</typeparam>
-	/// <param name="source">An <see cref="IEnumerable{T}" /> to return an element from.</param>
-	/// <param name="index">The index of the element to retrieve, which is either from the start or the end.</param>
-	/// <exception cref="ArgumentNullException"><paramref name="source" /> is <see langword="null" />.</exception>
-	/// <returns><see langword="default" /> if <paramref name="index" /> is outside the bounds of the <paramref name="source" /> sequence; otherwise, the element at the specified position in the <paramref name="source" /> sequence.</returns>
+	/// <summary>
+	///	    Returns the element at a specified index in a sequence or a default value if the index is out of range.
+	/// </summary>
+	/// <typeparam name="TSource">
+	///	    The type of the elements of <paramref name="source" />.
+	/// </typeparam>
+	/// <param name="source">
+	///	    An <see cref="IEnumerable{T}" /> to return an element from.
+	/// </param>
+	/// <param name="index">
+	///	    The index of the element to retrieve, which is either from the start or the end.
+	/// </param>
+	/// <exception cref="ArgumentNullException">
+	///	    <paramref name="source" /> is <see langword="null" />.
+	/// </exception>
+	/// <returns>
+	///	    <see langword="default" /> if <paramref name="index" /> is outside the bounds of the <paramref name="source"
+	///     /> sequence; otherwise, the element at the specified position in the <paramref name="source" /> sequence.
+	/// </returns>
 	/// <remarks>
-	/// <para>If the type of <paramref name="source" /> implements <see cref="IList{T}" />, that implementation is used to obtain the element at the specified index. Otherwise, this method obtains the specified element.</para>
-	/// <para>The default value for reference and nullable types is <see langword="null" />.</para>
+	/// <para>
+	///	    If the type of <paramref name="source" /> implements <see cref="IList{T}" />, that implementation is used to
+	///     obtain the element at the specified index. Otherwise, this method obtains the specified element.
+	/// </para>
+	/// <para>
+	///		This operator is implemented in the bcl as of net6. Source and binary compatibility should be retained
+	///		across net versions, but this method should be inaccessible in net6+.
+	/// </para>
 	/// </remarks>
-#if !NET6_0_OR_GREATER
-	public static TSource? ElementAtOrDefault<TSource>(this IEnumerable<TSource> source, Index index)
-#else
+#if NET6_0_OR_GREATER
 	[Obsolete("This method has been implemented by the framework.")]
 	public static TSource? ElementAtOrDefault<TSource>(IEnumerable<TSource> source, Index index)
+#else
+	public static TSource? ElementAtOrDefault<TSource>(this IEnumerable<TSource> source, Index index)
 #endif
 	{
 		Guard.IsNotNull(source);
