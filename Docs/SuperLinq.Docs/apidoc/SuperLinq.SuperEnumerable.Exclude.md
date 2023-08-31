5e513b819098f6d96e4ce02e534b8ee738929077
Stuart Turner <stuart@turner-isageek.com>
2023-08-31 12:29:59 -0500

    Update documentation for `Exclude`

---
+++ Docs/SuperLinq.Docs/apidoc/SuperLinq.SuperEnumerable.Exclude.md
@@ -0,0 +1,6 @@
+﻿---
+uid: SuperLinq.SuperEnumerable.Exclude``1(System.Collections.Generic.IEnumerable{``0},System.Int32,System.Int32)
+example: [*content]
+---
+The following code example demonstrates how to exclude a contiguous set of elements from a sequence using `Exclude`.
+[!code-csharp[](SuperLinq/Exclude/Exclude.linq#L6-)]
--- Source/SuperLinq/Exclude.cs
+++ Source/SuperLinq/Exclude.cs
@@ -3,17 +3,32 @@
 public static partial class SuperEnumerable
 {
 	/// <summary>
-	/// Excludes a contiguous number of elements from a sequence starting at a given index.
+	///	    Excludes a contiguous number of elements from a sequence starting at a given index.
 	/// </summary>
-	/// <typeparam name="T">The type of the elements of the sequence</typeparam>
-	/// <param name="sequence">The sequence to exclude elements from</param>
-	/// <param name="startIndex">The zero-based index at which to begin excluding elements</param>
-	/// <param name="count">The number of elements to exclude</param>
-	/// <returns>A sequence that excludes the specified portion of elements</returns>
-	/// <exception cref="ArgumentNullException"><paramref name="sequence"/> is null.</exception>
-	/// <exception cref="ArgumentOutOfRangeException">
-	/// <paramref name="startIndex"/> or <paramref name="count"/> is less than <c>0</c>.
+	/// <typeparam name="T">
+	///	    The type of the elements of the sequence
+	/// </typeparam>
+	/// <param name="sequence">
+	///	    The sequence to exclude elements from
+	/// </param>
+	/// <param name="startIndex">
+	///	    The zero-based index at which to begin excluding elements
+	/// </param>
+	/// <param name="count">
+	///	    The number of elements to exclude
+	/// </param>
+	/// <returns>
+	///	    A sequence that excludes the specified portion of elements
+	/// </returns>
+	/// <exception cref="ArgumentNullException">
+	///	    <paramref name="sequence"/> is <see langword="null" />.
 	/// </exception>
+	/// <exception cref="ArgumentOutOfRangeException">
+	///	    <paramref name="startIndex"/> or <paramref name="count"/> is less than <c>0</c>.
+	/// </exception>
+	/// <remarks>
+	///	    This method uses deferred execution and streams its results.
+	/// </remarks>
 	public static IEnumerable<T> Exclude<T>(this IEnumerable<T> sequence, int startIndex, int count)
 	{
 		Guard.IsNotNull(sequence);
