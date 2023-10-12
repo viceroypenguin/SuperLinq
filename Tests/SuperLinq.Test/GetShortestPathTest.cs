f7a9fef2db4adf3bf0c948c7f2ac4831e5a5e823
Stuart Turner <stuart@turner-isageek.com>
2023-10-12 16:19:30 -0500

    Update documentation for `GetShortestPath`

--- Tests/SuperLinq.Test/GetShortestPathTest.cs
+++ Tests/SuperLinq.Test/GetShortestPathTest.cs
@@ -540,6 +540,49 @@ public static class GetShortestPathTest
 		}
 
 		[Fact]
+		public void Test()
+		{
+			var start = (x: 0, y: 0);
+			var end = (x: -2, y: -2);
+			((int x, int y) p, double cost, double bestGuess) GetNeighbor((int x, int y) p, double newCost)
+			{
+				var xD = p.x - end.x;
+				var yD = p.y - end.y;
+				var dist = Math.Sqrt((xD * xD) + (yD * yD));
+				return (p, newCost, newCost + dist);
+			}
+
+			IEnumerable<((int x, int y) p, double cost, double bestGuess)> GetNeighbors((int x, int y) p, double cost)
+			{
+				yield return GetNeighbor((p.x + 1, p.y), cost + 1.001d);
+				yield return GetNeighbor((p.x, p.y + 1), cost + 1.002d);
+				yield return GetNeighbor((p.x - 1, p.y), cost + 1.003d);
+				yield return GetNeighbor((p.x, p.y - 1), cost + 1.004d);
+			}
+
+			// Find the shortest path from start to end
+			var result = SuperEnumerable
+				.GetShortestPath<(int x, int y), double>(
+					start,
+					GetNeighbors,
+					end,
+					new PointComparer(),
+					null);
+		}
+
+		private class PointComparer : IEqualityComparer<(int x, int y)>
+		{
+			public bool Equals((int x, int y) x, (int x, int y) y) =>
+				ManhattanDistance(x) == ManhattanDistance(y);
+
+			public int GetHashCode((int x, int y) obj) =>
+				ManhattanDistance(obj).GetHashCode();
+
+			private static double ManhattanDistance((int x, int y) obj) =>
+				Math.Sqrt((obj.x * obj.x) + (obj.y * obj.y));
+		}
+
+		[Fact]
 		public void InvalidMapThrowsException()
 		{
 			_ = Assert.Throws<InvalidOperationException>(() =>
