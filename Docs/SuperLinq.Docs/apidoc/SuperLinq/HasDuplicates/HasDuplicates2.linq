a5530fcbf0fe2b2d0e739a92a3502110e3b929a4
Stuart Turner <stuart@turner-isageek.com>
2023-10-13 09:28:21 -0500

    Update documentation for `HasDuplicates`

---
+++ Docs/SuperLinq.Docs/apidoc/SuperLinq/HasDuplicates/HasDuplicates2.linq
@@ -0,0 +1,29 @@
+<Query Kind="Statements">
+  <NuGetReference>SuperLinq</NuGetReference>
+  <Namespace>SuperLinq</Namespace>
+</Query>
+
+var seq1 = new[] { "foo", "bar", "baz" };
+var seq2 = new[] { "foo", "bar", "baz", "foo", };
+var seq3 = new[] { "foo", "bar", "baz", "FOO", };
+
+// determine if a sequence has duplicate items
+var result = seq1
+	.HasDuplicates(
+		StringComparer.OrdinalIgnoreCase);
+Console.WriteLine($"Has Duplicates: {result}");
+
+result = seq2
+	.HasDuplicates(
+		StringComparer.OrdinalIgnoreCase);
+Console.WriteLine($"Has Duplicates: {result}");
+
+result = seq3
+	.HasDuplicates(
+		StringComparer.OrdinalIgnoreCase);
+Console.WriteLine($"Has Duplicates: {result}");
+
+// This code produces the following output:
+// Has Duplicates: False
+// Has Duplicates: True
+// Has Duplicates: True
--- Source/SuperLinq/HasDuplicates.cs
+++ Source/SuperLinq/HasDuplicates.cs
@@ -3,69 +3,128 @@
 public static partial class SuperEnumerable
 {
 	/// <summary>
-	///     Checks if sequence contains duplicates
+	///     Checks if sequence contains duplicates.
 	/// </summary>
-	/// <param name="source">The sequence to check.</param>
-	/// <typeparam name="T">The type of the elements in the source sequence</typeparam>
+	/// <typeparam name="T">
+	///	    The type of the elements in the source sequence.
+	/// </typeparam>
+	/// <param name="source">
+	///	    The sequence to check.
+	/// </param>
+	/// <exception cref="ArgumentNullException">
+	///	    <paramref name="source"/> is <see langword="null"/>.
+	/// </exception>
 	/// <returns>
-	///     <see langword="true" /> if any element of the sequence <paramref name="source" /> is duplicated,
-	///     <see langword="false" /> otherwise
+	///     <see langword="true" /> if any element of the sequence <paramref name="source" /> is duplicated, <see
+	///     langword="false" /> otherwise.
 	/// </returns>
+	/// <remarks>
+	/// <para>
+	///	    This method executes immediately.
+	/// </para>
+	/// </remarks>
 	public static bool HasDuplicates<T>(this IEnumerable<T> source)
 	{
 		return source.HasDuplicates(EqualityComparer<T>.Default);
 	}
 
 	/// <summary>
-	///     Checks if sequence contains duplicates, using the specified element equality comparer
+	///     Checks if sequence contains duplicates, using the specified element equality comparer.
 	/// </summary>
-	/// <param name="source">The sequence to check.</param>
-	/// <param name="comparer">
-	///     The equality comparer to use to determine whether or not keys are equal.
-	///     If null, the default equality comparer for <c>TSource</c> is used.
+	/// <typeparam name="T">
+	///	    The type of the elements in the source sequence.
+	/// </typeparam>
+	/// <param name="source">
+	///	    The sequence to check.
 	/// </param>
-	/// <typeparam name="T">The type of the elements in the source sequence</typeparam>
+	/// <param name="comparer">
+	///     The equality comparer to use to determine whether or not keys are equal. If <see langword="null"/>, the
+	///     default equality comparer for <typeparamref name="T"/> is used.
+	/// </param>
+	/// <exception cref="ArgumentNullException">
+	///	    <paramref name="source"/> is <see langword="null"/>.
+	/// </exception>
 	/// <returns>
-	///     <see langword="true" /> if any element of the sequence <paramref name="source" /> is duplicated,
-	///     <see langword="false" /> otherwise
+	///     <see langword="true" /> if any element of the sequence <paramref name="source" /> is duplicated, <see
+	///     langword="false" /> otherwise.
 	/// </returns>
+	/// <remarks>
+	/// <para>
+	///	    This method executes immediately.
+	/// </para>
+	/// </remarks>
 	public static bool HasDuplicates<T>(this IEnumerable<T> source, IEqualityComparer<T>? comparer)
 	{
 		return source.HasDuplicates(Identity, comparer);
 	}
 
 	/// <summary>
-	///     Checks if sequence contains duplicates, using the specified element equality comparer
+	///     Checks if sequence contains duplicates according to a specified key selector function.
 	/// </summary>
-	/// <param name="source">The sequence to check.</param>
-	/// <param name="keySelector">Projection for determining "distinctness"</param>
-	/// <typeparam name="TSource">Type of the source sequence</typeparam>
-	/// <typeparam name="TKey">Type of the projected element</typeparam>
+	/// <typeparam name="TSource">
+	///	    Type of the source sequence.
+	/// </typeparam>
+	/// <typeparam name="TKey">
+	///	    The type of key to distinguish elements by.
+	/// </typeparam>
+	/// <param name="source">
+	///	    The sequence to check.
+	/// </param>
+	/// <param name="keySelector">
+	///	    A function to extract the key for each element.
+	/// </param>
+	/// <exception cref="ArgumentNullException">
+	///	    <paramref name="source"/> or <paramref name="keySelector"/> is <see langword="null"/>.
+	/// </exception>
 	/// <returns>
-	///     <see langword="true" /> if any element of the sequence <paramref name="source" /> is duplicated,
-	///     <see langword="false" /> otherwise
+	///     <see langword="true" /> if any element of the sequence <paramref name="source" /> is duplicated, <see
+	///     langword="false" /> otherwise.
 	/// </returns>
+	/// <remarks>
+	/// <para>
+	///	    This method executes immediately.
+	/// </para>
+	/// </remarks>
 	public static bool HasDuplicates<TSource, TKey>(this IEnumerable<TSource> source, Func<TSource, TKey> keySelector)
 	{
 		return source.HasDuplicates(keySelector, EqualityComparer<TKey>.Default);
 	}
 
 	/// <summary>
-	///     Checks if sequence contains duplicates, using the specified element equality comparer
+	///     Checks if sequence contains duplicates according to a specified key selector function, using the specified
+	///     element equality comparer.
 	/// </summary>
-	/// <param name="source">The sequence to check.</param>
-	/// <param name="keySelector">Projection for determining "distinctness"</param>
-	/// <param name="comparer">
-	///     The equality comparer to use to determine whether or not keys are equal.
-	///     If null, the default equality comparer for <c>TSource</c> is used.
+	/// <typeparam name="TSource">
+	///	    Type of the source sequence.
+	/// </typeparam>
+	/// <typeparam name="TKey">
+	///	    The type of key to distinguish elements by.
+	/// </typeparam>
+	/// <param name="source">
+	///	    The sequence to check.
 	/// </param>
-	/// <typeparam name="TSource">Type of the source sequence</typeparam>
-	/// <typeparam name="TKey">Type of the projected element</typeparam>
+	/// <param name="keySelector">
+	///	    A function to extract the key for each element.
+	/// </param>
+	/// <param name="comparer">
+	///     The equality comparer to use to determine whether or not keys are equal. If <see langword="null"/>, the
+	///     default equality comparer for <typeparamref name="TKey"/> is used.
+	/// </param>
+	/// <exception cref="ArgumentNullException">
+	///	    <paramref name="source"/> or <paramref name="keySelector"/> is <see langword="null"/>.
+	/// </exception>
 	/// <returns>
-	///     <see langword="true" /> if any element of the sequence <paramref name="source" /> is duplicated,
-	///     <see langword="false" /> otherwise
+	///     <see langword="true" /> if any element of the sequence <paramref name="source" /> is duplicated, <see
+	///     langword="false" /> otherwise.
 	/// </returns>
-	public static bool HasDuplicates<TSource, TKey>(this IEnumerable<TSource> source, Func<TSource, TKey> keySelector,
+	/// <remarks>
+	/// <para>
+	///	    This method executes immediately.
+	/// </para>
+	/// </remarks>
+	public static bool HasDuplicates<TSource, TKey>(
+		this IEnumerable<TSource> source,
+		Func<TSource, TKey> keySelector,
 		IEqualityComparer<TKey>? comparer)
 	{
 		Guard.IsNotNull(source);
