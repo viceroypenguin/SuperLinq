cf237e27ed7aa15ac60a4018818e02df2aa1e703
Stuart Turner <stuart@turner-isageek.com>
2023-10-17 15:45:55 -0500

    Add async `AggregateBy`

---
+++ Source/SuperLinq.Async/AggregateBy.cs
@@ -0,0 +1,151 @@
+﻿namespace SuperLinq.Async;
+
+public static partial class AsyncSuperEnumerable
+{
+	/// <summary>
+	///	    Applies a key-generating function to each element of a sequence and returns an aggregate value for each key.
+	///     An additional argument specifies a comparer to use for testing equivalence of keys.
+	/// </summary>
+	/// <typeparam name="TSource">
+	///	    Type of the elements of the source sequence.
+	/// </typeparam>
+	/// <typeparam name="TKey">
+	///	    Type of the projected element.
+	/// </typeparam>
+	/// <typeparam name="TAccumulate">
+	///	    Type of the accumulator value.
+	/// </typeparam>
+	/// <param name="source">
+	///	    Source sequence.
+	/// </param>
+	/// <param name="keySelector">
+	///	    Function that transforms each item of source sequence into a key to be compared against the others.
+	/// </param>
+	/// <param name="seed">
+	///	    The initial accumulator value for each key-group.
+	/// </param>
+	/// <param name="func">
+	///	    An accumulator function to be invoked on each element. The accumulator value is tracked separately for each
+	///     key.
+	/// </param>
+	/// <param name="comparer">
+	///	    The equality comparer to use to determine whether or not keys are equal. If <see langword="null" />, the
+	///     default equality comparer for <typeparamref name="TKey"/> is used.
+	/// </param>
+	/// <exception cref="ArgumentNullException">
+	///	    <paramref name="source"/>, <paramref name="keySelector"/>, or <paramref name="func"/> is <see
+	///     langword="null" />.
+	/// </exception>
+	/// <returns>
+	///	    A sequence of unique keys and their accumulated value.
+	/// </returns>
+	/// <remarks>
+	/// <para>
+	///	    This method is implemented by using deferred execution. The operator will be executed in it's entirety
+	///	    immediately when the sequence is first enumerated.
+	/// </para>
+	/// </remarks>
+	public static IAsyncEnumerable<KeyValuePair<TKey, TAccumulate>> AggregateBy<TSource, TKey, TAccumulate>(
+		this IAsyncEnumerable<TSource> source,
+		Func<TSource, TKey> keySelector,
+		TAccumulate seed,
+		Func<TAccumulate, TSource, TAccumulate> func,
+		IEqualityComparer<TKey>? comparer = null) where TKey : notnull
+	{
+		return AggregateBy(source, keySelector, _ => seed, func, comparer);
+	}
+
+	/// <summary>
+	///	    Applies a key-generating function to each element of a sequence and returns an aggregate value for each key.
+	///     An additional argument specifies a comparer to use for testing equivalence of keys.
+	/// </summary>
+	/// <typeparam name="TSource">
+	///	    Type of the elements of the source sequence.
+	/// </typeparam>
+	/// <typeparam name="TKey">
+	///	    Type of the projected element.
+	/// </typeparam>
+	/// <typeparam name="TAccumulate">
+	///	    Type of the accumulator value.
+	/// </typeparam>
+	/// <param name="source">
+	///	    Source sequence.
+	/// </param>
+	/// <param name="keySelector">
+	///	    Function that transforms each item of source sequence into a key to be compared against the others.
+	/// </param>
+	/// <param name="seedSelector">
+	///	    A function that returns the initial seed for each key.
+	/// </param>
+	/// <param name="func">
+	///	    An accumulator function to be invoked on each element. The accumulator value is tracked separately for each
+	///     key.
+	/// </param>
+	/// <param name="comparer">
+	///	    The equality comparer to use to determine whether or not keys are equal. If <see langword="null" />, the
+	///     default equality comparer for <typeparamref name="TKey"/> is used.
+	/// </param>
+	/// <exception cref="ArgumentNullException">
+	///	    <paramref name="source"/>, <paramref name="keySelector"/>, or <paramref name="func"/> is <see
+	///     langword="null" />.
+	/// </exception>
+	/// <returns>
+	///	    A sequence of unique keys and their accumulated value.
+	/// </returns>
+	/// <remarks>
+	/// <para>
+	///	    This method is implemented by using deferred execution. The operator will be executed in it's entirety
+	///	    immediately when the sequence is first enumerated.
+	/// </para>
+	/// </remarks>
+	public static IAsyncEnumerable<KeyValuePair<TKey, TAccumulate>> AggregateBy<TSource, TKey, TAccumulate>(
+		this IAsyncEnumerable<TSource> source,
+		Func<TSource, TKey> keySelector,
+		Func<TKey, TAccumulate> seedSelector,
+		Func<TAccumulate, TSource, TAccumulate> func,
+		IEqualityComparer<TKey>? comparer = null) where TKey : notnull
+	{
+		Guard.IsNotNull(source);
+		Guard.IsNotNull(keySelector);
+		Guard.IsNotNull(seedSelector);
+		Guard.IsNotNull(func);
+
+		return Core(source, keySelector, seedSelector, func, comparer ?? EqualityComparer<TKey>.Default);
+
+		static async IAsyncEnumerable<KeyValuePair<TKey, TAccumulate>> Core(
+			IAsyncEnumerable<TSource> source,
+			Func<TSource, TKey> keySelector,
+			Func<TKey, TAccumulate> seedSelector,
+			Func<TAccumulate, TSource, TAccumulate> func,
+			IEqualityComparer<TKey> comparer,
+			[EnumeratorCancellation] CancellationToken token = default)
+		{
+			foreach (var kvp in await Loop(source, keySelector, seedSelector, func, comparer, token).ConfigureAwait(false))
+				yield return kvp;
+		}
+
+		async static Task<Dictionary<TKey, TAccumulate>> Loop(
+			IAsyncEnumerable<TSource> source,
+			Func<TSource, TKey> keySelector,
+			Func<TKey, TAccumulate> seedSelector,
+			Func<TAccumulate, TSource, TAccumulate> func,
+			IEqualityComparer<TKey> cmp,
+			CancellationToken token)
+		{
+			var dict = new Dictionary<TKey, TAccumulate>(cmp);
+
+			await foreach (var item in source.WithCancellation(token).ConfigureAwait(false))
+			{
+				var key = keySelector(item);
+
+				dict[key] = func(
+					dict.TryGetValue(key, out var acc)
+						? acc
+						: seedSelector(key),
+					item);
+			}
+
+			return dict;
+		}
+	}
+}
---
+++ Tests/SuperLinq.Async.Test/AggregateByTest.cs
@@ -0,0 +1,205 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+namespace Test.Async;
+
+public class AggregateByTest
+{
+	[Theory]
+	[MemberData(nameof(AggregateBy_TestData))]
+	public static async Task AggregateBy_HasExpectedOutput<TSource, TKey, TAccumulate>(
+		IEnumerable<TSource> source,
+		Func<TSource, TKey> keySelector,
+		Func<TKey, TAccumulate> seedSelector,
+		Func<TAccumulate, TSource, TAccumulate> func,
+		IEqualityComparer<TKey>? comparer,
+		IEnumerable<KeyValuePair<TKey, TAccumulate>> expected) where TKey : notnull
+	{
+		await using var ts = source.AsTestingSequence();
+		await ts.AggregateBy(keySelector, seedSelector, func, comparer)
+			.AssertCollectionEqual(expected);
+	}
+
+	public static IEnumerable<object?[]> AggregateBy_TestData()
+	{
+		yield return WrapArgs(
+			source: Enumerable.Empty<int>(),
+			keySelector: x => x,
+			seedSelector: x => 0,
+			func: (x, y) => x + y,
+			comparer: null,
+			expected: Enumerable.Empty<KeyValuePair<int, int>>());
+
+		yield return WrapArgs(
+			source: Enumerable.Range(0, 10),
+			keySelector: x => x,
+			seedSelector: x => 0,
+			func: (x, y) => x + y,
+			comparer: null,
+			expected: Enumerable.Range(0, 10).ToDictionary(x => x, x => x));
+
+		yield return WrapArgs(
+			source: Enumerable.Range(5, 10),
+			keySelector: x => true,
+			seedSelector: x => 0,
+			func: (x, y) => x + y,
+			comparer: null,
+			expected: Enumerable.Repeat(true, 1).ToDictionary(x => x, x => 95));
+
+		yield return WrapArgs(
+			source: Enumerable.Range(0, 20),
+			keySelector: x => x % 5,
+			seedSelector: x => 0,
+			func: (x, y) => x + y,
+			comparer: null,
+			expected: Enumerable.Range(0, 5).ToDictionary(x => x, x => 30 + (4 * x)));
+
+		yield return WrapArgs(
+			source: Enumerable.Repeat(5, 20),
+			keySelector: x => x,
+			seedSelector: x => 0,
+			func: (x, y) => x + y,
+			comparer: null,
+			expected: Enumerable.Repeat(5, 1).ToDictionary(x => x, x => 100));
+
+		yield return WrapArgs(
+			source: new string[] { "Bob", "bob", "tim", "Bob", "Tim" },
+			keySelector: x => x,
+			seedSelector: x => string.Empty,
+			func: (x, y) => x + y,
+			null,
+			expected: new Dictionary<string, string>
+			{
+				{ "Bob", "BobBob" },
+				{ "bob", "bob" },
+				{ "tim", "tim" },
+				{ "Tim", "Tim" },
+			});
+
+		yield return WrapArgs(
+			source: new string[] { "Bob", "bob", "tim", "Bob", "Tim" },
+			keySelector: x => x,
+			seedSelector: x => string.Empty,
+			func: (x, y) => x + y,
+			StringComparer.OrdinalIgnoreCase,
+			expected: new Dictionary<string, string>
+			{
+				{ "Bob", "BobbobBob" },
+				{ "tim", "timTim" }
+			});
+
+		yield return WrapArgs(
+			source: new (string Name, int Age)[] { ("Tom", 20), ("Dick", 30), ("Harry", 40) },
+			keySelector: x => x.Age,
+			seedSelector: x => $"I am {x} and my name is ",
+			func: (x, y) => x + y.Name,
+			comparer: null,
+			expected: new Dictionary<int, string>
+			{
+				{ 20, "I am 20 and my name is Tom" },
+				{ 30, "I am 30 and my name is Dick" },
+				{ 40, "I am 40 and my name is Harry" }
+			});
+
+		yield return WrapArgs(
+			source: new (string Name, int Age)[] { ("Tom", 20), ("Dick", 20), ("Harry", 40) },
+			keySelector: x => x.Age,
+			seedSelector: x => $"I am {x} and my name is",
+			func: (x, y) => $"{x} maybe {y.Name}",
+			comparer: null,
+			expected: new Dictionary<int, string>
+			{
+				{ 20, "I am 20 and my name is maybe Tom maybe Dick" },
+				{ 40, "I am 40 and my name is maybe Harry" }
+			});
+
+		yield return WrapArgs(
+			source: new (string Name, int Age)[] { ("Bob", 20), ("bob", 20), ("Harry", 20) },
+			keySelector: x => x.Name,
+			seedSelector: x => 0,
+			func: (x, y) => x + y.Age,
+			comparer: null,
+			expected: new string[] { "Bob", "bob", "Harry" }.ToDictionary(x => x, x => 20));
+
+		yield return WrapArgs(
+			source: new (string Name, int Age)[] { ("Bob", 20), ("bob", 30), ("Harry", 40) },
+			keySelector: x => x.Name,
+			seedSelector: x => 0,
+			func: (x, y) => x + y.Age,
+			comparer: StringComparer.OrdinalIgnoreCase,
+			expected: new Dictionary<string, int>
+			{
+				{ "Bob", 50 },
+				{ "Harry", 40 }
+			});
+
+		static object?[] WrapArgs<TSource, TKey, TAccumulate>(IEnumerable<TSource> source, Func<TSource, TKey> keySelector, Func<TKey, TAccumulate> seedSelector, Func<TAccumulate, TSource, TAccumulate> func, IEqualityComparer<TKey>? comparer, IEnumerable<KeyValuePair<TKey, TAccumulate>> expected)
+			=> new object?[] { source, keySelector, seedSelector, func, comparer, expected };
+	}
+
+	[Fact]
+	public async Task GroupBy()
+	{
+		static IAsyncEnumerable<KeyValuePair<TKey, List<TSource>>> GroupBy<TSource, TKey>(IAsyncEnumerable<TSource> source, Func<TSource, TKey> keySelector)
+			where TKey : notnull =>
+			source.AggregateBy(
+				keySelector,
+				seedSelector: _ => new List<TSource>(),
+				(group, element) => { group.Add(element); return group; });
+
+		await using var ts = TestingSequence.Of(1, 2, 3, 4);
+		var oddsEvens = await
+			GroupBy(
+				ts,
+				i => i % 2 == 0)
+			.ToDictionaryAsync(x => x.Key, x => x.Value);
+
+		Assert.True(oddsEvens[true].CollectionEqual(new[] { 2, 4, }));
+		Assert.True(oddsEvens[false].CollectionEqual(new[] { 1, 3, }));
+	}
+
+	[Fact]
+	public async Task LongCountBy()
+	{
+		static IAsyncEnumerable<KeyValuePair<TKey, long>> LongCountBy<TSource, TKey>(IAsyncEnumerable<TSource> source, Func<TSource, TKey> keySelector)
+			where TKey : notnull =>
+			source.AggregateBy(
+				keySelector,
+				seed: 0L,
+				(count, _) => ++count);
+
+		await using var ts = TestingSequence.Of(1, 2, 3, 4);
+		var oddsEvens = LongCountBy(
+			ts,
+			i => i % 2 == 0);
+
+		await oddsEvens
+			.AssertCollectionEqual(new Dictionary<bool, long>
+			{
+				[false] = 2,
+				[true] = 2,
+			});
+	}
+
+	[Fact]
+	public async Task Score()
+	{
+		await using var data = TestingSequence.Of<(string id, int score)>(
+			("0", 42),
+			("1", 5),
+			("2", 4),
+			("1", 10),
+			("0", 25));
+
+		var scores = await data
+			.AggregateBy(
+				keySelector: entry => entry.id,
+				seed: 0,
+				(totalScore, curr) => totalScore + curr.score)
+			.ToDictionaryAsync(x => x.Key, x => x.Value);
+
+		Assert.Equal(67, scores["0"]);
+		Assert.Equal(15, scores["1"]);
+		Assert.Equal(4, scores["2"]);
+	}
+}
