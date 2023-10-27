c40d4b70fd739f2b1088a5b621e3d52827aa09b8
Stuart Turner <stuart@turner-isageek.com>
2023-10-27 11:03:08 -0500

    Update documentation for `Lead`

--- Source/SuperLinq/Lead.cs
+++ Source/SuperLinq/Lead.cs
@@ -3,18 +3,34 @@
 public static partial class SuperEnumerable
 {
 	/// <summary>
-	/// Produces a projection of a sequence by evaluating pairs of elements separated by a positive offset.
+	///		Produces a projection of a sequence by evaluating pairs of elements separated by a positive offset.
 	/// </summary>
-	/// <typeparam name="TSource">The type of the elements in the source sequence</typeparam>
-	/// <param name="source">The sequence over which to evaluate Lead</param>
-	/// <param name="offset">The offset (expressed as a positive number) by which to lead each element of the sequence</param>
-	/// <returns>A sequence of tuples with the current and lead elements</returns>
-	/// <exception cref="ArgumentNullException"><paramref name="source"/> is <see langword="null"/>.</exception>
-	/// <exception cref="ArgumentOutOfRangeException"><paramref name="offset"/> is below 1.</exception>
+	/// <typeparam name="TSource">
+	///		The type of the elements in the source sequence
+	/// </typeparam>
+	/// <param name="source">
+	///		The sequence over which to evaluate Lead
+	/// </param>
+	/// <param name="offset">
+	///		The offset (expressed as a positive number) by which to lead each element of the sequence
+	/// </param>
+	/// <returns>
+	///		A sequence of tuples with the current and lead elements
+	/// </returns>
+	/// <exception cref="ArgumentNullException">
+	///		<paramref name="source"/> is <see langword="null"/>.
+	/// </exception>
+	/// <exception cref="ArgumentOutOfRangeException">
+	///		<paramref name="offset"/> is below <c>1</c>.
+	/// </exception>
 	/// <remarks>
-	/// This operator evaluates in a deferred and streaming manner.<br/>
-	/// For elements of the sequence that are less than <paramref name="offset"/> items from the end,
-	/// <see langword="default"/>(<typeparamref name="TSource"/>?) is used as the lead value.<br/>
+	/// <para>
+	///	    For elements of the sequence that are less than <paramref name="offset"/> items from the end, <c><see
+	///     langword="default"/>(<typeparamref name="TSource"/>?)</c> is used as the lead value.<br/>
+	/// </para>
+	/// <para>
+	///	    This operator evaluates in a deferred and streaming manner.
+	/// </para>
 	/// </remarks>
 	public static IEnumerable<(TSource current, TSource? lead)> Lead<TSource>(this IEnumerable<TSource> source, int offset)
 	{
@@ -22,21 +38,40 @@ public static partial class SuperEnumerable
 	}
 
 	/// <summary>
-	/// Produces a projection of a sequence by evaluating pairs of elements separated by a positive offset.
+	///		Produces a projection of a sequence by evaluating pairs of elements separated by a positive offset.
 	/// </summary>
-	/// <typeparam name="TSource">The type of the elements in the source sequence</typeparam>
-	/// <typeparam name="TResult">The type of the elements in the result sequence</typeparam>
-	/// <param name="source">The sequence over which to evaluate Lead</param>
-	/// <param name="offset">The offset (expressed as a positive number) by which to lead each element of the sequence</param>
-	/// <param name="resultSelector">A projection function which accepts the current and subsequent (lead) element (in that order) and produces a result</param>
-	/// <returns>A sequence produced by projecting each element of the sequence with its lead pairing</returns>
-	/// <exception cref="ArgumentNullException"><paramref name="source"/> is <see langword="null"/>.</exception>
-	/// <exception cref="ArgumentNullException"><paramref name="resultSelector"/> is <see langword="null"/>.</exception>
-	/// <exception cref="ArgumentOutOfRangeException"><paramref name="offset"/> is below 1.</exception>
+	/// <typeparam name="TSource">
+	///		The type of the elements in the source sequence
+	/// </typeparam>
+	/// <typeparam name="TResult">
+	///		The type of the elements in the result sequence
+	/// </typeparam>
+	/// <param name="source">
+	///		The sequence over which to evaluate Lead
+	/// </param>
+	/// <param name="offset">
+	///		The offset (expressed as a positive number) by which to lead each element of the sequence
+	/// </param>
+	/// <param name="resultSelector">
+	///		A projection function which accepts the current and subsequent (lead) element (in that order) and produces a result
+	/// </param>
+	/// <returns>
+	///		A sequence produced by projecting each element of the sequence with its lead pairing
+	/// </returns>
+	/// <exception cref="ArgumentNullException">
+	///		<paramref name="source"/> or <paramref name="resultSelector"/> is <see langword="null"/>.
+	/// </exception>
+	/// <exception cref="ArgumentOutOfRangeException">
+	///		<paramref name="offset"/> is below <c>1</c>.
+	/// </exception>
 	/// <remarks>
-	/// This operator evaluates in a deferred and streaming manner.<br/>
-	/// For elements of the sequence that are less than <paramref name="offset"/> items from the end,
-	/// <see langword="default"/>(<typeparamref name="TSource"/>?) is used as the lead value.<br/>
+	/// <para>
+	///	    For elements of the sequence that are less than <paramref name="offset"/> items from the end, <c><see
+	///     langword="default"/>(<typeparamref name="TSource"/>?)</c> is used as the lead value.<br/>
+	/// </para>
+	/// <para>
+	///	    This operator evaluates in a deferred and streaming manner.
+	/// </para>
 	/// </remarks>
 	public static IEnumerable<TResult> Lead<TSource, TResult>(this IEnumerable<TSource> source, int offset, Func<TSource, TSource?, TResult> resultSelector)
 	{
@@ -44,23 +79,42 @@ public static partial class SuperEnumerable
 	}
 
 	/// <summary>
-	/// Produces a projection of a sequence by evaluating pairs of elements separated by a positive offset.
+	///	    Produces a projection of a sequence by evaluating pairs of elements separated by a positive offset.
 	/// </summary>
+	/// <typeparam name="TSource">
+	///	    The type of the elements in the source sequence
+	/// </typeparam>
+	/// <typeparam name="TResult">
+	///	    The type of the elements in the result sequence
+	/// </typeparam>
+	/// <param name="source">
+	///	    The sequence over which to evaluate Lead
+	/// </param>
+	/// <param name="offset">
+	///	    The offset (expressed as a positive number) by which to lead each element of the sequence
+	/// </param>
+	/// <param name="defaultLeadValue">
+	///	    A default value supplied for the leading element when none is available
+	/// </param>
+	/// <param name="resultSelector">
+	///	    A projection function which accepts the current and subsequent (lead) element (in that order) and produces a
+	///     result</param>
+	/// <returns>A sequence produced by projecting each element of the sequence with its lead pairing
+	/// </returns>
+	/// <exception cref="ArgumentNullException">
+	///	    <paramref name="source"/> or <paramref name="resultSelector"/> is <see langword="null"/>.
+	/// </exception>
+	/// <exception cref="ArgumentOutOfRangeException">
+	///	    <paramref name="offset"/> is below <c>1</c>.
+	/// </exception>
 	/// <remarks>
-	/// This operator evaluates in a deferred and streaming manner.<br/>
-	/// </remarks>
-	/// <typeparam name="TSource">The type of the elements in the source sequence</typeparam>
-	/// <typeparam name="TResult">The type of the elements in the result sequence</typeparam>
-	/// <param name="source">The sequence over which to evaluate Lead</param>
-	/// <param name="offset">The offset (expressed as a positive number) by which to lead each element of the sequence</param>
-	/// <param name="defaultLeadValue">A default value supplied for the leading element when none is available</param>
-	/// <param name="resultSelector">A projection function which accepts the current and subsequent (lead) element (in that order) and produces a result</param>
-	/// <returns>A sequence produced by projecting each element of the sequence with its lead pairing</returns>
-	/// <exception cref="ArgumentNullException"><paramref name="source"/> is <see langword="null"/>.</exception>
-	/// <exception cref="ArgumentNullException"><paramref name="resultSelector"/> is <see langword="null"/>.</exception>
-	/// <exception cref="ArgumentOutOfRangeException"><paramref name="offset"/> is below 1.</exception>
-	/// <remarks>
-	/// This operator evaluates in a deferred and streaming manner.<br/>
+	/// <para>
+	///	    For elements of the sequence that are less than <paramref name="offset"/> items from the end, <paramref
+	///     name="defaultLeadValue"/> is used as the lead value.<br/>
+	/// </para>
+	/// <para>
+	///	    This operator evaluates in a deferred and streaming manner.
+	/// </para>
 	/// </remarks>
 	public static IEnumerable<TResult> Lead<TSource, TResult>(this IEnumerable<TSource> source, int offset, TSource defaultLeadValue, Func<TSource, TSource, TResult> resultSelector)
 	{
