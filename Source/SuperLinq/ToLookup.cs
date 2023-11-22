d6fbae6b0128bf365c7b7a4070abe513a4a37431
Stuart Turner <stuart@turner-isageek.com>
2023-11-22 07:26:03 -0600

    Update documentation for `ToLookup`

--- Source/SuperLinq/ToLookup.cs
+++ Source/SuperLinq/ToLookup.cs
@@ -3,35 +3,64 @@
 public static partial class SuperEnumerable
 {
 	/// <summary>
-	/// Creates a <see cref="ILookup{TKey,TValue}" /> from a sequence of
-	/// <see cref="KeyValuePair{TKey,TValue}" /> elements.
+	///	    Creates an <see cref="ILookup{TKey,TValue}" /> from a sequence of <see cref="KeyValuePair{TKey,TValue}" />
+	///     elements.
 	/// </summary>
-	/// <typeparam name="TKey">The type of the key.</typeparam>
-	/// <typeparam name="TValue">The type of the value.</typeparam>
-	/// <param name="source">The source sequence of key-value pairs.</param>
+	/// <typeparam name="TKey">
+	///	    The type of the key.
+	/// </typeparam>
+	/// <typeparam name="TValue">
+	///	    The type of the value.
+	/// </typeparam>
+	/// <param name="source">
+	///	    The source sequence of key-value pairs.
+	/// </param>
 	/// <returns>
-	/// A <see cref="ILookup{TKey,TValue}"/> containing the values
-	/// mapped to their keys.
+	///	    An <see cref="ILookup{TKey,TValue}"/> containing the values mapped to their keys.
 	/// </returns>
-
-	public static ILookup<TKey, TValue> ToLookup<TKey, TValue>(this IEnumerable<KeyValuePair<TKey, TValue>> source) =>
-		source.ToLookup(null);
+	/// <exception cref="ArgumentNullException">
+	///	    <paramref name="source"/> is <see langword="null"/>
+	/// </exception>
+	/// <remarks>
+	/// <para>
+	///	    This method executes immediately.
+	/// </para>
+	/// </remarks>
+	public static ILookup<TKey, TValue> ToLookup<TKey, TValue>(
+		this IEnumerable<KeyValuePair<TKey, TValue>> source)
+	{
+		return source.ToLookup(null);
+	}
 
 	/// <summary>
-	/// Creates a <see cref="ILookup{TKey,TValue}" /> from a sequence of
-	/// <see cref="KeyValuePair{TKey,TValue}" /> elements. An additional
-	/// parameter specifies a comparer for keys.
+	///	    Creates an <see cref="ILookup{TKey,TValue}" /> from a sequence of <see cref="KeyValuePair{TKey,TValue}" />
+	///     elements. An additional parameter specifies a comparer for keys.
 	/// </summary>
-	/// <typeparam name="TKey">The type of the key.</typeparam>
-	/// <typeparam name="TValue">The type of the value.</typeparam>
-	/// <param name="source">The source sequence of key-value pairs.</param>
-	/// <param name="comparer">The comparer for keys.</param>
+	/// <typeparam name="TKey">
+	///	    The type of the key.
+	/// </typeparam>
+	/// <typeparam name="TValue">
+	///	    The type of the value.
+	/// </typeparam>
+	/// <param name="source">
+	///	    The source sequence of key-value pairs.
+	/// </param>
+	/// <param name="comparer">
+	///	    The comparer for keys.
+	/// </param>
 	/// <returns>
-	/// A <see cref="ILookup{TKey,TValue}"/> containing the values
-	/// mapped to their keys.
+	///	    An <see cref="ILookup{TKey,TValue}"/> containing the values mapped to their keys.
 	/// </returns>
-
-	public static ILookup<TKey, TValue> ToLookup<TKey, TValue>(this IEnumerable<KeyValuePair<TKey, TValue>> source,
+	/// <exception cref="ArgumentNullException">
+	///	    <paramref name="source"/> is <see langword="null"/>
+	/// </exception>
+	/// <remarks>
+	/// <para>
+	///	    This method executes immediately.
+	/// </para>
+	/// </remarks>
+	public static ILookup<TKey, TValue> ToLookup<TKey, TValue>(
+		this IEnumerable<KeyValuePair<TKey, TValue>> source,
 		IEqualityComparer<TKey>? comparer)
 	{
 		Guard.IsNotNull(source);
@@ -39,36 +68,64 @@ public static partial class SuperEnumerable
 	}
 
 	/// <summary>
-	/// Creates a <see cref="Lookup{TKey,TValue}" /> from a sequence of
-	/// tuples of 2 where the first item is the key and the second the
-	/// value.
+	///	    Creates an <see cref="ILookup{TKey,TValue}" /> /> from a sequence of tuples of 2 where the first item is the
+	///     key and the second the value.
 	/// </summary>
-	/// <typeparam name="TKey">The type of the key.</typeparam>
-	/// <typeparam name="TValue">The type of the value.</typeparam>
-	/// <param name="source">The source sequence of tuples of 2.</param>
+	/// <typeparam name="TKey">
+	///	    The type of the key.
+	/// </typeparam>
+	/// <typeparam name="TValue">
+	///	    The type of the value.
+	/// </typeparam>
+	/// <param name="source">
+	///	    The source sequence of tuples of 2.
+	/// </param>
 	/// <returns>
-	/// A <see cref="Lookup{TKey, TValue}"/> containing the values
-	/// mapped to their keys.
+	///	    An <see cref="ILookup{TKey,TValue}"/> containing the values mapped to their keys.
 	/// </returns>
-
-	public static ILookup<TKey, TValue> ToLookup<TKey, TValue>(this IEnumerable<(TKey Key, TValue Value)> source) =>
-		source.ToLookup(null);
+	/// <exception cref="ArgumentNullException">
+	///	    <paramref name="source"/> is <see langword="null"/>
+	/// </exception>
+	/// <remarks>
+	/// <para>
+	///	    This method executes immediately.
+	/// </para>
+	/// </remarks>
+	public static ILookup<TKey, TValue> ToLookup<TKey, TValue>(
+		this IEnumerable<(TKey Key, TValue Value)> source)
+	{
+		return source.ToLookup(null);
+	}
 
 	/// <summary>
-	/// Creates a <see cref="Lookup{TKey,TValue}" /> from a sequence of
-	/// tuples of 2 where the first item is the key and the second the
-	/// value. An additional parameter specifies a comparer for keys.
+	///	    Creates an <see cref="ILookup{TKey,TValue}" /> from a sequence of tuples of 2 where the first item is the
+	///     key and the second the value. An additional parameter specifies a comparer for keys.
 	/// </summary>
-	/// <typeparam name="TKey">The type of the key.</typeparam>
-	/// <typeparam name="TValue">The type of the value.</typeparam>
-	/// <param name="source">The source sequence of tuples of 2.</param>
-	/// <param name="comparer">The comparer for keys.</param>
+	/// <typeparam name="TKey">
+	///	    The type of the key.
+	/// </typeparam>
+	/// <typeparam name="TValue">
+	///	    The type of the value.
+	/// </typeparam>
+	/// <param name="source">
+	///	    The source sequence of tuples of 2.
+	/// </param>
+	/// <param name="comparer">
+	///	    The comparer for keys.
+	/// </param>
 	/// <returns>
-	/// A <see cref="Lookup{TKey, TValue}"/> containing the values
-	/// mapped to their keys.
+	///	    An <see cref="ILookup{TKey,TValue}"/> containing the values mapped to their keys.
 	/// </returns>
-
-	public static ILookup<TKey, TValue> ToLookup<TKey, TValue>(this IEnumerable<(TKey Key, TValue Value)> source,
+	/// <exception cref="ArgumentNullException">
+	///	    <paramref name="source"/> is <see langword="null"/>
+	/// </exception>
+	/// <remarks>
+	/// <para>
+	///	    This method executes immediately.
+	/// </para>
+	/// </remarks>
+	public static ILookup<TKey, TValue> ToLookup<TKey, TValue>(
+		this IEnumerable<(TKey Key, TValue Value)> source,
 		IEqualityComparer<TKey>? comparer)
 	{
 		Guard.IsNotNull(source);
