b9089429e2de276dc8df7a29eec28d833f20d54f
Stuart Turner <stuart@turner-isageek.com>
2023-11-22 12:31:44 -0600

    Update documentation for `WhereLag`

---
+++ Docs/SuperLinq.Docs/apidoc/SuperLinq/WhereLag/WhereLag2.linq
@@ -0,0 +1,18 @@
+<Query Kind="Statements">
+  <NuGetReference>SuperLinq</NuGetReference>
+  <Namespace>SuperLinq</Namespace>
+</Query>
+
+var sequence = Enumerable.Range(1, 10);
+
+// Filter a sequence based on a leading value
+var result = sequence
+	.WhereLag(1, 20, (cur, lag) => cur + lag < 10);
+
+Console.WriteLine(
+	"[" +
+	string.Join(", ", result) +
+	"]");
+
+// This code produces the following output:
+// [2, 3, 4, 5]
--- Source/SuperLinq/WhereLag.cs
+++ Source/SuperLinq/WhereLag.cs
@@ -3,26 +3,36 @@
 public static partial class SuperEnumerable
 {
 	/// <summary>
-	/// Filters a sequence of values based on a predicate evaluated on the current value and a lagging value.
+	///	    Filters a sequence of values based on a predicate evaluated on the current value and a lagging value.
 	/// </summary>
-	/// <typeparam name="TSource">The type of the elements in the source sequence.</typeparam>
-	/// <param name="source">An <see cref="IEnumerable{T}"/> to filter.</param>
-	/// <param name="offset">The offset (expressed as a positive number) by which to lag each element of the
-	/// sequence.</param>
-	/// <param name="predicate">A function which accepts the current and lagged element (in that order) to
-	/// test for a condition.</param>
-	/// <returns>An <see cref="IEnumerable{T}"/> that contains elements from the input sequence that satisfy the
-	/// condition.</returns>
-	/// <exception cref="ArgumentNullException"><paramref name="source"/> is <see langword="null"/>.</exception>
-	/// <exception cref="ArgumentNullException"><paramref name="predicate"/> is <see langword="null"/>.</exception>
-	/// <exception cref="ArgumentOutOfRangeException"><paramref name="offset"/> is below 1.</exception>
+	/// <typeparam name="TSource">
+	///	    The type of the elements in the source sequence.
+	/// </typeparam>
+	/// <param name="source">
+	///	    An <see cref="IEnumerable{T}"/> to filter.
+	/// </param>
+	/// <param name="offset">
+	///	    The offset (expressed as a positive number) by which to lag each element of the sequence.
+	/// </param>
+	/// <param name="predicate">
+	///	    A function which accepts the current and lagged element (in that order) to test for a condition.
+	/// </param>
+	/// <returns>
+	///	    An <see cref="IEnumerable{T}"/> that contains elements from the input sequence that satisfy the condition.
+	/// </returns>
+	/// <exception cref="ArgumentNullException">
+	///	    <paramref name="source"/> or <paramref name="predicate"/> is <see langword="null"/>.
+	/// </exception>
+	/// <exception cref="ArgumentOutOfRangeException">
+	///	    <paramref name="offset"/> is below <c>1</c>.
+	/// </exception>
 	/// <remarks>
 	/// <para>
-	/// For elements of the sequence that are less than <paramref name="offset"/> items from the end, <see
-	/// langword="default"/>(<typeparamref name="TSource"/>?) is used as the Lag value.
+	///	    For elements of the sequence that are less than <paramref name="offset"/> items from the end, <see
+	///     langword="default"/>(<typeparamref name="TSource"/>?) is used as the Lag value.
 	/// </para>
 	/// <para>
-	/// This operator evaluates in a deferred and streaming manner.
+	///	    This operator evaluates in a deferred and streaming manner.
 	/// </para>
 	/// </remarks>
 	public static IEnumerable<TSource> WhereLag<TSource>(this IEnumerable<TSource> source, int offset, Func<TSource, TSource?, bool> predicate)
@@ -31,23 +41,35 @@ public static partial class SuperEnumerable
 	}
 
 	/// <summary>
-	/// Filters a sequence of values based on a predicate evaluated on the current value and a lagging value.
+	///	    Filters a sequence of values based on a predicate evaluated on the current value and a lagging value.
 	/// </summary>
-	/// <typeparam name="TSource">The type of the elements in the source sequence.</typeparam>
-	/// <param name="source">An <see cref="IEnumerable{T}"/> to filter.</param>
-	/// <param name="offset">The offset (expressed as a positive number) by which to lag each element of the
-	/// sequence.</param>
-	/// <param name="defaultLagValue">A default value supplied for the lagged element when none is available</param>
-	/// <param name="predicate">A function which accepts the current and lagged element (in that order) to
-	/// test for a condition.</param>
-	/// <returns>An <see cref="IEnumerable{T}"/> that contains elements from the input sequence that satisfy the
-	/// condition.</returns>
-	/// <exception cref="ArgumentNullException"><paramref name="source"/> is <see langword="null"/>.</exception>
-	/// <exception cref="ArgumentNullException"><paramref name="predicate"/> is <see langword="null"/>.</exception>
-	/// <exception cref="ArgumentOutOfRangeException"><paramref name="offset"/> is below 1.</exception>
+	/// <typeparam name="TSource">
+	///	    The type of the elements in the source sequence.
+	/// </typeparam>
+	/// <param name="source">
+	///	    An <see cref="IEnumerable{T}"/> to filter.
+	/// </param>
+	/// <param name="offset">
+	///	    The offset (expressed as a positive number) by which to lag each element of the sequence.
+	/// </param>
+	/// <param name="defaultLagValue">
+	///	    A default value supplied for the lagged element when none is available
+	///	</param>
+	/// <param name="predicate">
+	///	    A function which accepts the current and lagged element (in that order) to test for a condition.
+	/// </param>
+	/// <returns>
+	///	    An <see cref="IEnumerable{T}"/> that contains elements from the input sequence that satisfy the condition.
+	/// </returns>
+	/// <exception cref="ArgumentNullException">
+	///	    <paramref name="source"/> or <paramref name="predicate"/> is <see langword="null"/>.
+	/// </exception>
+	/// <exception cref="ArgumentOutOfRangeException">
+	///	    <paramref name="offset"/> is below <c>1</c>.
+	/// </exception>
 	/// <remarks>
 	/// <para>
-	/// This operator evaluates in a deferred and streaming manner.
+	///	    This operator evaluates in a deferred and streaming manner.
 	/// </para>
 	/// </remarks>
 	public static IEnumerable<TSource> WhereLag<TSource>(this IEnumerable<TSource> source, int offset, TSource defaultLagValue, Func<TSource, TSource, bool> predicate)
