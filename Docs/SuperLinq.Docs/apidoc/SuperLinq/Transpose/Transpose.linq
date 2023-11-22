72dccf7fbf3e80096505cdd90ffbe43fc66620bf
Stuart Turner <stuart@turner-isageek.com>
2023-11-22 09:58:11 -0600

    Update documentation for `Transpose`

---
+++ Docs/SuperLinq.Docs/apidoc/SuperLinq/Transpose/Transpose.linq
@@ -0,0 +1,28 @@
+<Query Kind="Statements">
+  <NuGetReference>SuperLinq</NuGetReference>
+  <Namespace>SuperLinq</Namespace>
+</Query>
+
+var matrix = new[]
+{
+    new[] { 10, 11 },
+    new[] { 20 },
+    new[] { 30, 31, 32 }
+};
+
+// Transpose a 2d sequence
+var result = matrix.Transpose();
+
+Console.WriteLine(
+	"[" + Environment.NewLine +
+	string.Join(
+		", " + Environment.NewLine,
+		result.Select(c => "   [" + string.Join(", ", c) + "]")) +
+	Environment.NewLine + "]");
+
+// One possible random output is as follows:
+// [
+//    [10, 20, 30],
+//    [11, 31],
+//    [32]
+// ]
--- Source/SuperLinq/Transpose.cs
+++ Source/SuperLinq/Transpose.cs
@@ -3,34 +3,26 @@
 public static partial class SuperEnumerable
 {
 	/// <summary>
-	/// Transposes a sequence of rows into a sequence of columns.
+	///	    Transposes a sequence of rows into a sequence of columns.
 	/// </summary>
-	/// <typeparam name="T">Type of source sequence elements.</typeparam>
-	/// <param name="source">Source sequence to transpose.</param>
+	/// <typeparam name="T">
+	///	    Type of source sequence elements.
+	/// </typeparam>
+	/// <param name="source">
+	///	    Source sequence to transpose.
+	/// </param>
 	/// <returns>
-	/// Returns a sequence of columns in the source swapped into rows.
+	///	    Returns a sequence of columns in the source swapped into rows.
 	/// </returns>
-	/// <exception cref="ArgumentNullException"><paramref name="source"/> is <see langword="null"/>.</exception>
+	/// <exception cref="ArgumentNullException">
+	///	    <paramref name="source"/> is <see langword="null"/>.
+	/// </exception>
 	/// <remarks>
-	/// If a rows is shorter than a follow it then the shorter row's
-	/// elements are skipped in the corresponding column sequences.
-	/// This operator uses deferred execution and streams its results.
-	/// Source sequence is consumed greedily when an iteration begins.
-	/// The inner sequences representing rows are consumed lazily and
-	/// resulting sequences of columns are streamed.
+	///	    If a rows is shorter than a follow it then the shorter row's elements are skipped in the corresponding
+	///     column sequences. This operator uses deferred execution and streams its results. Source sequence is consumed
+	///     greedily when an iteration begins. The inner sequences representing rows are consumed lazily and resulting
+	///     sequences of columns are streamed.
 	/// </remarks>
-	/// <example>
-	/// <code><![CDATA[
-	/// var matrix = new[]
-	/// {
-	///     new[] { 10, 11 },
-	///     new[] { 20 },
-	///     new[] { 30, 31, 32 }
-	/// };
-	/// var result = matrix.Transpose();
-	/// ]]></code>
-	/// The <c>result</c> variable will contain [[10, 20, 30], [11, 31], [32]].
-	/// </example>
 	public static IEnumerable<IEnumerable<T>> Transpose<T>(this IEnumerable<IEnumerable<T>> source)
 	{
 		Guard.IsNotNull(source);
