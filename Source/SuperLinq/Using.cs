ba4471ae883d4809be27098540b85ea9efe5f492
Stuart Turner <stuart@turner-isageek.com>
2023-11-22 11:46:44 -0600

    Update documentation for `Using`

--- Source/SuperLinq/Using.cs
+++ Source/SuperLinq/Using.cs
@@ -3,26 +3,37 @@
 public static partial class SuperEnumerable
 {
 	/// <summary>
-	/// Generates a sequence that's dependent on a resource object whose lifetime is determined by the sequence usage
-	/// duration.
+	///	    Generates a sequence that's dependent on a resource object whose lifetime is determined by the sequence
+	///     usage duration.
 	/// </summary>
-	/// <typeparam name="TSource">Source element type.</typeparam>
-	/// <typeparam name="TResource">Resource type.</typeparam>
-	/// <param name="resourceFactory">Resource factory function.</param>
-	/// <param name="enumerableFactory">Enumerable factory function, having access to the obtained resource.</param>
-	/// <returns>Sequence whose use controls the lifetime of the associated obtained resource.</returns>
-	/// <exception cref="ArgumentNullException"><paramref name="resourceFactory"/> or <paramref
-	/// name="enumerableFactory"/> is <see langword="null"/>.</exception>
+	/// <typeparam name="TSource">
+	///	    Source element type.
+	/// </typeparam>
+	/// <typeparam name="TResource">
+	///	    Resource type.
+	/// </typeparam>
+	/// <param name="resourceFactory">
+	///	    Resource factory function.
+	/// </param>
+	/// <param name="enumerableFactory">
+	///	    Enumerable factory function, having access to the obtained resource.
+	/// </param>
+	/// <returns>
+	///	    Sequence whose use controls the lifetime of the associated obtained resource.
+	/// </returns>
+	/// <exception cref="ArgumentNullException">
+	///	    <paramref name="resourceFactory"/> or <paramref name="enumerableFactory"/> is <see langword="null"/>.
+	/// </exception>
 	/// <remarks>
 	/// <para>
-	/// <paramref name="resourceFactory"/> and <paramref name="enumerableFactory"/> are evaluated lazily, once
-	/// enumeration has begun. The value returned by <paramref name="resourceFactory"/> will be disposed after the
-	/// enumeration has completed. 
+	///	    <paramref name="resourceFactory"/> and <paramref name="enumerableFactory"/> are evaluated lazily, once
+	///     enumeration has begun. The value returned by <paramref name="resourceFactory"/> will be disposed after the
+	///     enumeration has completed. 
 	/// </para>
 	/// <para>
-	/// The values returned by <paramref name="enumerableFactory"/> and <paramref name="enumerableFactory"/> are not
-	/// cached; multiple iterations of the <see cref="IEnumerable{T}"/> returned by this method will call these methods
-	/// separately for each iteration.
+	///	    The values returned by <paramref name="enumerableFactory"/> and <paramref name="enumerableFactory"/> are not
+	///     cached; multiple iterations of the <see cref="IEnumerable{T}"/> returned by this method will call these
+	///     methods separately for each iteration.
 	/// </para>
 	/// </remarks>
 	public static IEnumerable<TSource> Using<TSource, TResource>(
