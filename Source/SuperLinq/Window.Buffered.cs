a6907be039e78568c416e98b716be97513c3ca00
Stuart Turner <stuart@turner-isageek.com>
2023-11-22 13:39:14 -0600

    Update documentation for `Window`

--- Source/SuperLinq/Window.Buffered.cs
+++ Source/SuperLinq/Window.Buffered.cs
@@ -3,24 +3,46 @@
 public static partial class SuperEnumerable
 {
 	/// <summary>
-	/// Processes a sequence into a series of subsequences representing a windowed subset of the original, and then
-	/// projecting them into a new form.
+	///	    Processes a sequence into a series of subsequences representing a windowed subset of the original, and then
+	///     projecting them into a new form.
 	/// </summary>
-	/// <typeparam name="TSource">The type of the elements of <paramref name="selector"/>.</typeparam>
-	/// <typeparam name="TResult">The type of the value return by <paramref name="selector"/>.</typeparam>
-	/// <param name="source">The sequence to evaluate a sliding window over.</param>
-	/// <param name="size">The size (number of elements) in each window.</param>
-	/// <param name="selector">A transform function to apply to each window.</param>
-	/// <returns>An <see cref="IEnumerable{T}"/> whose elements are the result of invoking the transform function on
-	/// each window of <paramref name="source"/>.</returns>
+	/// <typeparam name="TSource">
+	///	    The type of the elements of <paramref name="selector"/>.
+	/// </typeparam>
+	/// <typeparam name="TResult">
+	///	    The type of the value return by <paramref name="selector"/>.
+	/// </typeparam>
+	/// <param name="source">
+	///	    The sequence to evaluate a sliding window over.
+	/// </param>
+	/// <param name="size">
+	///	    The size (number of elements) in each window.
+	/// </param>
+	/// <param name="selector">
+	///	    A transform function to apply to each window.
+	/// </param>
+	/// <returns>
+	///	    An <see cref="IEnumerable{T}"/> whose elements are the result of invoking the transform function on each
+	///     window of <paramref name="source"/>.
+	/// </returns>
+	/// <exception cref="ArgumentNullException">
+	///	    <paramref name="source"/> is <see langword="null" />.
+	/// </exception>
+	/// <exception cref="ArgumentOutOfRangeException">
+	///	    <paramref name="size"/> is below <c>1</c>.
+	/// </exception>
 	/// <remarks>
 	/// <para>
-	/// The number of sequences returned is: <c>Max(0, <paramref name="source"/>.Count() - <paramref name="size"/> +
-	/// 1)</c><br/> Returned subsequences are buffered, but the overall operation is streamed.<br/>
+	///	    A window can contain fewer elements than <paramref name="size"/>, especially as it slides over the start of
+	///     the sequence.
 	/// </para>
 	/// <para>
-	/// In this overload of <c>Window</c>, a single array of length <paramref name="size"/> is allocated as a buffer for
-	/// all subsequences.</para>
+	///	    In this overload of <c>Window</c>, a single array of length <paramref name="size"/> is allocated as a
+	///     buffer for all subsequences.
+	/// </para>
+	/// <para>
+	///	    This operator uses deferred execution and streams its results.
+	/// </para>
 	/// </remarks>
 	public static IEnumerable<TResult> Window<TSource, TResult>(
 		this IEnumerable<TSource> source,
@@ -35,25 +57,42 @@ public static partial class SuperEnumerable
 	}
 
 	/// <summary>
-	/// Processes a sequence into a series of subsequences representing a windowed subset of the original, and then
-	/// projecting them into a new form.
+	///	    Processes a sequence into a series of subsequences representing a windowed subset of the original, and then
+	///     projecting them into a new form.
 	/// </summary>
-	/// <typeparam name="TSource">The type of the elements of <paramref name="selector"/>.</typeparam>
-	/// <typeparam name="TResult">The type of the value return by <paramref name="selector"/>.</typeparam>
-	/// <param name="source">The sequence to evaluate a sliding window over.</param>
-	/// <param name="array">An array to use as a buffer for each subsequence.</param>
-	/// <param name="selector">A transform function to apply to each window.</param>
-	/// <returns>An <see cref="IEnumerable{T}"/> whose elements are the result of invoking the transform function on
-	/// each window of <paramref name="source"/>.</returns>
+	/// <typeparam name="TSource">
+	///	    The type of the elements of <paramref name="selector"/>.
+	/// </typeparam>
+	/// <typeparam name="TResult">
+	///	    The type of the value return by <paramref name="selector"/>.
+	/// </typeparam>
+	/// <param name="source">
+	///	    The sequence to evaluate a sliding window over.
+	/// </param>
+	/// <param name="array">
+	///	    An array to use as a buffer for each subsequence.
+	/// </param>
+	/// <param name="selector">
+	///	    A transform function to apply to each window.
+	/// </param>
+	/// <returns>
+	///	    An <see cref="IEnumerable{T}"/> whose elements are the result of invoking the transform function on each
+	///     window of <paramref name="source"/>.
+	/// </returns>
+	/// <exception cref="ArgumentNullException">
+	///	    <paramref name="source"/> is <see langword="null" />.
+	/// </exception>
 	/// <remarks>
 	/// <para>
-	/// The number of sequences returned is: <c>Max(0, <paramref name="source"/>.Count() - <paramref
-	/// name="array"/>.Length + 1)</c><br/> Returned subsequences are buffered, but the overall operation is
-	/// streamed.<br/>
+	///	    A window can contain fewer elements than <c><paramref name="array"/>.Length</c>, especially as it slides
+	///     over the start of the sequence.
 	/// </para>
 	/// <para>
-	/// In this overload of <c>Window</c>, <paramref name="array"/> is used as a common buffer for all
-	/// subsequences.
+	///	    In this overload of <c>Window</c>, <paramref name="array"/> is used as a common buffer for all
+	///     subsequences.
+	/// </para>
+	/// <para>
+	///	    This operator uses deferred execution and streams its results.
 	/// </para>
 	/// </remarks>
 	public static IEnumerable<TResult> Window<TSource, TResult>(
@@ -69,28 +108,49 @@ public static partial class SuperEnumerable
 	}
 
 	/// <summary>
-	/// Processes a sequence into a series of subsequences representing a windowed subset of the original, and then
-	/// projecting them into a new form.
+	///	    Processes a sequence into a series of subsequences representing a windowed subset of the original, and then
+	///     projecting them into a new form.
 	/// </summary>
-	/// <typeparam name="TSource">The type of the elements of <paramref name="selector"/>.</typeparam>
-	/// <typeparam name="TResult">The type of the value return by <paramref name="selector"/>.</typeparam>
-	/// <param name="source">The sequence to evaluate a sliding window over.</param>
-	/// <param name="array">An array to use as a buffer for each subsequence.</param>
-	/// <param name="size">The size (number of elements) in each window.</param>
-	/// <param name="selector">A transform function to apply to each window.</param>
-	/// <returns>An <see cref="IEnumerable{T}"/> whose elements are the result of invoking the transform function on
-	/// each window of <paramref name="source"/>.</returns>
+	/// <typeparam name="TSource">
+	///	    The type of the elements of <paramref name="selector"/>.
+	/// </typeparam>
+	/// <typeparam name="TResult">
+	///	    The type of the value return by <paramref name="selector"/>.
+	/// </typeparam>
+	/// <param name="source">
+	///	    The sequence to evaluate a sliding window over.
+	/// </param>
+	/// <param name="array">
+	///	    An array to use as a buffer for each subsequence.
+	/// </param>
+	/// <param name="size">
+	///	    The size (number of elements) in each window.
+	/// </param>
+	/// <param name="selector">
+	///	    A transform function to apply to each window.
+	/// </param>
+	/// <returns>
+	///	    An <see cref="IEnumerable{T}"/> whose elements are the result of invoking the transform function on each
+	///     window of <paramref name="source"/>.
+	/// </returns>
+	/// <exception cref="ArgumentNullException">
+	///	    <paramref name="source"/> is <see langword="null" />.
+	/// </exception>
+	/// <exception cref="ArgumentOutOfRangeException">
+	///	    <paramref name="size"/> is below <c>1</c> or above <c><paramref name="array"/>.Length</c>.
+	/// </exception>
 	/// <remarks>
 	/// <para>
-	/// The number of sequences returned is: <c>Max(0, <paramref name="source"/>.Count() - <paramref name="size" /> +
-	/// 1)</c><br/> Returned subsequences are buffered, but the overall operation is streamed.<br/>
+	///	    A window can contain fewer elements than <paramref name="size"/>, especially as it slides over the start of
+	///     the sequence.
 	/// </para>
 	/// <para>
-	/// In this overload of <c>Window</c>, <paramref name="array"/> is used as a common buffer for all subsequences.
+	///	    In this overload of <c>Window</c>, <paramref name="array"/> is used as a common buffer for all
+	///     subsequences.<br/> This overload is provided to ease usage of common buffers, such as those rented from <see
+	///     cref="System.Buffers.ArrayPool{T}"/>, which may return an array larger than requested.
 	/// </para>
 	/// <para>
-	/// This overload is provided to ease usage of common buffers, such as those rented from <see
-	/// cref="System.Buffers.ArrayPool{T}"/>, which may return an array larger than requested.
+	///	    This operator uses deferred execution and streams its results.
 	/// </para>
 	/// </remarks>
 	public static IEnumerable<TResult> Window<TSource, TResult>(
