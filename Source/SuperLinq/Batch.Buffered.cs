9e1ffda071de874a46416d2899c4bd51a143e8c9
Stuart Turner <stuart@turner-isageek.com>
2023-11-24 10:14:29 -0600

    Fix bug in implementation

--- Source/SuperLinq/Batch.Buffered.cs
+++ Source/SuperLinq/Batch.Buffered.cs
@@ -166,12 +166,17 @@ public static partial class SuperEnumerable
 		int size,
 		Func<IReadOnlyList<TSource>, TResult> resultSelector)
 	{
-		if (source is ICollection<TSource> coll
-			&& coll.Count <= size)
+		if (source is ICollection<TSource> coll)
 		{
-			coll.CopyTo(array, 0);
-			yield return resultSelector(new ArraySegment<TSource>(array, 0, coll.Count));
-			yield break;
+			if (coll.Count == 0)
+				yield break;
+
+			if (coll.Count <= size)
+			{
+				coll.CopyTo(array, 0);
+				yield return resultSelector(new ArraySegment<TSource>(array, 0, coll.Count));
+				yield break;
+			}
 		}
 
 		var n = 0;
