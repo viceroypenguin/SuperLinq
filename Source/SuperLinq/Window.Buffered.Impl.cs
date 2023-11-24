1e9151a23ebe6d9f9f790c6abae699ffe127248b
Stuart Turner <stuart@turner-isageek.com>
2023-11-24 09:43:14 -0600

    Fix bug in buffered `Window` implementations

--- Source/SuperLinq/Window.Buffered.Impl.cs
+++ Source/SuperLinq/Window.Buffered.Impl.cs
@@ -22,10 +22,10 @@ public static partial class SuperEnumerable
 			}
 			else
 			{
-				array.AsSpan()[1..].CopyTo(array);
-				array[^1] = i;
+				array.AsSpan()[1..n].CopyTo(array);
+				array[n - 1] = i;
 
-				yield return projector(new ArraySegment<TSource>(array));
+				yield return projector(new ArraySegment<TSource>(array, 0, n));
 			}
 		}
 
