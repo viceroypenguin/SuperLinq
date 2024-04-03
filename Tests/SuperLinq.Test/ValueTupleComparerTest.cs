e76f4ed8a1b4b91db3e1fd24e7aed5a5324ab963
Elianora Rose <developer@elianorarose.com>
2024-04-03 10:42:28 -0400

    Adding tests for custom IComparer input to ValueTupleComparer

--- Tests/SuperLinq.Test/ValueTupleComparerTest.cs
+++ Tests/SuperLinq.Test/ValueTupleComparerTest.cs
@@ -1,7 +1,19 @@
+using System.Diagnostics.CodeAnalysis;
+
 namespace Test;
 
 public class ValueTupleComparerTest
 {
+	private class TestComparer : IComparer<string>
+	{
+		public int Compare([AllowNull] string x, [AllowNull] string y)
+		{
+			var xLen = x?.Length ?? 0;
+			var yLen = y?.Length ?? 0;
+			return xLen.CompareTo(yLen);
+		}
+	}
+
 	[Fact]
 	public void ValueTupleComparerShouldCreateWithDefaultComparers()
 	{
@@ -20,5 +32,17 @@ public class ValueTupleComparerTest
 		ValueTuple<int, int> right = new(1, 2);
 		var result = comparer.Compare(left, right);
 		Assert.Equal(1, result);
+	}
+
+	[Fact]
+	public void ValueTupleComparerShouldAcceptCustomComparers()
+	{
+		TestComparer innerLeftComparer = new();
+		TestComparer innerRightComparer = new();
+		var comparer = ValueTupleComparer.Create(innerLeftComparer, innerRightComparer);
+		ValueTuple<string, string> left = new("123", "1");
+		ValueTuple<string, string> right = new("123", "123");
+		var result = comparer.Compare(left, right);
+		Assert.Equal(-1, result);
 	}
 }
