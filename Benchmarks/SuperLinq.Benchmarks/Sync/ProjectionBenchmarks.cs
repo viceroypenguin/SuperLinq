499bbf91ced1c78e6735754e4052f90b5a6ffe24
Stuart Turner <stuart@turner-isageek.com>
2024-04-30 19:31:09 -0500

    Fix Projection benchmarks

--- Benchmarks/SuperLinq.Benchmarks/Sync/ProjectionBenchmarks.cs
+++ Benchmarks/SuperLinq.Benchmarks/Sync/ProjectionBenchmarks.cs
@@ -1,51 +1,87 @@
 using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Engines;
 
 namespace SuperLinq.Benchmarks.Sync;
 
 [BenchmarkCategory("Projection")]
 public class ProjectionBenchmarks : TestDataBenchmark
 {
-	[Benchmark]
-	[ArgumentsSource(nameof(ListSpecialization))]
-	public void EquiZip(LinqTestData testData) => testData.Collection.EquiZip(testData.Collection);
+	private readonly Consumer _consumer = new();
 
 	[Benchmark]
 	[ArgumentsSource(nameof(ListSpecialization))]
-	public void ZipShortest(LinqTestData testData) => testData.Collection.ZipShortest(testData.Collection);
+	public void EquiZip(LinqTestData testData) =>
+		testData.Collection
+			.EquiZip(testData.Collection)
+			.Consume(_consumer);
 
 	[Benchmark]
 	[ArgumentsSource(nameof(ListSpecialization))]
-	public void ZipLongest(LinqTestData testData) => testData.Collection.ZipLongest(testData.Collection);
+	public void ZipShortest(LinqTestData testData) =>
+		testData.Collection
+			.ZipShortest(testData.Collection)
+			.Consume(_consumer);
+
+	[Benchmark]
+	[ArgumentsSource(nameof(ListSpecialization))]
+	public void ZipLongest(LinqTestData testData) =>
+		testData.Collection
+			.ZipLongest(testData.Collection)
+			.Consume(_consumer);
 
 	[Benchmark]
 	[ArgumentsSource(nameof(CountSpecialization))]
-	public void CountDown(LinqTestData testData) => testData.Collection.CountDown(5);
+	public void CountDown(LinqTestData testData) =>
+		testData.Collection
+			.CountDown(5)
+			.Consume(_consumer);
 
 	[Benchmark]
 	[ArgumentsSource(nameof(CountSpecialization))]
-	public void TagFirstLast(LinqTestData testData) => testData.Collection.TagFirstLast();
+	public void TagFirstLast(LinqTestData testData) =>
+		testData.Collection
+			.TagFirstLast()
+			.Consume(_consumer);
 
 	[Benchmark]
 	[ArgumentsSource(nameof(ListSpecialization))]
-	public void Index(LinqTestData testData) => testData.Collection.Index();
+	public void Index(LinqTestData testData) =>
+		testData.Collection
+			.Index()
+			.Consume(_consumer);
 
 	[Benchmark]
 	[ArgumentsSource(nameof(NoSpecialization))]
-	public void IndexBy(LinqTestData testData) => testData.Collection.IndexBy(x => x % 10);
+	public void IndexBy(LinqTestData testData) =>
+		testData.Collection
+			.IndexBy(x => x % 10)
+			.Consume(_consumer);
 
 	[Benchmark]
 	[ArgumentsSource(nameof(ListSpecialization))]
-	public void Lag(LinqTestData testData) => testData.Collection.Lag(1);
+	public void Lag(LinqTestData testData) =>
+		testData.Collection
+			.Lag(1)
+			.Consume(_consumer);
 
 	[Benchmark]
 	[ArgumentsSource(nameof(ListSpecialization))]
-	public void Lead(LinqTestData testData) => testData.Collection.Lead(1);
+	public void Lead(LinqTestData testData) =>
+		testData.Collection
+			.Lead(1)
+			.Consume(_consumer);
 
 	[Benchmark]
 	[ArgumentsSource(nameof(NoSpecialization))]
-	public void Rank(LinqTestData testData) => testData.Collection.Rank();
+	public void Rank(LinqTestData testData) =>
+		testData.Collection
+			.Rank()
+			.Consume(_consumer);
 
 	[Benchmark]
 	[ArgumentsSource(nameof(ListSpecialization))]
-	public void ZipMap(LinqTestData testData) => testData.Collection.ZipMap(_ => true);
+	public void ZipMap(LinqTestData testData) =>
+		testData.Collection
+			.ZipMap(_ => true)
+			.Consume(_consumer);
 }
